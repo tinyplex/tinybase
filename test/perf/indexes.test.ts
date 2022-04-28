a8f08ec38c6a24633ddf7fb4c7fdabd1eca06dbb
James Pearce <github@tripleodeon.com>
2022-04-28 10:18:32 -0700

    [hygiene] Performance test keys

--- test/perf/indexes.test.ts
+++ test/perf/indexes.test.ts
@@ -4,25 +4,25 @@ import {repeatRows} from './common';
 let store: Store;
 beforeEach(() => {
   store = createStore();
-  createIndexes(store).setIndexDefinition('oddEven', 'table', (getCell) =>
-    (getCell('cell') as any as number) % 2 == 0 ? 'even' : 'odd',
+  createIndexes(store).setIndexDefinition('i1', 't1', (getCell) =>
+    (getCell('c1') as any as number) % 2 == 0 ? 'even' : 'odd',
   );
 });
 
 repeatRows(
   'Grow store, different table to index',
-  (n) => store.setRow('table2', 'row' + n, {cell: n}),
+  (n) => store.setRow('t2', 'r' + n, {c1: n}),
   90,
 );
 
 repeatRows(
   'Grow store, same table as index, unrelated cells',
-  (n) => store.setRow('table', 'row' + n, {cell2: n}),
+  (n) => store.setRow('t1', 'r' + n, {c2: n}),
   90,
 );
 
 repeatRows(
   'Grow store, with index',
-  (n) => store.setRow('table', 'row' + n, {cell: n}),
+  (n) => store.setRow('t1', 'r' + n, {c1: n}),
   90,
 );
