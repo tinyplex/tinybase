3c1e2ea95af31efb94a9eba52003b41f2c7d9e78
James Pearce <github@tripleodeon.com>
2022-09-15 12:15:56 -0400

    [tests] Split out E2E tests

---
+++ test/e2e/demos/countries.test.ts
@@ -0,0 +1,33 @@
+import {
+  expectNoFramedElement,
+  expectProperty,
+  expectedElement,
+  expectedFramedElement,
+  getServerFunctions,
+} from '../common';
+
+const [startServer, stopServer, expectPage] = getServerFunctions(8801);
+
+beforeAll(startServer);
+afterAll(stopServer);
+
+test('countries', async () => {
+  await expectPage(`/demos/countries`);
+  await expectedElement('h1', 'Countries');
+  await (await expectedFramedElement('.filter', '★8')).click();
+  await expectedFramedElement('#countries .country', 'United Kingdom');
+  await expectNoFramedElement('#countries .country', 'Bahamas');
+  await (await expectedFramedElement('#countries .country .star', '★')).click();
+  await expectedFramedElement('.filter', '★7');
+  await expectNoFramedElement('#countries .country', 'United Kingdom');
+  const b = await expectedFramedElement('.filter', 'B21');
+  await expectProperty(b, 'className', 'filter');
+  await b.click();
+  await expectProperty(b, 'className', 'filter current');
+  await expectedFramedElement('#countries .country', 'Bahamas');
+  await (await expectedFramedElement('#countries .country .star', '☆')).click();
+  await (await expectedFramedElement('.filter', '★8')).click();
+  await expectedFramedElement('#countries .country', 'Bahamas');
+  await page.reload();
+  await expectedFramedElement('#countries .country', 'Bahamas');
+});
---
+++ test/e2e/demos/hello-world.test.ts
@@ -0,0 +1,34 @@
+import {
+  expectedElement,
+  expectedFramedElement,
+  getServerFunctions,
+} from '../common';
+
+const [startServer, stopServer, expectPage] = getServerFunctions(8801);
+
+beforeAll(startServer);
+afterAll(stopServer);
+
+test('hello-world-v1', async () => {
+  await expectPage(`/demos/hello-world/hello-world-v1/`);
+  await expectedElement('h1', 'Hello World v1');
+  await expectedFramedElement('body', 'Hello World');
+});
+
+test('hello-world-v2', async () => {
+  await expectPage(`/demos/hello-world/hello-world-v2/`);
+  await expectedElement('h1', 'Hello World v2');
+  await expectedFramedElement(
+    'body',
+    new Date(new Date().getTime() + 1000).toLocaleTimeString(),
+  );
+});
+
+test('hello-world-v3', async () => {
+  await expectPage(`/demos/hello-world/hello-world-v3/`);
+  await expectedElement('h1', 'Hello World v3');
+  await expectedFramedElement(
+    'body',
+    new Date(new Date().getTime() + 1000).toLocaleTimeString(),
+  );
+});
