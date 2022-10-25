55fb933e1bef0cba465aa5e09bb7a3a574158d5d
James Pearce <github@tripleodeon.com>
2022-10-25 13:46:32 -0400

    [tools] Refactor code generation

--- src/tools/code.ts
+++ src/tools/code.ts
@@ -1,11 +1,29 @@
-import {arrayMap, arrayPush} from '../common/array';
+import {
+  IdMap,
+  mapEnsure,
+  mapForEach,
+  mapMap,
+  mapNew,
+  mapSet,
+} from '../common/map';
+import {IdSet2, setAdd, setNew} from '../common/set';
+import {Pair, pairNewMap} from '../common/pairs';
+import {
+  arrayForEach,
+  arrayMap,
+  arrayPop,
+  arrayPush,
+  arrayShift,
+  arraySort,
+} from '../common/array';
 import {EMPTY_STRING} from '../common/strings';
+import {collValues} from '../common/coll';
+import {isArray} from '../common/other';
 
 const NON_ALPHANUMERIC = /[^A-Za-z0-9]+/;
 const CLOSING = /^[\])}]/;
 const OPENING = /[[({]$/;
-
-const join = (str: string[], sep = '') => str.join(sep);
+const LINE = '\n';
 
 const substr = (str: string, start: number, end?: number) =>
   str.substring(start, end);
@@ -13,6 +31,8 @@ const substr = (str: string, start: number, end?: number) =>
 const upper = (str: string) => str.toUpperCase();
 
 const lower = (str: string) => str.toLowerCase();
+
+export const join = (str: string[], sep = EMPTY_STRING) => str.join(sep);
 
 export const camel = (str: string, firstCap = false) =>
   join(
@@ -25,26 +45,114 @@ export const camel = (str: string, firstCap = false) =>
   );
 
 export const getCodeFunctions = (): [
-  (...lines: string[]) => void,
-  () => string,
+  (...lines: string[]) => string,
+  (location: 0 | 1, source: string, ...items: string[]) => void,
+  (name: string, body: string) => void,
+  (name: string, parameters: string, returnType: string, body: string) => void,
+  (name: string, parameters: string, body: string | string[]) => void,
+  (name: string, body: string | string[]) => void,
+  (location: 0 | 1) => string[],
+  () => string[],
+  (location: 0 | 1) => string[],
+  () => string[],
 ] => {
-  const allLines: string[] = [];
-  const add = (...lines: string[]) =>
-    lines.forEach((line) => arrayPush(allLines, line.trim()));
-  const build = () => {
+  const allImports: Pair<IdSet2> = pairNewMap();
+  const types: IdMap<string> = mapNew();
+  const methods: IdMap<[parameters: string, returnType: string, body: string]> =
+    mapNew();
+  const constants: IdMap<string[]> = mapNew();
+
+  const build = (...lines: string[]): string => {
     let indent = 0;
-    return allLines
-      .map(
-        (line) =>
-          EMPTY_STRING.padStart(
-            (CLOSING.test(line)
-              ? --indent
-              : OPENING.test(line)
-              ? indent++
-              : indent) * 2,
-          ) + line,
-      )
-      .join('\n');
+    return (
+      lines
+        .map(
+          (line) =>
+            EMPTY_STRING.padStart(
+              (CLOSING.test(line)
+                ? --indent
+                : OPENING.test(line)
+                ? indent++
+                : indent) * 2,
+            ) + line,
+        )
+        .join(LINE) + LINE
+    );
   };
-  return [add, build];
+
+  const addImport = (location: 0 | 1, source: string, ...items: string[]) =>
+    arrayForEach(items, (item) =>
+      setAdd(mapEnsure(allImports[location], source, setNew), item),
+    );
+
+  const addType = (name: string, body: string) => mapSet(types, name, body);
+
+  const addMethod = (
+    name: string,
+    parameters: string,
+    returnType: string,
+    body: string,
+  ) => mapSet(methods, name, [parameters, returnType, body]);
+
+  const addFunction = (
+    name: string,
+    parameters: string,
+    body: string | string[],
+  ) =>
+    mapSet(
+      constants,
+      name,
+      isArray(body)
+        ? [`(${parameters}) => {`, ...body, '}']
+        : [`(${parameters}) => ${body}`],
+    );
+
+  const addConstant = (name: string, body: string | string[]) =>
+    mapSet(constants, name, isArray(body) ? ['{', ...body, '}'] : [body]);
+
+  const getImports = (location: 0 | 1): string[] => [
+    ...arraySort(
+      mapMap(
+        allImports[location],
+        (items, source) =>
+          `import {${join(
+            arraySort(collValues(items)),
+            ', ',
+          )}} from '${source}';`,
+      ),
+    ),
+    EMPTY_STRING,
+  ];
+
+  const getTypes = (): string[] =>
+    mapMap(types, (body, name) => `export type ${name} = ${body};`);
+
+  const getMethods = (location: 0 | 1): string[] =>
+    mapMap(methods, ([parameters, returnType, body], name) =>
+      location
+        ? `${name}: (${parameters}): ${returnType} => ${body},`
+        : `${name}(${parameters}): ${returnType};`,
+    );
+
+  const getConstants = (): string[] => {
+    const lines: string[] = [];
+    mapForEach(constants, (name, body) => {
+      arrayPush(body, `${arrayPop(body)};`);
+      arrayPush(lines, `const ${name} = ${arrayShift(body)}`, ...body);
+    });
+    return lines;
+  };
+
+  return [
+    build,
+    addImport,
+    addType,
+    addMethod,
+    addFunction,
+    addConstant,
+    getImports,
+    getTypes,
+    getMethods,
+    getConstants,
+  ];
 };
