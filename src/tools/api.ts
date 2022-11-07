46b791cf2ee8b6544d640048c4c6ff2341cbd52a
James Pearce <github@tripleodeon.com>
2022-11-07 14:51:21 -0500

    [tools] Remove verbose helper functions

--- src/tools/api.ts
+++ src/tools/api.ts
@@ -12,6 +12,10 @@ const THE_CONTENT_OF = 'the content of';
 const THE_CONTENT_OF_THE_STORE = `${THE_CONTENT_OF} ${THE_STORE}`;
 const THE_SPECIFIED_ROW = 'the specified Row';
 
+const storeMethod = (method: string, parameters = '', cast = '') =>
+  `store.${method}(${parameters})${cast ? ` as ${cast}` : ''}`;
+const fluentStoreMethod = (method: string, parameters = '') =>
+  `fluent(() => ${storeMethod(method, parameters)})`;
 const getIdsDoc = (idsNoun: string, parentNoun: string, sorted = 0) =>
   `Gets ${
     sorted ? 'sorted, paginated' : 'the'
@@ -69,15 +73,11 @@ export const getStoreApi = (
   addImport(
     1,
     'tinybase',
-    'Cell',
     'ChangedCells',
     'Id',
     'Ids',
     'InvalidCells',
-    'Row',
     'Store',
-    'Table',
-    'Tables',
     'createStore',
   );
   addImport(
@@ -88,77 +88,6 @@ export const getStoreApi = (
   );
 
   addFunction('fluent', 'actions: () => Store', ['actions();', returnStore]);
-
-  addFunction('setTables', 'tables: Tables', [
-    'store.setTables(tables);',
-    returnStore,
-  ]);
-  addFunction('delTables', '', ['store.delTables();', returnStore]);
-
-  addFunction('hasTable', 'tableId: Id', 'store.hasTable(tableId)');
-  addFunction('getTable', 'tableId: Id', 'store.getTable(tableId) as any');
-  addFunction('setTable', 'tableId: Id, table: Table', [
-    'store.setTable(tableId, table);',
-    returnStore,
-  ]);
-  addFunction('delTable', 'tableId: Id', [
-    'store.delTable(tableId);',
-    returnStore,
-  ]);
-  addFunction('getRowIds', 'tableId: Id', 'store.getRowIds(tableId)');
-  addFunction(
-    'getSortedRowIds',
-    'tableId: Id, ...args: any[]',
-    'store.getSortedRowIds(tableId, ...args)',
-  );
-
-  addFunction(
-    'hasRow',
-    'tableId: Id, rowId: Id',
-    'store.hasRow(tableId, rowId)',
-  );
-  addFunction(
-    'getRow',
-    'tableId: Id, rowId: Id',
-    'store.getRow(tableId, rowId) as any',
-  );
-  addFunction('setRow', 'tableId: Id, rowId: Id, row: Row', [
-    'store.setRow(tableId, rowId, row);',
-    returnStore,
-  ]);
-  addFunction('addRow', 'tableId: Id, row: Row', 'store.addRow(tableId, row)');
-  addFunction('setPartialRow', 'tableId: Id, rowId: Id, partialRow: Row', [
-    'store.setPartialRow(tableId, rowId, partialRow);',
-    returnStore,
-  ]);
-  addFunction('delRow', 'tableId: Id, rowId: Id', [
-    'store.delRow(tableId, rowId);',
-    returnStore,
-  ]);
-  addFunction(
-    'getCellIds',
-    'tableId: Id, rowId: Id',
-    'store.getCellIds(tableId, rowId)',
-  );
-
-  addFunction(
-    'hasCell',
-    'tableId: Id, rowId: Id, cellId: Id',
-    'store.hasCell(tableId, rowId, cellId)',
-  );
-  addFunction(
-    'getCell',
-    'tableId: Id, rowId: Id, cellId: Id',
-    'store.getCell(tableId, rowId, cellId) as any',
-  );
-  addFunction('setCell', 'tableId: Id, rowId: Id, cellId: Id, cell: Cell', [
-    'store.setCell(tableId, rowId, cellId, cell);',
-    returnStore,
-  ]);
-  addFunction('delCell', 'tableId: Id, rowId: Id, cellId: Id', [
-    'store.delCell(tableId, rowId, cellId);',
-    returnStore,
-  ]);
 
   const TYPE2 = addConstant(snake(TYPE), `'${TYPE}'`);
   const DEFAULT2 = addConstant(snake(DEFAULT), `'${DEFAULT}'`);
@@ -171,29 +100,35 @@ export const getStoreApi = (
     `hasTables`,
     '',
     BOOLEAN,
-    'store.hasTables()',
+    storeMethod('hasTables'),
     getHasDoc('any Table'),
   );
   addMethod(
     `getTables`,
     '',
     tablesType,
-    'store.getTables()',
+    storeMethod('getTables'),
     getStoreContentDoc(),
   );
   addMethod(
     `setTables`,
     `tables: ${tablesType}`,
     storeType,
-    'setTables(tables)',
+    fluentStoreMethod('setTables', 'tables'),
     getStoreContentDoc(1),
   );
-  addMethod(`delTables`, '', storeType, 'delTables()', getStoreContentDoc(3));
+  addMethod(
+    `delTables`,
+    '',
+    storeType,
+    fluentStoreMethod('delTables'),
+    getStoreContentDoc(3),
+  );
   addMethod(
     `getTableIds`,
     '',
     'Ids',
-    'store.getTableIds()',
+    storeMethod('getTableIds'),
     getIdsDoc('Table', THE_STORE),
   );
 
@@ -228,35 +163,35 @@ export const getStoreApi = (
       `has${table}Table`,
       '',
       BOOLEAN,
-      `hasTable(${TABLE_ID})`,
+      storeMethod('hasTable', TABLE_ID),
       getHasDoc(tableDoc),
     );
     addMethod(
       `get${table}Table`,
       '',
       tableType,
-      `getTable(${TABLE_ID})`,
+      storeMethod('getTable', TABLE_ID, tableType),
       getTableContentDoc(),
     );
     addMethod(
       `set${table}Table`,
       `table: ${tableType}`,
       storeType,
-      `setTable(${TABLE_ID}, table)`,
+      fluentStoreMethod('setTable', `${TABLE_ID}, table`),
       getTableContentDoc(1),
     );
     addMethod(
       `del${table}Table`,
       '',
       storeType,
-      `delTable(${TABLE_ID})`,
+      fluentStoreMethod('delTable', TABLE_ID),
       getTableContentDoc(3),
     );
     addMethod(
       `get${table}RowIds`,
       '',
       'Ids',
-      `getRowIds(${TABLE_ID})`,
+      storeMethod('getRowIds', TABLE_ID),
       getIdsDoc('Row', tableDoc),
     );
     addMethod(
@@ -268,7 +203,10 @@ export const getStoreApi = (
         'limit?: number,',
       ],
       'Ids',
-      `getSortedRowIds(${TABLE_ID}, cellId, descending, offset, limit)`,
+      storeMethod(
+        'getSortedRowIds',
+        `${TABLE_ID}, cellId, descending, offset, limit`,
+      ),
       getIdsDoc('Row', tableDoc, 1),
     );
 
@@ -276,49 +214,49 @@ export const getStoreApi = (
       `has${table}Row`,
       'rowId: Id',
       BOOLEAN,
-      `hasRow(${TABLE_ID}, rowId)`,
+      storeMethod('hasRow', `${TABLE_ID}, rowId`),
       getHasDoc(THE_SPECIFIED_ROW, tableDoc),
     );
     addMethod(
       `get${table}Row`,
       'rowId: Id',
       rowType,
-      `getRow(${TABLE_ID}, rowId)`,
+      storeMethod('getRow', `${TABLE_ID}, rowId`, rowType),
       getRowContentDoc(),
     );
     addMethod(
       `set${table}Row`,
       `rowId: Id, row: ${rowWhenSetType}`,
       storeType,
-      `setRow(${TABLE_ID}, rowId, row)`,
+      fluentStoreMethod('setRow', `${TABLE_ID}, rowId, row`),
       getRowContentDoc(1),
     );
     addMethod(
       `add${table}Row`,
       `row: ${rowWhenSetType}`,
       'Id | undefined',
-      `addRow(${TABLE_ID}, row)`,
+      storeMethod('addRow', `${TABLE_ID}, row`),
       `Adds a new Row to ${tableDoc}`,
     );
     addMethod(
       `set${table}PartialRow`,
       `rowId: Id, partialRow: ${rowWhenSetType}`,
       storeType,
-      `setPartialRow(${TABLE_ID}, rowId, partialRow)`,
+      fluentStoreMethod('setPartialRow', `${TABLE_ID}, rowId, partialRow`),
       getRowContentDoc(2),
     );
     addMethod(
       `del${table}Row`,
       `rowId: Id`,
       storeType,
-      `delRow(${TABLE_ID}, rowId)`,
+      fluentStoreMethod('delRow', `${TABLE_ID}, rowId`),
       getRowContentDoc(3),
     );
     addMethod(
       `get${table}CellIds`,
       'rowId: Id',
       'Ids',
-      `getCellIds(${TABLE_ID}, rowId)`,
+      storeMethod('getCellIds', `${TABLE_ID}, rowId`),
       getIdsDoc('Cell', rowDoc),
     );
 
@@ -353,28 +291,33 @@ export const getStoreApi = (
         `has${table}${cell}Cell`,
         'rowId: Id',
         BOOLEAN,
-        `hasCell(${TABLE_ID}, rowId, ${CELL_ID})`,
+        storeMethod('hasCell', `${TABLE_ID}, rowId, ${CELL_ID}`),
         getHasDoc(cellDoc, rowDoc),
       );
+      const returnCellType = `${type}${defaulted ? '' : ' | undefined'}`;
       addMethod(
         `get${table}${cell}Cell`,
         'rowId: Id',
-        `${type}${defaulted ? '' : ' | undefined'}`,
-        `getCell(${TABLE_ID}, rowId, ${CELL_ID})`,
+        returnCellType,
+        storeMethod(
+          'getCell',
+          `${TABLE_ID}, rowId, ${CELL_ID}`,
+          returnCellType,
+        ),
         getCellContentDoc(),
       );
       addMethod(
         `set${table}${cell}Cell`,
         `rowId: Id, cell: ${type}`,
         storeType,
-        `setCell(${TABLE_ID}, rowId, ${CELL_ID}, cell)`,
+        fluentStoreMethod('setCell', `${TABLE_ID}, rowId, ${CELL_ID}, cell`),
         getCellContentDoc(1),
       );
       addMethod(
         `del${table}${cell}Cell`,
         `rowId: Id`,
         storeType,
-        `delCell(${TABLE_ID}, rowId, ${CELL_ID})`,
+        fluentStoreMethod('delCell', `${TABLE_ID}, rowId, ${CELL_ID}`),
         getCellContentDoc(3),
       );
     });
@@ -399,7 +342,7 @@ export const getStoreApi = (
       ') => boolean,',
     ],
     'Return',
-    'store.transaction(actions, doRollback)',
+    storeMethod('transaction', 'actions, doRollback'),
     'Execute a transaction to make multiple mutations',
     '<Return>',
   );
@@ -407,7 +350,7 @@ export const getStoreApi = (
     'startTransaction',
     '',
     storeType,
-    'fluent(() => store.startTransaction())',
+    fluentStoreMethod('startTransaction'),
     'Explicitly starts a transaction',
   );
   addMethod(
@@ -419,7 +362,7 @@ export const getStoreApi = (
       ') => boolean,',
     ],
     storeType,
-    'fluent(() => store.finishTransaction(doRollback))',
+    fluentStoreMethod('finishTransaction', 'doRollback'),
     'Explicitly finishes a transaction',
   );
 
