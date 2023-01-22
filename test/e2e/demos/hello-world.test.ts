ecc46a0349c3648ece980908fdf1e7aada471f1f
James Pearce <github@tripleodeon.com>
2023-01-22 15:15:23 -0500

    [keyvalues] Some docs

--- test/e2e/demos/hello-world.test.ts
+++ test/e2e/demos/hello-world.test.ts
@@ -18,15 +18,21 @@ test('hello-world-v1', async () => {
 test('hello-world-v2', async () => {
   await expectPage(`/demos/hello-world/hello-world-v2/`);
   await expectedElement('h1', 'Hello World v2');
+  await expectedFramedElement('body', 'Hello World');
+});
+
+test('hello-world-v3', async () => {
+  await expectPage(`/demos/hello-world/hello-world-v3/`);
+  await expectedElement('h1', 'Hello World v3');
   await expectedFramedElement(
     'body',
     new Date(new Date().getTime() + 1000).toLocaleTimeString(),
   );
 });
 
-test('hello-world-v3', async () => {
-  await expectPage(`/demos/hello-world/hello-world-v3/`);
-  await expectedElement('h1', 'Hello World v3');
+test('hello-world-v4', async () => {
+  await expectPage(`/demos/hello-world/hello-world-v4/`);
+  await expectedElement('h1', 'Hello World v4');
   await expectedFramedElement(
     'body',
     new Date(new Date().getTime() + 1000).toLocaleTimeString(),
