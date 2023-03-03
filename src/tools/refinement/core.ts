e5e1e34a77ad03038c46e04f821520f8bd04dac2
James Pearce <github@tripleodeon.com>
2023-03-02 23:01:38 -0400

    [tools] Refinement boilerplate 2

--- src/tools/refinement/core.ts
+++ src/tools/refinement/core.ts
@@ -1,9 +1,101 @@
+import {IdMap, mapMap, mapNew} from '../../common/map';
+import {
+  LINE,
+  LINE_TREE,
+  comment,
+  getCodeFunctions,
+  join,
+  mapUnique,
+} from '../common/code';
 import {TablesSchema, ValuesSchema} from '../../store.d';
+import {EMPTY_STRING} from '../../common/strings';
+import {Id} from '../../common.d';
+import {getSchemaFunctions} from '../common/schema';
+import {isUndefined} from '../../common/other';
 
 export const getStoreCoreRefinement = (
-  _tablesSchema: TablesSchema,
-  _valuesSchema: ValuesSchema,
+  tablesSchema: TablesSchema,
+  valuesSchema: ValuesSchema,
   _module: string,
 ): [string] => {
-  return [`export * from 'tinybase';`];
+  const [
+    build,
+    addImport,
+    addType,
+    _addInternalFunction,
+    _addConstant,
+    getImports,
+    getTypes,
+    _getConstants,
+  ] = getCodeFunctions();
+
+  const [mapTablesSchema, mapCellSchema] = getSchemaFunctions(
+    tablesSchema,
+    valuesSchema,
+    () => '',
+  );
+
+  const methods: IdMap<
+    [parameters: LINE, returnType: string, doc: string, generic: string]
+  > = mapNew();
+
+  const getMethods = (): LINE_TREE =>
+    mapMap(methods, ([parameters, returnType, doc, generic], name) => [
+      comment(doc),
+      name + generic + `(${parameters}): ${returnType};`,
+      EMPTY_STRING,
+    ]);
+
+  const addMethod = (
+    name: Id,
+    parameters: LINE,
+    returnType: string,
+    doc: string,
+    generic = EMPTY_STRING,
+  ): Id => mapUnique(methods, name, [parameters, returnType, doc, generic]);
+
+  addImport(0, 'tinybase', 'Id', 'Store as StoreCore');
+
+  addType(
+    'Tables',
+    '{' +
+      join(
+        mapTablesSchema(
+          (tableId) =>
+            `'${tableId}': {[rowId: Id]: {` +
+            join(
+              mapCellSchema(
+                tableId,
+                (cellId, type, defaultValue) =>
+                  `'${cellId}'${
+                    isUndefined(defaultValue) ? '?' : EMPTY_STRING
+                  }: ${type}`,
+              ),
+              '; ',
+            ) +
+            '}}',
+        ),
+        '; ',
+      ) +
+      '}',
+    'Tables type',
+  );
+
+  addMethod('setTables', 'tables: Tables', 'Store', 'Set the tables');
+
+  return [
+    build(
+      `export * from 'tinybase';`,
+      ...getImports(0),
+      ...getTypes(),
+      'interface Refined {',
+      ...getMethods(),
+      '}',
+      EMPTY_STRING,
+      'export type Store = Omit<StoreCore, keyof Refined> & Refined;',
+      EMPTY_STRING,
+      comment(`Creates a Store object`),
+      'export function createStore(): Store',
+    ),
+  ];
 };
