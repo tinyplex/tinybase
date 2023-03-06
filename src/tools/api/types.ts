f0759351ada70b0013f132abf26b7f16a90b9bb4
James Pearce <github@tripleodeon.com>
2023-03-05 23:26:03 -0400

    [tools] Rationalize type generation

--- src/tools/api/types.ts
+++ src/tools/api/types.ts
@@ -1,100 +1,86 @@
-import {Cell, Value} from '../../store.d';
 import {EMPTY_STRING, TABLES, VALUES} from '../../common/strings';
+import {
+  MapCellSchema,
+  MapTablesSchema,
+  MapValuesSchema,
+} from '../common/schema';
 import {WHEN_SET, getTheContentOfTheStoreDoc} from '../common/strings';
 import {getFieldTypeList} from '../common/code';
 import {isUndefined} from '../../common/other';
 
-export const getTablesTypes = (
+export const getTypeFunctions = (
   addType: (name: string, body: string, doc: string) => string,
-  mapTablesSchema: (
-    callback: (tableId: string, tableName: string, TABLE_ID: string) => string,
-  ) => string[],
-  mapCellSchema: (
-    tableId: string,
-    callback: (
-      cellId: string,
-      type: 'string' | 'number' | 'boolean',
-      defaultValue: Cell | undefined,
-      CELL_ID: string,
-      cellName: string,
-    ) => string,
-  ) => string[],
+  mapTablesSchema: MapTablesSchema,
+  mapCellSchema: MapCellSchema,
+  mapValuesSchema: MapValuesSchema,
 ) => {
-  const tablesType = addType(
-    TABLES,
-    getFieldTypeList(
-      ...mapTablesSchema(
-        (tableId) =>
-          `'${tableId}'?: {[rowId: Id]: ` +
-          getFieldTypeList(
-            ...mapCellSchema(
-              tableId,
-              (cellId, type, defaultValue) =>
-                `'${cellId}'${
-                  isUndefined(defaultValue) ? '?' : EMPTY_STRING
-                }: ${type}`,
-            ),
-          ) +
-          '}',
+  const getTablesTypes = () => {
+    const tablesType = addType(
+      TABLES,
+      getFieldTypeList(
+        ...mapTablesSchema(
+          (tableId) =>
+            `'${tableId}'?: {[rowId: Id]: ` +
+            getFieldTypeList(
+              ...mapCellSchema(
+                tableId,
+                (cellId, type, defaultValue) =>
+                  `'${cellId}'${
+                    isUndefined(defaultValue) ? '?' : EMPTY_STRING
+                  }: ${type}`,
+              ),
+            ) +
+            '}',
+        ),
       ),
-    ),
-    getTheContentOfTheStoreDoc(1, 5),
-  );
+      getTheContentOfTheStoreDoc(1, 5),
+    );
 
-  const tablesWhenSetType = addType(
-    TABLES + WHEN_SET,
-    getFieldTypeList(
-      ...mapTablesSchema(
-        (tableId) =>
-          `'${tableId}'?: {[rowId: Id]: ` +
-          getFieldTypeList(
-            ...mapCellSchema(
-              tableId,
-              (cellId, type) => `'${cellId}'?: ${type}`,
-            ),
-          ) +
-          '}',
+    const tablesWhenSetType = addType(
+      TABLES + WHEN_SET,
+      getFieldTypeList(
+        ...mapTablesSchema(
+          (tableId) =>
+            `'${tableId}'?: {[rowId: Id]: ` +
+            getFieldTypeList(
+              ...mapCellSchema(
+                tableId,
+                (cellId, type) => `'${cellId}'?: ${type}`,
+              ),
+            ) +
+            '}',
+        ),
       ),
-    ),
-    getTheContentOfTheStoreDoc(1, 5, 1),
-  );
+      getTheContentOfTheStoreDoc(1, 5, 1),
+    );
 
-  return [tablesType, tablesWhenSetType];
-};
+    return [tablesType, tablesWhenSetType];
+  };
 
-export const getValuesType = (
-  addType: (name: string, body: string, doc: string) => string,
-  mapValuesSchema: (
-    callback: (
-      valueId: string,
-      type: 'string' | 'number' | 'boolean',
-      defaultValue: Value | undefined,
-      VALUE_ID: string,
-      valueName: string,
-    ) => string,
-  ) => string[],
-) => {
-  const valuesType = addType(
-    VALUES,
-    getFieldTypeList(
-      ...mapValuesSchema(
-        (valueId, type, defaultValue) =>
-          `'${valueId}'${
-            isUndefined(defaultValue) ? '?' : EMPTY_STRING
-          }: ${type}`,
+  const getValuesTypes = () => {
+    const valuesType = addType(
+      VALUES,
+      getFieldTypeList(
+        ...mapValuesSchema(
+          (valueId, type, defaultValue) =>
+            `'${valueId}'${
+              isUndefined(defaultValue) ? '?' : EMPTY_STRING
+            }: ${type}`,
+        ),
       ),
-    ),
-    getTheContentOfTheStoreDoc(2, 5),
-  );
+      getTheContentOfTheStoreDoc(2, 5),
+    );
 
-  // ValuesWhenSet
-  const valuesWhenSetType = addType(
-    VALUES + WHEN_SET,
-    getFieldTypeList(
-      ...mapValuesSchema((valueId, type) => `'${valueId}'?: ${type}`),
-    ),
-    getTheContentOfTheStoreDoc(2, 5, 1),
-  );
+    const valuesWhenSetType = addType(
+      VALUES + WHEN_SET,
+      getFieldTypeList(
+        ...mapValuesSchema((valueId, type) => `'${valueId}'?: ${type}`),
+      ),
+      getTheContentOfTheStoreDoc(2, 5, 1),
+    );
 
-  return [valuesType, valuesWhenSetType];
+    return [valuesType, valuesWhenSetType];
+  };
+
+  return [getTablesTypes, getValuesTypes];
 };
