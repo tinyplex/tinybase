531a8a22704e00970bc08babc3bf5920be148f13
James Pearce <github@tripleodeon.com>
2023-04-25 15:26:39 -0400

    [tests] Metrics types

---
+++ test/unit/types/metrics.ts
@@ -0,0 +1,53 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+// NB: an exclamation mark after a line visually indicates an expected TS error
+
+import {createMetrics, createStore} from 'tinybase/debug/with-schemas';
+
+const tablesSchema = {
+  t0: {c0: {type: 'number'}},
+  t1: {c1: {type: 'number'}, c1d: {type: 'string', default: ''}},
+} as const;
+
+const storeWithSchemas = createStore().setSchema(tablesSchema);
+const metricsWithSchema = createMetrics(storeWithSchemas);
+
+const metricsWithNoSchema = createMetrics(createStore());
+metricsWithNoSchema.getStore().getTables().t1;
+metricsWithNoSchema.getStore().getTables().t2;
+
+metricsWithSchema.setMetricDefinition('m1', 't1', 'sum', 'c1');
+metricsWithSchema.setMetricDefinition('m1', 't1', 'sum', (getCell) =>
+  getCell('c1'),
+);
+metricsWithSchema.setMetricDefinition('m1', 't2', 'sum', 'c1'); // !
+metricsWithSchema.setMetricDefinition('m1', 't1', 'sum', 'c2'); // !
+metricsWithSchema.setMetricDefinition(
+  'm1',
+  't1',
+  'sum',
+  (getCell) => getCell('c2'), // !
+);
+
+metricsWithSchema.delMetricDefinition('m1').getStore().getTables().t1;
+metricsWithSchema.delMetricDefinition('m1').getStore().getTables().t2; // !
+
+metricsWithSchema.getStore().getTables().t1;
+metricsWithSchema.getStore().getTables().t2; // !
+
+metricsWithSchema.forEachMetric((_metricId, metric) => {
+  metric as number;
+  metric as string; // !
+});
+
+metricsWithSchema.getTableId('m1') == 't0';
+metricsWithSchema.getTableId('m1') == 't1';
+metricsWithSchema.getTableId('m1') == 't2'; // !
+
+metricsWithSchema.addMetricListener('m1', (metrics) => {
+  metrics.getStore().getTables().t1;
+  metrics.getStore().getTables().t2; // !
+});
+
+metricsWithSchema.delListener('m1').getStore().getTables().t1;
+metricsWithSchema.delListener('m1').getStore().getTables().t2; // !
