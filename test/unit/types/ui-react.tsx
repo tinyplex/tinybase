1407787dec14fc6ac8af8e89a3e878c7f88eb49b
James Pearce <github@tripleodeon.com>
2023-04-27 10:35:33 -0400

    [tests] React types, setters

--- test/unit/types/ui-react.tsx
+++ test/unit/types/ui-react.tsx
@@ -20,22 +20,31 @@ const valuesSchema = {
 } as const;
 
 const {
-  useTable,
-  useStore,
-  useTableIds,
-  useRowIds,
-  useTables,
-  useSortedRowIds,
-  useCreateStore,
-  useRow,
+  useAddRowCallback,
   useCell,
   useCellIds,
-  useValues,
-  useValueIds,
+  useCreateStore,
+  useRow,
+  useRowIds,
+  useSetCellCallback,
+  useSetPartialRowCallback,
+  useSetPartialValuesCallback,
+  useSetRowCallback,
+  useSetTableCallback,
+  useSetTablesCallback,
+  useSetValueCallback,
+  useSetValuesCallback,
+  useSortedRowIds,
+  useStore,
+  useTable,
+  useTableIds,
+  useTables,
   useValue,
+  useValueIds,
+  useValues,
 } = UiReact as UiReact.WithSchemas<[typeof tablesSchema, typeof valuesSchema]>;
 
-const _App = () => {
+const _Getters = () => {
   const storeWithSchemas = useCreateStore(() =>
     createStore().setSchema(tablesSchema, valuesSchema),
   );
@@ -90,4 +99,170 @@ const _App = () => {
   useValue('v1') as number;
   useValue('v1') as string; // !
   useValue('v2'); // !
+};
+
+const _Setters = () => {
+  useSetTablesCallback(
+    (_e, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+      return {t1: {r1: {c1: 1}}};
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetTablesCallback(() => ({t1: {r1: {c2: 1}}})); // !
+
+  useSetTableCallback(
+    't1',
+    (_e, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+      return {r1: {c1: 1}};
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetTableCallback('t1', () => ({r1: {c2: 1}})); // !
+  useSetTableCallback('t2', () => ({r1: {c1: 1}})); // !
+
+  useSetRowCallback(
+    't1',
+    'r1',
+    (_e, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+      return {c1: 1};
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetRowCallback('t1', 'r1', () => ({c2: 1})); // !
+  useSetRowCallback('t2', 'r1', () => ({c1: 1})); // !
+
+  useAddRowCallback(
+    't1',
+    (_e, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+      return {c1: 1};
+    },
+    undefined,
+    undefined,
+    (_rowId, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useAddRowCallback('t1', () => ({c2: 1})); // !
+  useAddRowCallback('t2', () => ({c1: 1})); // !
+
+  useSetPartialRowCallback(
+    't1',
+    'r1',
+    (_e, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+      return {c1: 1};
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetPartialRowCallback('t1', 'r1', () => ({c2: 1})); // !
+  useSetPartialRowCallback('t2', 'r1', () => ({c1: 1})); // !
+
+  useSetCellCallback(
+    't1',
+    'r1',
+    'c1',
+    (_e, store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+      return 1;
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetCellCallback('t1', 'r1', 'c1', () => (cell) => {
+    cell as number;
+    cell as string; // !
+    return 0;
+  }); // !
+  useSetCellCallback('t1', 'r1', 'c1', () => () => ''); // !
+  useSetCellCallback('t1', 'r1', 'c1', () => ''); // !
+  useSetCellCallback('t1', 'r1', 'c2', () => 1); // !
+  useSetCellCallback('t2', 'r1', 'c1', () => 1); // !
+
+  useSetValuesCallback(
+    (_e, store) => {
+      store.getValues().v1;
+      store.getValues().v2; // !
+      return {v1: 1};
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetValuesCallback(() => ({v2: 1})); // !
+
+  useSetPartialValuesCallback(
+    (_e, store) => {
+      store.getValues().v1;
+      store.getValues().v2; // !
+      return {v1: 1};
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetPartialValuesCallback(() => ({v2: 1})); // !
+
+  useSetValueCallback(
+    'v1',
+    (_e, store) => {
+      store.getValues().v1;
+      store.getValues().v2; // !
+      return 1;
+    },
+    undefined,
+    undefined,
+    (store) => {
+      store.getTables().t1;
+      store.getTables().t2; // !
+    },
+  );
+  useSetValueCallback('v1', () => (value) => {
+    value as number;
+    value as string; // !
+    return 0;
+  }); // !
+  useSetValueCallback('v1', () => () => ''); // !
+  useSetValueCallback('v1', () => ''); // !
+  useSetValueCallback('v2', () => 1); // !
 };
