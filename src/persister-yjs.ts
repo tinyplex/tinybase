cf1a418550fbb76096dba56dc80ad1dc7c4b0eb4
James Pearce <github@tripleodeon.com>
2023-05-01 20:24:22 -0400

    [sync] Very basic Yjs connector

---
+++ src/persister-yjs.ts
@@ -0,0 +1,33 @@
+import * as Y from 'yjs';
+import {Callback, Json} from './types/common.d';
+import {Persister} from './types/persisters.d';
+import {Store} from './types/store.d';
+import {createCustomPersister} from './persisters/common';
+
+export const createYjsPersister = (store: Store, yDoc: Y.Doc): Persister => {
+  const map = yDoc.getMap('tinybase/store');
+
+  const getPersisted = async (): Promise<string | null | undefined> =>
+    map.get('json') as string;
+
+  const setPersisted = async (json: Json): Promise<void> => {
+    map.set('json', json);
+  };
+
+  const startListeningToPersisted = (didChange: Callback): Callback => {
+    const observer = () => didChange();
+    map.observe(observer);
+    return observer;
+  };
+
+  const stopListeningToPersisted = (observer: Callback): void =>
+    map.unobserve(observer);
+
+  return createCustomPersister(
+    store,
+    getPersisted,
+    setPersisted,
+    startListeningToPersisted,
+    stopListeningToPersisted,
+  );
+};
