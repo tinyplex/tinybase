1a84d3fd1191780de2b6d2fd7c4e3dd9748c3aed
James Pearce <github@tripleodeon.com>
2023-05-16 21:49:35 -0400

    [yjs] More tests and top-level event

--- test/unit/persister-yjs.test.ts
+++ test/unit/persister-yjs.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable jest/no-conditional-expect */
 import {Persister, Store, createStore} from 'tinybase/debug';
-import {Doc as YDoc, Map as YMap} from 'yjs';
+import {Doc as YDoc, Map as YMap, applyUpdate, encodeStateAsUpdate} from 'yjs';
 import {createYjsPersister} from 'tinybase/debug/persister-yjs';
 
 let doc1: YDoc;
@@ -160,6 +160,86 @@ describe('Two stores, one doc', () => {
     store1.setCell('t1', 'r1', 'c1', 2);
     expect(store2.getContent()).toEqual([{t1: {r1: {c1: 2}}}, {v1: 1}]);
     store1.setValue('v1', 2);
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 2}}}, {v1: 2}]);
+  });
+});
+
+describe('Two stores, two docs', () => {
+  let doc2: YDoc;
+  let store2: Store;
+  let persister2: Persister;
+  beforeEach(() => {
+    doc2 = new YDoc();
+    store2 = createStore();
+    persister2 = createYjsPersister(store2, doc2);
+  });
+
+  test('manual', async () => {
+    store1.setTables({t1: {r1: {c1: 1}}}).setValues({v1: 1});
+    await persister1.save();
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    await persister2.load();
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+  });
+
+  test('autoSave1', async () => {
+    await persister1.startAutoSave();
+    store1.setTables({t1: {r1: {c1: 1}}}).setValues({v1: 1});
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    await persister2.load();
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+  });
+
+  test('autoLoad2', async () => {
+    await persister2.startAutoLoad();
+    store1.setTables({t1: {r1: {c1: 1}}}).setValues({v1: 1});
+    await persister1.save();
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+  });
+
+  test('autoSave1 & autoLoad2', async () => {
+    await persister1.startAutoSave();
+    await persister2.startAutoLoad();
+    store1.setTables({t1: {r1: {c1: 1}}}).setValues({v1: 1});
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+  });
+
+  test('autoSave1 & autoLoad2, complex transactions', async () => {
+    await persister1.startAutoSave();
+    await persister2.startAutoLoad();
+    store1
+      .setTables({t1: {r1: {c1: 1, c2: 2}, r2: {c1: 1}}, t2: {r1: {c1: 1}}})
+      .setValues({v1: 1, v2: 2});
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c1: 1}}, t2: {r1: {c1: 1}}},
+      {v1: 1, v2: 2},
+    ]);
+    store1.delCell('t1', 'r1', 'c2');
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([
+      {t1: {r1: {c1: 1}, r2: {c1: 1}}, t2: {r1: {c1: 1}}},
+      {v1: 1, v2: 2},
+    ]);
+    store1.delRow('t1', 'r2');
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([
+      {t1: {r1: {c1: 1}}, t2: {r1: {c1: 1}}},
+      {v1: 1, v2: 2},
+    ]);
+    store1.delTable('t2');
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 1}}}, {v1: 1, v2: 2}]);
+    store1.delValue('v2');
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+    store1.setCell('t1', 'r1', 'c1', 2);
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
+    expect(store2.getContent()).toEqual([{t1: {r1: {c1: 2}}}, {v1: 1}]);
+    store1.setValue('v1', 2);
+    applyUpdate(doc2, encodeStateAsUpdate(doc1));
     expect(store2.getContent()).toEqual([{t1: {r1: {c1: 2}}}, {v1: 2}]);
   });
 });
