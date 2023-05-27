00ee4e75a14b52a6ec2a204f588e195d2996e19c
James Pearce <github@tripleodeon.com>
2023-05-27 17:01:25 -0400

    [automerge] Boilerplate

---
+++ src/types/with-schemas/persister-automerge.d.ts
@@ -0,0 +1,12 @@
+/// persister-automerge
+
+import {OptionalSchemas, Store} from './store';
+import {DocHandle} from 'automerge-repo';
+import {Persister} from './persisters';
+
+/// createAutomergePersister
+export function createAutomergePersister<Schemas extends OptionalSchemas>(
+  store: Store<Schemas>,
+  docHandle: DocHandle<any>,
+  docMapName?: string,
+): Persister<Schemas>;
