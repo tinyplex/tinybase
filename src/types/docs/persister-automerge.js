6de4e28467dc8dedc5cdc6663da1584857fc430e
James Pearce <github@tripleodeon.com>
2023-06-01 09:56:43 -0400

    [automerge] Docs test

--- src/types/docs/persister-automerge.js
+++ src/types/docs/persister-automerge.js
@@ -27,15 +27,75 @@
  * @example
  * This example creates a Persister object and persists the Store to an
  * Automerge document.
- * @example
- * This more complex example uses Automerge updates to keep two Store objects
- * (each with their own Persister objects and Automerge documents) in sync with
- * each other. We use the `await` keyword extensively for the purpose of
- * ensuring sequentiality in this example.
  *
- * Typically, real-world synchronization would happen between two systems via a
- * Automerge network connection. Here, we synthesize that with the `syncDocs`
- * function.
+ * ```js
+ * const docHandler = new AutomergeRepo.Repo({network: []}).create();
+ * const store = createStore().setTables({pets: {fido: {species: 'dog'}}});
+ * const persister = createAutomergePersister(store, docHandler);
+ *
+ * await persister.save();
+ * // Store JSON will be saved to the document.
+ *
+ * console.log(docHandler.doc);
+ * // -> {tinybase: {t: {pets: {fido: {species: 'dog'}}}, v: {}}}
+ *
+ * persister.destroy();
+ * ```
+ * @example
+ * This more complex example uses Automerge networking to keep two Store objects
+ * (each with their own Persister objects and Automerge documents) in sync with
+ * each other using a network.
+ *
+ * ```js
+ * // Bind the first Store to a network-enabled automerge-repo
+ * const repo1 = new AutomergeRepo.Repo({
+ *   network: [new BroadcastChannelNetworkAdapter()],
+ * });
+ * const docHandler1 = repo1.create()
+ * await docHandler1.value();
+ * const store1 = createStore();
+ * const persister1 = createAutomergePersister(store1, docHandler1);
+ * await persister1.startAutoLoad();
+ * await persister1.startAutoSave();
+ *
+ * // Bind the second Store to a different network-enabled automerge-repo
+ * const repo2 = new AutomergeRepo.Repo({
+ *   network: [new BroadcastChannelNetworkAdapter()],
+ * });
+ * const docHandler2 = repo2.find(docHandler1.documentId);
+ * await docHandler2.value();
+ * const store2 = createStore();
+ * const persister2 = createAutomergePersister(store2, docHandler2);
+ * await persister2.startAutoLoad();
+ * await persister2.startAutoSave();
+ *
+ * // A function that waits briefly and then for the documents to synchronize
+ * // with each other, merely for the purposes of sequentiality in this example.
+ * const syncDocsWait = async () => {
+ *   await new Promise((resolve) => setTimeout(() => resolve(0), 10));
+ *   await docHandler1.value();
+ *   await docHandler2.value();
+ * };
+ *
+ * // Wait for the documents to synchronize in their initial state.
+ * await syncDocsWait();
+ *
+ * // Make a change to each of the two Stores.
+ * store1.setTables({pets: {fido: {species: 'dog'}}});
+ * store2.setValues({open: true});
+ *
+ * // Wait for the documents to synchronize in their new state.
+ * await syncDocsWait();
+ *
+ * // Ensure the Stores are in sync.
+ * console.log(store1.getContent());
+ * // -> [{pets: {fido: {species: 'dog'}}}, {open: true}]
+ * console.log(store2.getContent());
+ * // -> [{pets: {fido: {species: 'dog'}}}, {open: true}]
+ *
+ * persister1.destroy();
+ * persister2.destroy();
+ * ```
  * @category Creation
  * @since v4.0
  */
