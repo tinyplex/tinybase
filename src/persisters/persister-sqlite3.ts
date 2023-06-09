58aa570e68baa61e02bcfbbcb991c626e722d916
James Pearce <github@tripleodeon.com>
2023-06-08 21:29:03 -0400

    [sqlite] Share infrastructure

--- src/persisters/persister-sqlite3.ts
+++ src/persisters/persister-sqlite3.ts
@@ -1,53 +1,29 @@
 import {IdObj, objMap} from '../common/obj';
 import {Persister, PersisterListener} from '../types/persisters';
-import {Store, Tables, Values} from '../types/store';
 import {Database} from 'sqlite3';
+import {Store} from '../types/store';
 import {arrayMap} from '../common/array';
 import {createCustomPersister} from '../persisters';
 import {createSqlite3Persister as createSqlite3PersisterDecl} from '../types/persisters/persister-sqlite3';
-import {jsonString} from '../common/other';
-
-const run = (db: Database, sql: string, args: any[] = []): Promise<void> =>
-  new Promise((resolve, reject) =>
-    db.run(sql, args, (err) => (err ? reject(err) : resolve())),
-  );
-
-const get = (db: Database, sql: string): Promise<any> =>
-  new Promise((resolve) =>
-    db.all(sql, (_, rows: IdObj<any>[]) =>
-      resolve(
-        arrayMap(rows, (row: IdObj<any>) => objMap(row, (value) => value)),
-      ),
-    ),
-  );
-
-const ensureTable = async (db: Database): Promise<void> =>
-  await run(db, 'CREATE TABLE IF NOT EXISTS tinybase(json);');
+import {getSqlitePersistedFunctions} from './common';
+import {promise} from '../common/other';
 
 export const createSqlite3Persister = ((
   store: Store,
   db: Database,
 ): Persister => {
-  const getPersisted = async (): Promise<[Tables, Values]> => {
-    await ensureTable(db);
-    return JSON.parse(
-      (await get(db, 'SELECT json FROM tinybase LIMIT 1'))[0][0],
-    );
-  };
-
-  const setPersisted = async (
-    getContent: () => [Tables, Values],
-  ): Promise<void> => {
-    try {
-      await ensureTable(db);
-      await run(
-        db,
-        'INSERT INTO tinybase(rowId, json) VALUES (1, ?) ON CONFLICT DO ' +
-          'UPDATE SET json=excluded.json',
-        [jsonString(getContent())],
-      );
-    } catch {}
-  };
+  const [getPersisted, setPersisted] = getSqlitePersistedFunctions(
+    (sql: string, args: any[] = []): Promise<void> =>
+      promise((resolve) => db.run(sql, args, () => resolve())),
+    (sql: string): Promise<any[][]> =>
+      promise((resolve) =>
+        db.all(sql, (_, rows: IdObj<any>[]) =>
+          resolve(
+            arrayMap(rows, (row: IdObj<any>) => objMap(row, (value) => value)),
+          ),
+        ),
+      ),
+  );
 
   const addPersisterListener = (listener: PersisterListener): (() => void) => {
     const observer = () => listener();
