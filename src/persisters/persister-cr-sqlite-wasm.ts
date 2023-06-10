f3af498926076743ae19d4d044a01e1babded8af
James Pearce <github@tripleodeon.com>
2023-06-09 20:48:37 -0400

    [sqlite] Fully abstract persisters

--- src/persisters/persister-cr-sqlite-wasm.ts
+++ src/persisters/persister-cr-sqlite-wasm.ts
@@ -6,31 +6,19 @@ import {
 import {DB} from '@vlcn.io/crsqlite-wasm';
 import {Store} from '../types/store';
 import {createCrSqliteWasmPersister as createCrSqliteWasmPersisterDecl} from '../types/persisters/persister-cr-sqlite-wasm';
-import {createCustomPersister} from '../persisters';
-import {getSqlitePersistedFunctions} from './sqlite';
+import {createSqlitePersister} from './sqlite';
 
 export const createCrSqliteWasmPersister = ((
   store: Store,
   db: DB,
   storeTableOrConfig?: string | DatabasePersisterConfig,
-): Persister => {
-  const [getPersisted, setPersisted] = getSqlitePersistedFunctions(
+): Persister =>
+  createSqlitePersister(
+    store,
     storeTableOrConfig,
     async (sql: string, args: any[] = []): Promise<void> => db.exec(sql, args),
     async (sql: string): Promise<any[][]> => await db.execA(sql),
-  );
-
-  const addPersisterListener = (listener: PersisterListener): (() => void) =>
-    db.onUpdate(() => listener());
-
-  const delPersisterListener = (removeListener: () => void): void =>
-    removeListener();
-
-  return createCustomPersister(
-    store,
-    getPersisted,
-    setPersisted,
-    addPersisterListener,
-    delPersisterListener,
-  );
-}) as typeof createCrSqliteWasmPersisterDecl;
+    (listener: PersisterListener): (() => void) =>
+      db.onUpdate(() => listener()),
+    (removeListener: () => void): void => removeListener(),
+  )) as typeof createCrSqliteWasmPersisterDecl;
--- src/persisters/persister-sqlite-wasm.ts
+++ src/persisters/persister-sqlite-wasm.ts
@@ -4,35 +4,23 @@ import {
   PersisterListener,
 } from '../types/persisters';
 import {Store} from '../types/store';
-import {createCustomPersister} from '../persisters';
+import {createSqlitePersister} from './sqlite';
 import {createSqliteWasmPersister as createSqliteWasmPersisterDecl} from '../types/persisters/persister-sqlite-wasm';
-import {getSqlitePersistedFunctions} from './sqlite';
 
 export const createSqliteWasmPersister = ((
   store: Store,
   sqlite3: any,
   db: any,
   storeTableOrConfig?: string | DatabasePersisterConfig,
-): Persister => {
-  const [getPersisted, setPersisted] = getSqlitePersistedFunctions(
+): Persister =>
+  createSqlitePersister(
+    store,
     storeTableOrConfig,
     async (sql: string, args: any[] = []): Promise<void> =>
       db.exec(sql, {bind: args}),
     async (sql: string): Promise<any[][]> =>
       db.exec(sql, {returnValue: 'resultRows'}),
-  );
-
-  const addPersisterListener = (listener: PersisterListener): void =>
-    sqlite3.capi.sqlite3_update_hook(db, () => listener(), 0);
-
-  const delPersisterListener = (): void =>
-    sqlite3.capi.sqlite3_update_hook(db, () => 0, 0);
-
-  return createCustomPersister(
-    store,
-    getPersisted,
-    setPersisted,
-    addPersisterListener,
-    delPersisterListener,
-  );
-}) as typeof createSqliteWasmPersisterDecl;
+    (listener: PersisterListener): void =>
+      sqlite3.capi.sqlite3_update_hook(db, () => listener(), 0),
+    (): void => sqlite3.capi.sqlite3_update_hook(db, () => 0, 0),
+  )) as typeof createSqliteWasmPersisterDecl;
