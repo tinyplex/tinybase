8db6e86d3233c00e2fe042a4a283388811c7ce32
James Pearce <github@tripleodeon.com>
2023-06-11 11:50:28 -0400

    [tests] Refactor sqlite tests

---
+++ test/unit/persisters/sqlite.ts
@@ -0,0 +1,54 @@
+export type Dump = [
+  name: string,
+  sql: string,
+  rows: {[column: string]: any}[],
+][];
+
+const escapeId = (str: string) => `"${str.replace(/"/g, '""')}"`;
+
+export const getDatabaseFunctions = <Database>(
+  cmd: (
+    db: Database,
+    sql: string,
+    args?: any[],
+  ) => Promise<{[id: string]: any}[]>,
+): [(db: Database) => Promise<Dump>, (db: Database, dump: Dump) => void] => {
+  const getDatabase = async (db: Database): Promise<Dump> =>
+    await Promise.all(
+      (
+        await cmd(
+          db,
+          `SELECT sql, name FROM sqlite_schema WHERE type = 'table'`,
+        )
+      ).map(async ({sql, name}: any) => [
+        name,
+        sql,
+        await cmd(db, 'SELECT * FROM ' + escapeId(name)),
+      ]),
+    );
+
+  const setDatabase = async (db: Database, dump: Dump) =>
+    dump.forEach(async ([name, sql, rows]) => {
+      await cmd(db, sql);
+      await Promise.all(
+        rows.map(
+          async (row) =>
+            await cmd(
+              db,
+              'INSERT INTO ' +
+                name +
+                '(' +
+                Object.keys(row).join(',') +
+                ') VALUES (' +
+                Object.keys(row)
+                  .map(() => '?')
+                  .join(',') +
+                ')',
+              Object.values(row),
+            ),
+        ),
+      );
+    });
+
+  return [getDatabase, setDatabase];
+};
