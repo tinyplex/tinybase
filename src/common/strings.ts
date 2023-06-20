569d552a269748b118d7857b2f4211d724decb84
James Pearce <github@tripleodeon.com>
2023-06-20 09:33:47 -0400

    [sqlite] Some string shortening

--- src/common/strings.ts
+++ src/common/strings.ts
@@ -5,6 +5,7 @@ export const getTypeOf = (thing: unknown): string => typeof thing;
 export const TINYBASE = 'tinybase';
 export const EMPTY_STRING = '';
 export const STAR = '*';
+export const COMMA = ',';
 
 export const STRING = getTypeOf(EMPTY_STRING);
 export const BOOLEAN = getTypeOf(true);
