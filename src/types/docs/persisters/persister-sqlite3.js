41e0df06cd84730ba86fa3c8f6ece5949a6f5abb
James Pearce <github@tripleodeon.com>
2023-06-23 16:16:27 -0400

    [docs] Add sqlite3 doc tests

--- src/types/docs/persisters/persister-sqlite3.js
+++ src/types/docs/persisters/persister-sqlite3.js
@@ -13,25 +13,86 @@
  *
  * As well as providing a reference to the Store to persist, you must provide a
  * `db` parameter which identifies the database instance.
+ *
+ * A database Persister uses one of two modes: either a JSON serialization of
+ * the whole Store stored in a single row of a table (the default), or a tabular
+ * mapping of Table Ids to database table names and vice-versa).
+ *
+ * The third argument is a DatabasePersisterConfig object that configures which
+ * of those modes to use, and settings for each. If the third argument is simply
+ * a string, it is used as the `storeTableName` property of the JSON
+ * serialization.
+ *
+ * See the documentation for the DpcJson and DpcTabular types for more
+ * information on how both of those modes can be configured.
  * @param store The Store to persist.
  * @param db The database instance that was returned from `new
  * sqlite3.Database(...)`.
+ * @param configOrStoreTableName A DatabasePersisterConfig to configure the
+ * persistence mode (or a string to set the `storeTableName` property of the
+ * JSON serialization).
  * @returns A reference to the new Persister object.
  * @example
  * This example creates a Persister object and persists the Store to a local
- * SQLite database.
+ * SQLite database as a JSON serialization into the `my_tinybase` table. It
+ * makes a change to the database directly and then reloads it back into the
+ * Store.
  *
- * ```js yolo
- * const db = new sqlite3.Database('/db.sqlite3');
- *
+ * ```js
+ * const db = new sqlite3.Database(':memory:');
  * const store = createStore().setTables({pets: {fido: {species: 'dog'}}});
- * const persister = createSqlite3Persister(store, db);
+ * const persister = createSqlite3Persister(store, db, 'my_tinybase');
  *
  * await persister.save();
- * // Store JSON will be saved to the database.
+ * console.log(
+ *   await new Promise((resolve) =>
+ *     db.all('SELECT * FROM my_tinybase;', (_, rows) => resolve(rows)),
+ *   ),
+ * );
+ * // -> [{_id: '_', store: '[{"pets":{"fido":{"species":"dog"}}},{}]'}]
  *
+ * await new Promise((resolve) =>
+ *   db.all(
+ *     'UPDATE my_tinybase SET store = ' +
+ *       `'[{"pets":{"felix":{"species":"cat"}}},{}]' WHERE _id = '_';`,
+ *     resolve,
+ *   ),
+ * );
  * await persister.load();
- * // Store JSON will be loaded from the database.
+ * console.log(store.getTables());
+ * // -> {pets: {felix: {species: 'cat'}}}
+ *
+ * persister.destroy();
+ * ```
+ * @example
+ * This example creates a Persister object and persists the Store to a local
+ * SQLite database with tabular mapping.
+ *
+ * ```js
+ * const db = new sqlite3.Database(':memory:');
+ * const store = createStore().setTables({pets: {fido: {species: 'dog'}}});
+ * const persister = createSqlite3Persister(store, db, {
+ *   mode: 'tabular',
+ *   tables: {load: {pets: 'pets'}, save: {pets: 'pets'}},
+ * });
+ *
+ * await persister.save();
+ * console.log(
+ *   await new Promise((resolve) =>
+ *     db.all('SELECT * FROM pets;', (_, rows) => resolve(rows)),
+ *   ),
+ * );
+ * // -> [{_id: 'fido', species: 'dog'}]
+ *
+ * await new Promise((resolve) =>
+ *   db.all(
+ *     `INSERT INTO pets (_id, species) VALUES ('felix', 'cat')`,
+ *     resolve,
+ *   ),
+ * );
+ * await persister.load();
+ * console.log(store.getTables());
+ * // -> {pets: {fido: {species: 'dog'}, felix: {species: 'cat'}}}
  *
  * persister.destroy();
  * ```
