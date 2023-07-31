ee1df0df1d6da74fe69aa5ab51aa85cfe29aa876
James Pearce <github@tripleodeon.com>
2023-07-31 11:50:56 -0400

    [e2e] Word frequencies test

---
+++ test/e2e/demos/word-frequencies.test.ts
@@ -0,0 +1,40 @@
+import {
+  expectNoFramedElement,
+  expectedElement,
+  expectedFramedElement,
+  getServerFunctions,
+} from '../common';
+
+const [startServer, stopServer, expectPage] = getServerFunctions(8801);
+
+beforeAll(startServer);
+afterAll(stopServer);
+
+test('word-frequencies', async () => {
+  await expectPage(`/demos/word-frequencies`);
+  await expectedElement('h1', 'Word Frequencies');
+  await (await expectedFramedElement('input')).type('z');
+  await expectedFramedElement('.result', 'zone');
+  await expectedFramedElement('.result', 'zero');
+  await expectedFramedElement('.result', 'zoo');
+  await expectedFramedElement('.result', 'zip');
+  await expectedFramedElement('.result', 'zoom');
+  await expectedFramedElement('.result', 'zinc');
+  await expectedFramedElement('.result', 'zeal');
+  await expectedFramedElement('.result', 'zygote');
+  await (await expectedFramedElement('input')).type('o');
+  await expectedFramedElement('.result', 'zone');
+  await expectNoFramedElement('.result', 'zero');
+  await expectedFramedElement('.result', 'zoo');
+  await expectNoFramedElement('.result', 'zip');
+  await expectedFramedElement('.result', 'zoom');
+  await expectNoFramedElement('.result', 'zinc');
+  await expectNoFramedElement('.result', 'zeal');
+  await expectNoFramedElement('.result', 'zygote');
+  await (await expectedFramedElement('input')).type('n');
+  await expectedFramedElement('.result', 'zone');
+  await expectNoFramedElement('.result', 'zoo');
+  await expectNoFramedElement('.result', 'zoom');
+  await (await expectedFramedElement('input')).type('k');
+  await expectNoFramedElement('.result');
+});
