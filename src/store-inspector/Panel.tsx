b356a48eeaa4b51bb3b1149ee2e58a3274d6abb0
James Pearce <github@tripleodeon.com>
2023-08-01 17:12:11 -0400

    [inspector] Table styling

--- src/store-inspector/Panel.tsx
+++ src/store-inspector/Panel.tsx
@@ -1,18 +1,29 @@
 /** @jsx createElement */
 
+import {
+  BOTTOM_LEFT,
+  HORIZONTAL_PANEL,
+  TOP_RIGHT,
+  VERTICAL_PANEL,
+} from './style';
 import {useOpen, usePosition} from './hooks';
 import {Body} from './Body';
 import {Header} from './Header';
 import {StoreProp} from './types';
 import {createElement} from '../ui-react/common';
-import {getPanelStyle} from './style';
+import {objMerge} from '../common/obj';
 
 export const Panel = ({s: store}: StoreProp) => {
   const position = usePosition(store);
   const open = useOpen(store);
 
   return open ? (
-    <main style={getPanelStyle(position)}>
+    <main
+      style={objMerge(
+        [TOP_RIGHT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_LEFT][position],
+        [HORIZONTAL_PANEL, VERTICAL_PANEL][position % 2],
+      )}
+    >
       <Header s={store} />
       <Body s={store} />
     </main>
