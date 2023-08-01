4db9df81c1817ae9e0d3080e215d258087d4f053
James Pearce <github@tripleodeon.com>
2023-08-01 14:24:15 -0400

    [inspector] Style rethink

--- src/store-inspector/common.ts
+++ src/store-inspector/common.ts
@@ -1,2 +1,3 @@
+export const UNIQUE_ID = 'tinybaseStoreInspector';
 export const TITLE = 'TinyBase Store Inspector';
 export const POSITIONS = ['top', 'right', 'bottom', 'left'];
--- src/store-inspector/style.ts
+++ src/store-inspector/style.ts
@@ -1,54 +1,42 @@
+import {CLOSE_SVG, LOGO_SVG, POSITIONS_SVG} from './svg';
+/* eslint-disable max-len */
 import {Position, Style} from './types';
-import {objMerge} from '../common/obj';
+import {objMap, objMerge} from '../common/obj';
+import {UNIQUE_ID} from './common';
+import {arrayJoin} from '../common/array';
 
-const FIXED = {position: 'fixed'};
-const PALETTE = {background: '#111d', color: '#fff'};
-const FLEX = {display: 'flex'};
-const PADDING = {padding: '0.25rem'};
-
-const BUTTON_STYLE = {width: '1rem', height: '1rem'};
-const NUB_SQUARE = {width: '1.5rem', height: '1.5rem', padding: '0.25rem'};
 const HORIZONTAL_PANEL = {width: '100vw', height: '30vh'};
 const VERTICAL_PANEL = {width: '30vw', height: '100vh'};
 
-const TOP_RIGHT = {top: 0, right: 0};
-const BOTTOM_RIGHT = {bottom: 0, right: 0};
-const BOTTOM_LEFT = {bottom: 0, left: 0};
+export const TOP_RIGHT = {top: 0, right: 0};
+export const BOTTOM_RIGHT = {bottom: 0, right: 0};
+export const BOTTOM_LEFT = {bottom: 0, left: 0};
 
-export const APP_STYLE: Style = objMerge(
-  {all: 'initial', fontFamily: 'arial,sans-serif', fontSize: '0.75rem'},
-  FIXED,
+export const APP_STYLESHEET = arrayJoin(
+  objMap(
+    {
+      '': 'all:initial;font-family:sans-serif;font-size:0.75rem;position:fixed',
+      '*': 'all:revert',
+      img: 'width:1rem;height:1rem;background:#111;border:0',
+      '>img': `position:fixed;width:1.5rem;height:1.5rem;padding:0.25rem;${LOGO_SVG}`, // Nub
+      main: 'position:fixed;display:flex;flex-direction:column;background:#111d;color:#fff', // Panel
+      header: 'display:flex;padding:0.25rem;background:#000;align-items:center', // Header
+      'header>img:nth-of-type(1)': LOGO_SVG,
+      'header>img[data-id="0"]': POSITIONS_SVG[0],
+      'header>img[data-id="1"]': POSITIONS_SVG[1],
+      'header>img[data-id="2"]': POSITIONS_SVG[2],
+      'header>img[data-id="3"]': POSITIONS_SVG[3],
+      'header>img:nth-of-type(5)': CLOSE_SVG,
+      'header>span':
+        'flex:1;overflow:hidden;white-space:nowrap;text-overflow:ellipsis;margin-left:0.25rem',
+      article: 'padding:0.25rem;overflow:auto', // Body
+    },
+    (style, selector) => `#${UNIQUE_ID} ${selector}{${style}}`,
+  ),
 );
-export const HEADER_STYLE: Style = objMerge(FLEX, PADDING, {
-  background: '#000',
-  alignItems: 'center',
-});
-export const TITLE_STYLE: Style = objMerge({
-  flex: 1,
-  overflow: 'hidden',
-  whiteSpace: 'nowrap',
-  textOverflow: 'ellipsis',
-  marginLeft: '0.25rem',
-});
-export const BODY_STYLE: Style = objMerge(PADDING, {overflow: 'auto'});
-
-export const getNubStyle = (position: Position): Style =>
-  objMerge(
-    FIXED,
-    PALETTE,
-    NUB_SQUARE,
-    [TOP_RIGHT, BOTTOM_RIGHT, BOTTOM_RIGHT, BOTTOM_LEFT][position],
-  );
-
-export const getButtonStyle = (style: Style = {}): Style =>
-  objMerge(BUTTON_STYLE, style);
 
 export const getPanelStyle = (position: Position): Style =>
   objMerge(
-    FIXED,
-    PALETTE,
-    FLEX,
-    {flexDirection: 'column'},
     [TOP_RIGHT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_LEFT][position],
     [HORIZONTAL_PANEL, VERTICAL_PANEL][position % 2],
   );
