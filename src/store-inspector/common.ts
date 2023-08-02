1c6668e4ef3dcf665b4d19b329f20b9e51eeeb3b
James Pearce <github@tripleodeon.com>
2023-08-02 10:57:53 -0400

    [inspector] Common details

--- src/store-inspector/common.ts
+++ src/store-inspector/common.ts
@@ -1,7 +1,4 @@
-import {useCell, useSetCellCallback} from '../ui-react';
-import {CURRENT_TARGET} from '../common/strings';
 import {Id} from '../types/common';
-import {Store} from '../types/store';
 import {jsonString} from '../common/json';
 
 export const UNIQUE_ID = 'tinybaseStoreInspector';
@@ -9,20 +6,3 @@ export const TITLE = 'TinyBase Store Inspector';
 export const POSITIONS = ['top', 'right', 'bottom', 'left'];
 
 export const getUniqueId = (...args: (Id | undefined)[]) => jsonString(args);
-
-export const useOpen = (
-  thingType: Id,
-  thingId: Id,
-  inspectorStore: Store,
-): [boolean, (event: React.SyntheticEvent<HTMLDetailsElement>) => void] => [
-  !!useCell(thingType, thingId, 'open', inspectorStore),
-  useSetCellCallback(
-    thingType,
-    thingId,
-    'open',
-    (event: React.SyntheticEvent<HTMLDetailsElement>) =>
-      event[CURRENT_TARGET].open,
-    [],
-    inspectorStore,
-  ),
-];
