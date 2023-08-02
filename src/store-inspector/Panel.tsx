e931c0e0eae45b9e4b05c290cfa5814ab497ccd9
James Pearce <github@tripleodeon.com>
2023-08-02 11:19:38 -0400

    [inspector] Full screen

--- src/store-inspector/Panel.tsx
+++ src/store-inspector/Panel.tsx
@@ -1,29 +1,17 @@
 /** @jsx createElement */
 
-import {
-  BOTTOM_LEFT,
-  HORIZONTAL_PANEL,
-  TOP_RIGHT,
-  VERTICAL_PANEL,
-} from './style';
 import {useOpen, usePosition} from './hooks';
 import {Body} from './Body';
 import {Header} from './Header';
 import {StoreProp} from './types';
 import {createElement} from '../ui-react/common';
-import {objMerge} from '../common/obj';
 
 export const Panel = ({s}: StoreProp) => {
   const position = usePosition(s);
   const open = useOpen(s);
 
   return open ? (
-    <main
-      style={objMerge(
-        [TOP_RIGHT, TOP_RIGHT, BOTTOM_LEFT, BOTTOM_LEFT][position],
-        [HORIZONTAL_PANEL, VERTICAL_PANEL][position % 2],
-      )}
-    >
+    <main data-position={position}>
       <Header s={s} />
       <Body s={s} />
     </main>
