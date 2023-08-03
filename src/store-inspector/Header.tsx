291aa1f1a46b4cb4bf08d7462844504ae8950293
James Pearce <github@tripleodeon.com>
2023-08-02 23:55:57 -0400

    [inspector] Tidy up hooks

--- src/store-inspector/Header.tsx
+++ src/store-inspector/Header.tsx
@@ -1,16 +1,14 @@
 /** @jsx createElement */
 
 import {OPEN_VALUE, POSITIONS, POSITION_VALUE, TITLE} from './common';
+import {useSetValueCallback, useValue} from '../ui-react';
 import {CURRENT_TARGET} from '../common/strings';
 import {StoreProp} from './types';
 import {arrayMap} from '../common/array';
 import {createElement} from '../ui-react/common';
-import {usePosition} from './hooks';
-import {useSetValueCallback} from '../ui-react';
 
 export const Header = ({s}: StoreProp) => {
-  const position = usePosition(s);
-
+  const position = useValue(POSITION_VALUE, s) ?? 1;
   const handleClose = useSetValueCallback(OPEN_VALUE, () => false, [], s);
   const handleDock = useSetValueCallback(
     POSITION_VALUE,
