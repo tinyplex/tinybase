869ad03af828fad1c70633017f8b75dab98e2743
James Pearce <github@tripleodeon.com>
2023-08-03 10:55:19 -0400

    [store] Better sorting of sparse columns

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,4 @@
 import {SortKey} from './types/common.d';
 
 export const defaultSorter = (sortKey1: SortKey, sortKey2: SortKey): number =>
-  sortKey1 < sortKey2 ? -1 : 1;
+  (sortKey1 ?? 0) < (sortKey2 ?? 0) ? -1 : 1;
