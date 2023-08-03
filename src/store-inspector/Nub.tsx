d00f8b0f97200ab447a9b01c5f6245e1a262a8ef
James Pearce <github@tripleodeon.com>
2023-08-02 23:46:50 -0400

    [inspector] String reuse

--- src/store-inspector/Nub.tsx
+++ src/store-inspector/Nub.tsx
@@ -1,8 +1,8 @@
 /** @jsx createElement */
 
+import {OPEN_VALUE, TITLE} from './common';
 import {useOpen, usePosition} from './hooks';
 import {StoreProp} from './types';
-import {TITLE} from './common';
 import {createElement} from '../ui-react/common';
 import {useSetValueCallback} from '../ui-react';
 
@@ -10,7 +10,7 @@ export const Nub = ({s}: StoreProp) => {
   const position = usePosition(s);
   const open = useOpen(s);
 
-  const handleOpen = useSetValueCallback('open', () => true, [], s);
+  const handleOpen = useSetValueCallback(OPEN_VALUE, () => true, [], s);
   return open ? null : (
     <img onClick={handleOpen} title={TITLE} data-position={position} />
   );
