80c00d30fd662e8b3253257bc9a1cd01d260fdbf
James Pearce <github@tripleodeon.com>
2023-08-04 15:38:18 -0400

    [inspector] Metrics

--- src/store-inspector/Body.tsx
+++ src/store-inspector/Body.tsx
@@ -1,16 +1,21 @@
 /** @jsx createElement */
 
+import {useMetricsIds, useStoreIds} from '../ui-react';
+import {MetricsView} from './MetricsView';
 import {StoreProp} from './types';
 import {StoreView} from './StoreView';
 import {arrayMap} from '../common/array';
 import {createElement} from '../ui-react/common';
-import {useStoreIds} from '../ui-react';
 
 export const Body = (props: StoreProp) => (
   <article>
     <StoreView {...props} />
     {arrayMap(useStoreIds(), (storeId) => (
       <StoreView storeId={storeId} key={storeId} {...props} />
+    ))}
+    <MetricsView {...props} />
+    {arrayMap(useMetricsIds(), (metricsId) => (
+      <MetricsView metricsId={metricsId} key={metricsId} {...props} />
     ))}
   </article>
 );
---
+++ src/store-inspector/MetricsView.tsx
@@ -0,0 +1,50 @@
+/** @jsx createElement */
+
+import {arrayIsEmpty, arrayMap} from '../common/array';
+import {useMetric, useMetricIds, useMetrics} from '../ui-react';
+import {DEFAULT} from '../common/strings';
+import {Details} from './Details';
+import {Id} from '../types/common';
+import {MetricProps} from '../types/ui-react';
+import {StoreProp} from './types';
+import {createElement} from '../ui-react/common';
+import {getUniqueId} from './common';
+import {isUndefined} from '../common/other';
+
+const MetricRow = ({metrics, metricId}: MetricProps) => (
+  <tr>
+    <th>{metricId}</th>
+    <td>{useMetric(metricId, metrics)}</td>
+  </tr>
+);
+
+export const MetricsView = ({
+  metricsId,
+  s,
+}: {readonly metricsId?: Id} & StoreProp) => {
+  const metrics = useMetrics(metricsId);
+  const metricIds = useMetricIds(metrics);
+  return isUndefined(metrics) ? null : (
+    <Details
+      uniqueId={getUniqueId('m', metricsId)}
+      summary={'Metrics: ' + (metricsId ?? DEFAULT)}
+      s={s}
+    >
+      {arrayIsEmpty(metricIds) ? (
+        'No metrics defined'
+      ) : (
+        <table>
+          <thead>
+            <th>Id</th>
+            <th>Metric</th>
+          </thead>
+          <tbody>
+            {arrayMap(metricIds, (metricId) => (
+              <MetricRow metrics={metrics} metricId={metricId} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </Details>
+  );
+};
