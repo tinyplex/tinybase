2d020f4b008096ac0076ef3db994206ad4a83198
James Pearce <github@tripleodeon.com>
2023-08-06 11:54:25 -0400

    [inspector] Scroll value names

--- src/store-inspector/common.ts
+++ src/store-inspector/common.ts
@@ -11,8 +11,6 @@ export const SORT_CELL = 'sort';
 export const OPEN_CELL = 'open';
 export const POSITION_VALUE = 'position';
 export const OPEN_VALUE = OPEN_CELL;
-export const SCROLL_X_VALUE = 'scrollX';
-export const SCROLL_Y_VALUE = 'scrollY';
 
 export const getUniqueId = (...args: (Id | undefined)[]) => jsonString(args);
 
