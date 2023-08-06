d096226b9955eb69e0910b66d41e42ea8af69a52
James Pearce <github@tripleodeon.com>
2023-08-06 11:54:25 -0400

    [inspector] Scroll value names

--- src/store-inspector/Body.tsx
+++ src/store-inspector/Body.tsx
@@ -1,7 +1,11 @@
 /** @jsx createElement */
 
-import {SCROLL_X_VALUE, SCROLL_Y_VALUE} from './common';
-import {useIndexesIds, useMetricsIds, useStoreIds, useValue} from '../ui-react';
+import {
+  useIndexesIds,
+  useMetricsIds,
+  useStoreIds,
+  useValues,
+} from '../ui-react';
 import {CURRENT_TARGET} from '../common/strings';
 import {IndexesView} from './IndexesView';
 import {MetricsView} from './MetricsView';
@@ -17,12 +21,11 @@ export const Body = ({s}: StoreProp) => {
   const ref = useRef<HTMLElement>(null);
   const article = ref.current;
   const [scrolled, setScrolled] = useState(false);
-  const scrollX = (useValue(SCROLL_X_VALUE, s) as number) ?? 0;
-  const scrollY = (useValue(SCROLL_Y_VALUE, s) as number) ?? 0;
+  const {scrollLeft, scrollTop} = useValues(s);
 
   if (article && !scrolled) {
     new MutationObserver((_, observer) => {
-      article.scrollTo(scrollX, scrollY);
+      article.scrollTo((scrollLeft as number) ?? 0, (scrollTop as number) ?? 0);
       setScrolled(true);
       observer.disconnect();
     }).observe(article, {childList: true, subtree: true});
@@ -30,13 +33,8 @@ export const Body = ({s}: StoreProp) => {
 
   const handleScroll = useCallback(
     (event: React.SyntheticEvent<HTMLElement>) => {
-      const target = event[CURRENT_TARGET];
-      requestIdleCallback(() =>
-        s.setPartialValues({
-          [SCROLL_X_VALUE]: target.scrollLeft,
-          [SCROLL_Y_VALUE]: target.scrollTop,
-        }),
-      );
+      const {scrollLeft, scrollTop} = event[CURRENT_TARGET];
+      requestIdleCallback(() => s.setPartialValues({scrollLeft, scrollTop}));
     },
     [s],
   );
