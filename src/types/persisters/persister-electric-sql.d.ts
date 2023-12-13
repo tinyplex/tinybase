3169c58c6de3a6b167b45fed632e52f86b6dc32c
James Pearce <github@tripleodeon.com>
2023-12-13 13:10:26 -0400

    [electric-sql] Types

---
+++ src/types/persisters/persister-electric-sql.d.ts
@@ -0,0 +1,20 @@
+/// persister-electric-sql
+
+import {DatabasePersisterConfig, Persister} from '../persisters';
+import {ElectricClient} from 'electric-sql/client/model';
+import {Store} from '../store';
+
+/// ElectricSqlPersister
+export interface ElectricSqlPersister extends Persister {
+  /// ElectricSqlPersister.getElectricClient
+  getElectricClient: () => ElectricClient<any>;
+}
+
+/// createElectricSqlPersister
+export function createElectricSqlPersister(
+  store: Store,
+  electricClient: ElectricClient<any>,
+  configOrStoreTableName?: DatabasePersisterConfig | string,
+  onSqlCommand?: (sql: string, args?: any[]) => void,
+  onIgnoredError?: (error: any) => void,
+): ElectricSqlPersister;
---
+++ src/types/with-schemas/persisters/persister-electric-sql.d.ts
@@ -0,0 +1,21 @@
+/// persister-electric-sql
+
+import {DatabasePersisterConfig, Persister} from '../persisters';
+import {OptionalSchemas, Store} from '../store';
+import {ElectricClient} from 'electric-sql/client/model';
+
+/// ElectricSqlPersister
+export interface ElectricSqlPersister<Schemas extends OptionalSchemas>
+  extends Persister<Schemas> {
+  /// ElectricSqlPersister.getElectricClient
+  getElectricClient: () => ElectricClient<any>;
+}
+
+/// createElectricSqlPersister
+export function createElectricSqlPersister<Schemas extends OptionalSchemas>(
+  store: Store<Schemas>,
+  electricClient: ElectricClient<any>,
+  configOrStoreTableName?: DatabasePersisterConfig<Schemas> | string,
+  onSqlCommand?: (sql: string, args?: any[]) => void,
+  onIgnoredError?: (error: any) => void,
+): ElectricSqlPersister<Schemas>;
