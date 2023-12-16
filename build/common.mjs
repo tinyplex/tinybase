7c8530e49e6e8fda1682b41000784015db54e7c1
James Pearce <github@tripleodeon.com>
2023-12-16 13:57:41 -0400

    [types] Tweak

--- build/common.mjs
+++ build/common.mjs
@@ -496,7 +496,7 @@ export const compileDocsAndAssets = async (api = true, pages = true) => {
   await makeDir(TMP_DIR);
   await esbuild.build({
     entryPoints: ['site/build.ts'],
-    external: ['tinydocs', 'react', 'yjs', '@prettier/sync'],
+    external: ['tinydocs', 'react', '@prettier/sync'],
     target: 'esnext',
     bundle: true,
     outfile: './tmp/build.js',
@@ -519,7 +519,7 @@ export const compileDocsAndAssets = async (api = true, pages = true) => {
   });
 
   const {build} = await import('../tmp/build.js');
-  build(DOCS_DIR, api, pages);
+  await build(DOCS_DIR, api, pages);
   await removeDir(TMP_DIR);
 };
 
