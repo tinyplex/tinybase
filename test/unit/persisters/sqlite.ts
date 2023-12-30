eef098b30010773c093bbb64d400b64465afb882
James Pearce <github@tripleodeon.com>
2023-12-30 15:23:16 -0400

    [electric-sql] Test variant

--- test/unit/persisters/sqlite.ts
+++ test/unit/persisters/sqlite.ts
@@ -1,13 +1,21 @@
+import 'fake-indexeddb/auto';
 import {DatabasePersisterConfig, Persister, Store} from 'tinybase/debug';
+import {DbSchema, ElectricClient} from 'electric-sql/client/model';
+import {ElectricDatabase, electrify} from 'electric-sql/wa-sqlite';
 import initWasm, {DB} from '@vlcn.io/crsqlite-wasm';
 import sqlite3, {Database} from 'sqlite3';
 import {createCrSqliteWasmPersister} from 'tinybase/debug/persisters/persister-cr-sqlite-wasm';
+import {createElectricSqlPersister} from 'tinybase/debug/persisters/persister-electric-sql';
 import {createSqlite3Persister} from 'tinybase/debug/persisters/persister-sqlite3';
 import {createSqliteWasmPersister} from 'tinybase/debug/persisters/persister-sqlite-wasm';
+import {insecureAuthToken} from 'electric-sql/auth';
 import sqlite3InitModule from '@sqlite.org/sqlite-wasm';
 import {suppressWarnings} from '../common/other';
 
 export type SqliteWasmDb = [sqlite3: any, db: any];
+
+const electricSchema = new DbSchema({}, []);
+type Electric = ElectricClient<typeof electricSchema>;
 
 type Dump = {[name: string]: [sql: string, rows: {[column: string]: any}[]]};
 
@@ -27,11 +35,39 @@ type SqliteVariant<Database> = [
     args?: any[],
   ) => Promise<{[id: string]: any}[]>,
   close: (db: Database) => Promise<void>,
+  autoLoadInterval?: number,
 ];
 
 const escapeId = (str: string) => `"${str.replace(/"/g, '""')}"`;
 
 export const VARIANTS: {[name: string]: SqliteVariant<any>} = {
+  electricSql: [
+    async (): Promise<Electric> =>
+      await suppressWarnings(async () => {
+        const config = {auth: {token: insecureAuthToken({user_id: 'alice'})}};
+        const conn = await ElectricDatabase.init(':memory:', '');
+        return await electrify(conn, electricSchema, config);
+      }),
+    ['getDb', (electric: Electric) => electric],
+    (
+      store: Store,
+      electric: Electric,
+      storeTableOrConfig?: string | DatabasePersisterConfig,
+      onSqlCommand?: (sql: string, args?: any[]) => void,
+      onIgnoredError?: (error: any) => void,
+    ) =>
+      (createElectricSqlPersister as any)(
+        store,
+        electric,
+        storeTableOrConfig,
+        onSqlCommand,
+        onIgnoredError,
+      ),
+    async (electric: Electric, sql: string, args: any[] = []) =>
+      await electric.db.raw({sql, args}),
+    async (electric: Electric) => await electric.close(),
+    1000,
+  ],
   sqlite3: [
     async (): Promise<Database> => new sqlite3.Database(':memory:'),
     ['getDb', (database: Database) => database],
@@ -132,8 +168,8 @@ export const getDatabaseFunctions = <Database>(
           await cmd(
             db,
             'SELECT sql, name FROM sqlite_schema ' +
-              `WHERE type = 'table' AND name NOT LIKE ?`,
-            ['%sql%'],
+              `WHERE type = 'table' AND name NOT LIKE ? AND name NOT LIKE ?`,
+            ['%sql%', '%electric%'],
           )
         ).map(async ({sql, name}: any) => [
           name,
