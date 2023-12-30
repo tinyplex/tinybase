42da98c80ae9de434bb0deada5bdf955f0fb567e
James Pearce <github@tripleodeon.com>
2023-12-30 15:20:56 -0400

    [common] SELECT

--- src/persisters/sqlite/common.ts
+++ src/persisters/sqlite/common.ts
@@ -2,3 +2,5 @@ export const SINGLE_ROW_ID = '_';
 export const DEFAULT_ROW_ID_COLUMN_NAME = '_id';
 
 export const escapeId = (str: string) => `"${str.replace(/"/g, '""')}"`;
+
+export const SELECT = 'SELECT';
