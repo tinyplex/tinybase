15cbef3465fdc9408dd76f4d148ea911f4812f95
Alan Hughes <hughesno1@gmail.com>
2024-01-16 11:22:21 +0000

    Fix expo-sqlite-next breaking change

--- src/types/with-schemas/persisters/persister-expo-sqlite-next.d.ts
+++ src/types/with-schemas/persisters/persister-expo-sqlite-next.d.ts
@@ -2,19 +2,19 @@
 
 import {DatabasePersisterConfig, Persister} from '../persisters';
 import {OptionalSchemas, Store} from '../store';
-import {Database} from 'expo-sqlite/next';
+import {SQLiteDatabase} from 'expo-sqlite/next';
 
 /// ExpoSqliteNextPersister
 export interface ExpoSqliteNextPersister<Schemas extends OptionalSchemas>
   extends Persister<Schemas> {
   /// ExpoSqliteNextPersister.getDb
-  getDb: () => Database;
+  getDb: () => SQLiteDatabase;
 }
 
 /// createExpoSqliteNextPersister
 export function createExpoSqliteNextPersister<Schemas extends OptionalSchemas>(
   store: Store<Schemas>,
-  db: Database,
+  db: SQLiteDatabase,
   configOrStoreTableName?: DatabasePersisterConfig<Schemas> | string,
   onSqlCommand?: (sql: string, args?: any[]) => void,
   onIgnoredError?: (error: any) => void,
