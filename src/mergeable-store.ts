088750810b28ccb710495ac2fcde703355094b12
James Pearce <github@tripleodeon.com>
2024-01-20 15:42:13 -0400

    [mergeable] Fluent methods and listeners

--- src/mergeable-store.ts
+++ src/mergeable-store.ts
@@ -3,13 +3,55 @@ import {
   MergeableStore,
   createMergeableStore as createMergeableStoreDecl,
 } from './types/mergeable-store';
+import {strEndsWith, strStartsWith} from './common/strings';
+import {Store} from './types/store';
 import {createStore} from './store';
+import {slice} from './common/other';
+
+const LISTENER_ARGS: IdObj<number> = {
+  HasTable: 1,
+  Table: 1,
+  TableCellIds: 1,
+  HasTableCell: 2,
+  RowCount: 1,
+  RowIds: 1,
+  SortedRowIds: 5,
+  HasRow: 2,
+  Row: 2,
+  CellIds: 2,
+  HasCell: 3,
+  Cell: 3,
+  HasValue: 1,
+  Value: 1,
+  InvalidCell: 3,
+  InvalidValue: 1,
+};
 
 export const createMergeableStore = ((): MergeableStore => {
   const store = createStore();
   const mergeableStore: IdObj<any> = {};
-  objMap(store as IdObj<any>, (method, name) => {
-    mergeableStore[name] = method;
-  });
+  objMap(
+    store as IdObj<any>,
+    (method, name) =>
+      (mergeableStore[name] =
+        // fluent methods
+        strStartsWith(name, 'set') ||
+        strStartsWith(name, 'del') ||
+        strEndsWith(name, 'Transaction') ||
+        name == 'callListener'
+          ? (...args: any[]) => {
+              method(...args);
+              return mergeableStore;
+            }
+          : strStartsWith(name, 'add') && strEndsWith(name, 'Listener')
+            ? (...args: any[]) => {
+                const listenerArg = LISTENER_ARGS[slice(name, 3, -8)] ?? 0;
+                const listener = args[listenerArg];
+                args[listenerArg] = (_store: Store, ...args: any[]) =>
+                  listener(mergeableStore, ...args);
+                return method(...args);
+              }
+            : method),
+  );
   return objFreeze(mergeableStore) as MergeableStore;
 }) as typeof createMergeableStoreDecl;
