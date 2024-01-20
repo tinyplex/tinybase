aa74ff0d437f5d0d23e25297ea596eb828755ec6
James Pearce <github@tripleodeon.com>
2024-01-20 15:45:21 -0400

    [mergeable] Merge boilerplate

--- src/types/mergeable-store.d.ts
+++ src/types/mergeable-store.d.ts
@@ -3,7 +3,11 @@
 import {Store} from './store.d';
 
 /// MergeableStore
-export interface MergeableStore extends Store {}
+export interface MergeableStore extends Store {
+  //
+  /// MergeableStore.merge
+  merge(): MergeableStore;
+}
 
 /// createMergeableStore
 export function createMergeableStore(): MergeableStore;
