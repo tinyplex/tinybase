cb5e2d8d66ca36e8a9a6b5ece2e97ba5e2416871
James Pearce <github@tripleodeon.com>
2024-01-20 15:42:13 -0400

    [mergeable] Fluent methods and listeners

--- test/unit/other/mergeable-store.test.ts
+++ test/unit/other/mergeable-store.test.ts
@@ -1,6 +1,48 @@
-import {createMergeableStore} from 'tinybase/debug';
+import {MergeableStore, createMergeableStore} from 'tinybase/debug';
 
 test('Create', () => {
-  const mergeableStore = createMergeableStore();
-  expect(mergeableStore.getJson()).toEqual(JSON.stringify([{}, {}]));
+  const store = createMergeableStore();
+  expect(store.getJson()).toEqual(JSON.stringify([{}, {}]));
+});
+
+describe('Fluency of inherited methods', () => {
+  let store: MergeableStore;
+  beforeEach(() => {
+    store = createMergeableStore();
+  });
+  test('Setters', () => {
+    expect(store.setContent([{}, {}])).toEqual(store);
+    expect(store.setTables({})).toEqual(store);
+    expect(store.setTable('t1', {})).toEqual(store);
+    expect(store.setRow('t1', 'r1', {})).toEqual(store);
+    expect(store.setPartialRow('t1', 'r1', {})).toEqual(store);
+    expect(store.setCell('t1', 'r1', 'c1', 1)).toEqual(store);
+    expect(store.setValues({})).toEqual(store);
+    expect(store.setPartialValues({})).toEqual(store);
+    expect(store.setValue('v1', 1)).toEqual(store);
+    expect(store.setTransactionChanges([{}, {}])).toEqual(store);
+    expect(store.setTablesJson('{}')).toEqual(store);
+    expect(store.setValuesJson('{}')).toEqual(store);
+    expect(store.setJson('[{}, {}]')).toEqual(store);
+    expect(store.setTablesSchema({})).toEqual(store);
+    expect(store.setValuesSchema({})).toEqual(store);
+    expect(store.setSchema({}, {})).toEqual(store);
+  });
+  test('Deleters', () => {
+    expect(store.delTables()).toEqual(store);
+    expect(store.delTable('t1')).toEqual(store);
+    expect(store.delRow('t1', 'r1')).toEqual(store);
+    expect(store.delCell('t1', 'r1', 'c1')).toEqual(store);
+    expect(store.delValues()).toEqual(store);
+    expect(store.delValue('v1')).toEqual(store);
+    expect(store.delTablesSchema()).toEqual(store);
+    expect(store.delValuesSchema()).toEqual(store);
+    expect(store.delSchema()).toEqual(store);
+    expect(store.delListener('0')).toEqual(store);
+  });
+  test('Other', () => {
+    expect(store.callListener('0')).toEqual(store);
+    expect(store.startTransaction()).toEqual(store);
+    expect(store.finishTransaction()).toEqual(store);
+  });
 });
