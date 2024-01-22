1f5a042401f1ce646aa5db40a98580833dc99c83
James Pearce <github@tripleodeon.com>
2024-01-22 14:51:29 -0400

    [docs] Missing categories

--- src/types/docs/mergeable-store.js
+++ src/types/docs/mergeable-store.js
@@ -6,15 +6,18 @@
 /// mergeable-store
 /**
  * The MergeableStore type
+ * @category MergeableStore
  */
 /// MergeableStore
 {
   /**
    * The merge method
+   * @category Merging
    */
   /// MergeableStore.merge
 }
 /**
  * The createMergeableStore function
+ * @category Creation
  */
 /// createMergeableStore
