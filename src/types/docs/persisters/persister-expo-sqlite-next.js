47a4ecda6f226f1b47d39d6f660e3d32ffc0dbeb
James Pearce <github@tripleodeon.com>
2024-01-22 14:51:29 -0400

    [docs] Missing categories

--- src/types/docs/persisters/persister-expo-sqlite-next.js
+++ src/types/docs/persisters/persister-expo-sqlite-next.js
@@ -26,6 +26,7 @@
  *
  * It simply provides an extra getDb method for accessing a reference to the
  * database instance the Store is being persisted to.
+ * @category Persister
  * @since v4.5.0
  */
 /// ExpoSqliteNextPersister
