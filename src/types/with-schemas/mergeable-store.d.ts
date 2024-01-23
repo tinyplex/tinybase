efa1ca62aa896387a922de07b73d162eaa71fbeb
James Pearce <github@tripleodeon.com>
2024-01-23 15:47:16 -0400

    [mergeable] Typing & immutability

--- src/types/with-schemas/mergeable-store.d.ts
+++ src/types/with-schemas/mergeable-store.d.ts
@@ -1,9 +1,40 @@
 /// mergeable-store
 
-import {OptionalSchemas, Store} from './store.d';
+import {
+  Cell,
+  OptionalSchemas,
+  OptionalTablesSchema,
+  OptionalValuesSchema,
+  Store,
+  Value,
+} from './store.d';
+import {IdObj} from '../../common/obj';
 
 export type Timestamp = string;
 export type Timestamped<Thing> = [timestamp: Timestamp, thing: Thing];
+
+export type MergeableCell<Schema extends OptionalTablesSchema> =
+  Timestamped<Cell<Schema, any, any> | null>;
+export type MergeableRow<Schema extends OptionalTablesSchema> =
+  Timestamped<IdObj<MergeableCell<Schema>> | null>;
+export type MergeableTable<Schema extends OptionalTablesSchema> =
+  Timestamped<IdObj<MergeableRow<Schema>> | null>;
+export type MergeableTables<Schema extends OptionalTablesSchema> = Timestamped<
+  IdObj<MergeableTable<Schema>>
+>;
+
+export type MergeableValue<Schema extends OptionalValuesSchema> =
+  Timestamped<Value<Schema, any, any> | null>;
+export type MergeableValues<Schema extends OptionalValuesSchema> = Timestamped<
+  IdObj<MergeableValue<Schema>>
+>;
+
+export type MergeableContent<Schemas extends OptionalSchemas> = Timestamped<
+  [
+    mergeableTables: MergeableTables<Schemas[0]>,
+    mergeableValues: MergeableValues<Schemas[1]>,
+  ]
+>;
 
 /// MergeableStore
 export interface MergeableStore<Schemas extends OptionalSchemas>
@@ -13,7 +44,7 @@ export interface MergeableStore<Schemas extends OptionalSchemas>
   merge(): MergeableStore<Schemas>;
 
   /// MergeableStore.getMergeableContent
-  getMergeableContent(): any[];
+  getMergeableContent(): MergeableContent<Schemas>;
 }
 
 /// createMergeableStore
