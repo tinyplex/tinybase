4abeee8222356e6485ea6b12d1b3cd716db3451f
James Pearce <github@tripleodeon.com>
2024-02-19 20:35:35 -0500

    [mergeable] Missing types & docs

--- src/types/mergeable-store.d.ts
+++ src/types/mergeable-store.d.ts
@@ -4,7 +4,10 @@ import {CellOrUndefined, Store, ValueOrUndefined} from './store.d';
 import {Id} from './common';
 import {IdObj} from '../common/obj';
 
+/// Time
 export type Time = string;
+
+/// Stamp
 export type Stamp<Thing> = [time: Time, thing: Thing];
 
 type MergeableCell = Stamp<CellOrUndefined>;
@@ -13,10 +16,13 @@ type MergeableTable = Stamp<IdObj<MergeableRow>>;
 type MergeableTables = Stamp<IdObj<MergeableTable>>;
 type MergeableValue = Stamp<ValueOrUndefined>;
 type MergeableValues = Stamp<IdObj<MergeableValue>>;
+
+/// MergeableContent
 export type MergeableContent = Stamp<
   [mergeableTables: MergeableTables, mergeableValues: MergeableValues]
 >;
 
+/// MergeableChanges
 export type MergeableChanges = MergeableContent;
 
 /// MergeableStore
