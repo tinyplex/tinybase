27c82dec3d97c284217c804b04d957e299ebe703
James Pearce <github@tripleodeon.com>
2024-02-21 10:51:12 -0500

    [tests] Persister hygiene

--- test/unit/other/mergeable-store.test.ts
+++ test/unit/other/mergeable-store.test.ts
@@ -1,4 +1,5 @@
 import {
+  MergeableChanges,
   MergeableContent,
   MergeableStore,
   Stamp,
@@ -1394,12 +1395,10 @@ describe('Persistence', () => {
     expect(persisted).toEqual('[{"t1":{"r1":{"c1":1}}},{"v1":1}]');
   });
 
-  test('Supported, MergeableStore', async () => {
-    const store = createMergeableStore('s1');
-    let persisted = '';
-    const persister = createCustomPersister(
-      store,
-      async () => [
+  describe('Supported, MergeableStore', () => {
+    test('load / save', async () => {
+      const store = createMergeableStore('s1');
+      const changes: MergeableChanges = [
         'HeS2L2000000FG2W',
         [
           [
@@ -1413,70 +1412,59 @@ describe('Persistence', () => {
           ],
           ['HeS2L2000000FG2W', {v1: ['HeS2L2000000FG2W', 1]}],
         ],
-      ],
-      async (getContent: () => any) => {
-        persisted = JSON.stringify(getContent());
-      },
-      () => null,
-      () => null,
-      () => null,
-      true,
-    );
-    await persister.load();
-    await persister.save();
-    persister.destroy();
-    expect(persisted).toEqual(
-      JSON.stringify([
-        'HeS2L2000000FG2W',
-        [
-          [
-            'HeS2L2000000FG2W',
-            {
-              t1: [
-                'HeS2L2000000FG2W',
-                {r1: ['HeS2L2000000FG2W', {c1: ['HeS2L2000000FG2W', 1]}]},
-              ],
-            },
-          ],
-          ['HeS2L2000000FG2W', {v1: ['HeS2L2000000FG2W', 1]}],
-        ],
-      ]),
-    );
-  });
+      ];
+      let persisted = '';
+      const persister = createCustomPersister(
+        store,
+        async () => changes,
+        async (getContent: () => any) => {
+          persisted = JSON.stringify(getContent());
+        },
+        () => null,
+        () => null,
+        () => null,
+        true,
+      );
+      await persister.load();
+      await persister.save();
+      persister.destroy();
+      expect(persisted).toEqual(JSON.stringify(changes));
+    });
 
-  test('Supported, MergeableStore, loading from legacy', async () => {
-    const store = createMergeableStore('s1');
-    let persisted = '';
-    const persister = createCustomPersister(
-      store,
-      async () => [{t1: {r1: {c1: 1}}}, {v1: 1}],
-      async (getContent: () => any) => {
-        persisted = JSON.stringify(getContent());
-      },
-      () => null,
-      () => null,
-      () => null,
-      true,
-    );
-    await persister.load();
-    await persister.save();
-    persister.destroy();
-    expect(persisted).toEqual(
-      JSON.stringify([
-        'HeS2L2000000FG2W',
-        [
+    test('loading from legacy', async () => {
+      const store = createMergeableStore('s1');
+      let persisted = '';
+      const persister = createCustomPersister(
+        store,
+        async () => [{t1: {r1: {c1: 1}}}, {v1: 1}],
+        async (getContent: () => any) => {
+          persisted = JSON.stringify(getContent());
+        },
+        () => null,
+        () => null,
+        () => null,
+        true,
+      );
+      await persister.load();
+      await persister.save();
+      persister.destroy();
+      expect(persisted).toEqual(
+        JSON.stringify([
+          'HeS2L2000000FG2W',
           [
-            'HeS2L2000000FG2W',
-            {
-              t1: [
-                'HeS2L2000000FG2W',
-                {r1: ['HeS2L2000000FG2W', {c1: ['HeS2L2000000FG2W', 1]}]},
-              ],
-            },
+            [
+              'HeS2L2000000FG2W',
+              {
+                t1: [
+                  'HeS2L2000000FG2W',
+                  {r1: ['HeS2L2000000FG2W', {c1: ['HeS2L2000000FG2W', 1]}]},
+                ],
+              },
+            ],
+            ['HeS2L2000000FG2W', {v1: ['HeS2L2000000FG2W', 1]}],
           ],
-          ['HeS2L2000000FG2W', {v1: ['HeS2L2000000FG2W', 1]}],
-        ],
-      ]),
-    );
+        ]),
+      );
+    });
   });
 });
