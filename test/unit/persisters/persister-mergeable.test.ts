2b55f96b22ec43b8843dda4ccc05409a733ce5e7
James Pearce <github@tripleodeon.com>
2024-02-25 08:36:03 -0500

    [tests] Move mocks to shared file

--- test/unit/persisters/persister-mergeable.test.ts
+++ test/unit/persisters/persister-mergeable.test.ts
@@ -14,10 +14,8 @@ import {
 } from 'tinybase/debug';
 import {GetLocationMethod, Persistable, nextLoop} from './common';
 import {START_TIME, nullStamp, stamped1} from '../common/mergeable';
-import {createFilePersister} from 'tinybase/debug/persisters/persister-file';
-import fs from 'fs';
+import {mockFile, mockLocalStorage, mockSessionStorage} from './mocks';
 import {pause} from '../common/other';
-import tmp from 'tmp';
 
 beforeEach(() => {
   jest.useFakeTimers({now: START_TIME});
@@ -280,36 +278,13 @@ describe('Full sequence', () => {
     },
   );
 
-  const mockFile: Persistable = {
-    autoLoadPause: 100,
-    getLocation: async (): Promise<string> => {
-      tmp.setGracefulCleanup();
-      return tmp.fileSync().name;
-    },
-    getLocationMethod: ['getFilePath', (location) => location],
-    getPersister: createFilePersister,
-    get: async (
-      location: string,
-    ): Promise<Content | MergeableContent | void> => {
-      try {
-        return JSON.parse(fs.readFileSync(location, 'utf-8'));
-      } catch {}
-    },
-    set: async (
-      location: string,
-      content: Content | MergeableContent,
-    ): Promise<void> => await mockFile.write(location, JSON.stringify(content)),
-    write: async (location: string, rawContent: any): Promise<void> =>
-      fs.writeFileSync(location, rawContent, 'utf-8'),
-    del: async (location: string): Promise<void> => fs.unlinkSync(location),
-    testMissing: true,
-  };
-
   describe.each([
     ['mockMergeableNoContentListener', mockMergeableNoContentListener],
     ['mockMergeableContentListener', mockMergeableContentListener],
     ['mockMergeableChangesListener', mockMergeableChangesListener],
     ['file', mockFile],
+    ['localStorage', mockLocalStorage],
+    ['sessionStorage', mockSessionStorage],
   ])('Persists to/from %s', (name: string, persistable: Persistable<any>) => {
     let location: string;
     let getLocationMethod: GetLocationMethod<any> | undefined;
