2ff77cd9fa9e14cde2da5ab76baf418d1e7ae8b5
James Pearce <github@tripleodeon.com>
2024-02-25 08:35:45 -0500

    [persisters] Browsers support mergeable

--- src/types/persisters/persister-browser.d.ts
+++ src/types/persisters/persister-browser.d.ts
@@ -4,13 +4,13 @@ import {Persister} from '../persisters';
 import {Store} from '../store';
 
 /// SessionPersister
-export interface SessionPersister extends Persister {
+export interface SessionPersister extends Persister<true> {
   /// SessionPersister.getStorageName
   getStorageName(): string;
 }
 
 /// LocalPersister
-export interface LocalPersister extends Persister {
+export interface LocalPersister extends Persister<true> {
   /// LocalPersister.getStorageName
   getStorageName(): string;
 }
