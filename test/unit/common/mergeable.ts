8b1670ee6f146ac782ef773e9f68bf5040f52e30
James Pearce <github@tripleodeon.com>
2024-03-02 11:41:56 -0600

    [mergeable] Space in Stamp for a hash

--- test/unit/common/mergeable.ts
+++ test/unit/common/mergeable.ts
@@ -40,13 +40,19 @@ export const time = (offset: number, counter: number, storeId: string = 's1') =>
 export const START_TIME = new Date('2024-01-01 00:00:00 UTC');
 
 export const stamped1 = (offset: number, counter: number, thing: any) => [
+  '',
   time(offset, counter, 's1'),
   thing,
 ];
 
 export const stamped2 = (offset: number, counter: number, thing: any) => [
+  '',
   time(offset, counter, 's2'),
   thing,
 ];
 
-export const nullStamped = <Thing>(thing: Thing): Stamp<Thing> => ['', thing];
+export const nullStamped = <Thing>(thing: Thing): Stamp<Thing> => [
+  '',
+  '',
+  thing,
+];
