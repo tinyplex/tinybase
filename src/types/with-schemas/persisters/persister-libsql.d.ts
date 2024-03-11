56585f98077f9e9b34fe57e92e4b32ddeee00186
James Pearce <github@tripleodeon.com>
2024-03-11 15:12:17 -0600

    [libsql] Initial implementation

---
+++ src/types/with-schemas/persisters/persister-libsql.d.ts
@@ -0,0 +1,21 @@
+/// persister-libsql
+
+import {DatabasePersisterConfig, Persister} from '../persisters';
+import {OptionalSchemas, Store} from '../store';
+import {Client} from '@libsql/client';
+
+/// LibSqlPersister
+export interface LibSqlPersister<Schemas extends OptionalSchemas>
+  extends Persister<Schemas> {
+  /// LibSqlPersister.getClient
+  getClient: () => Client;
+}
+
+/// createLibSqlPersister
+export function createLibSqlPersister<Schemas extends OptionalSchemas>(
+  store: Store<Schemas>,
+  client: Client,
+  configOrStoreTableName?: DatabasePersisterConfig<Schemas> | string,
+  onSqlCommand?: (sql: string, args?: any[]) => void,
+  onIgnoredError?: (error: any) => void,
+): LibSqlPersister<Schemas>;
