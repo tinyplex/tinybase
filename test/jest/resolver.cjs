3e12734c1f632621d25c0b7de1dc19848435b348
James Pearce <github@tripleodeon.com>
2024-03-11 16:20:26 -0600

    [libsql] Jest resolution hacks

--- test/jest/resolver.cjs
+++ test/jest/resolver.cjs
@@ -6,9 +6,25 @@ module.exports = (path, options) =>
         packageJson.name === 'uuid' ||
         packageJson.name === '@automerge/automerge-wasm'
       ) {
-        delete packageJson['exports'];
-        delete packageJson['module'];
+        delete packageJson.exports;
+        delete packageJson.module;
       }
+
+      if (packageJson.name === '@libsql/client') {
+        packageJson.main = 'lib-esm/node.js';
+        delete packageJson['exports'];
+      }
+
+      if (packageJson.name === '@libsql/hrana-client') {
+        packageJson.main = 'lib-esm/index.js';
+        delete packageJson.exports;
+      }
+
+      if (packageJson.name === '@libsql/isomorphic-fetch') {
+        packageJson.main = 'node.js';
+        delete packageJson.exports;
+      }
+
       return packageJson;
     },
   });
