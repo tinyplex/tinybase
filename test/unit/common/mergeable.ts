9f371bede6a1fe9ad085f22be64f90fd95ea1c02
James Pearce <github@tripleodeon.com>
2024-03-14 22:37:56 -0600

    [mergeable] Cleanup

--- test/unit/common/mergeable.ts
+++ test/unit/common/mergeable.ts
@@ -39,13 +39,8 @@ export const time = (offset: number, counter: number, storeId: string = 's1') =>
 
 export const START_TIME = new Date('2024-01-01 00:00:00 UTC');
 
-export const stamped1 = (offset: number, counter: number, thing: any) => [
+export const stamped = (offset: number, counter: number, thing: any) => [
   time(offset, counter, 's1'),
-  thing,
-];
-
-export const stamped2 = (offset: number, counter: number, thing: any) => [
-  time(offset, counter, 's2'),
   thing,
 ];
 
