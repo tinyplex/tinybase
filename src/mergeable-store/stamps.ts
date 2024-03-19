67cb2636d770e138c0811bc3ae02ce584af78bd1
James Pearce <github@tripleodeon.com>
2024-03-19 12:37:18 -0600

    [mergeable] Wrestling types

--- src/mergeable-store/stamps.ts
+++ src/mergeable-store/stamps.ts
@@ -1,20 +1,33 @@
-import {Hash, HashStamp, Stamp, Time} from '../types/mergeable-store';
+import {
+  CellStamp,
+  Hash,
+  Stamp,
+  Time,
+  ValueStamp,
+} from '../types/mergeable-store';
 import {IdMap, mapNew, mapToObj} from '../common/map';
 import {IdObj, objNew} from '../common/obj';
 import {EMPTY_STRING} from '../common/strings';
 import {Id} from '../types/common';
 import {getHash} from './hash';
 
-export const stampClone = <Value>([time, value]:
-  | Stamp<Value>
-  | HashStamp<Value>): Stamp<Value> => [time, value];
+export type StampMap<Thing> = Stamp<IdMap<Thing>, true>;
+
+export type TablesStampMap = StampMap<TableStampMap>;
+export type TableStampMap = StampMap<RowStampMap>;
+export type RowStampMap = StampMap<CellStamp<true>>;
+export type ValuesStampMap = StampMap<ValueStamp<true>>;
+
+export const stampClone = <Value>([time, value]: Stamp<
+  Value,
+  boolean
+>): Stamp<Value> => [time, value];
 
 const cloneHashStamp = <Value>(
-  [time, value, hash]: HashStamp<Value>,
+  [time, value, hash]: Stamp<Value, true>,
   _id: Id = EMPTY_STRING,
   removeHash: 0 | 1 = 0,
-): HashStamp<Value> | Stamp<Value> =>
-  removeHash ? [time, value] : [time, value, hash];
+): Stamp<Value, boolean> => (removeHash ? [time, value] : [time, value, hash]);
 
 export const stampNew = <Thing>(
   time: Time = EMPTY_STRING,
@@ -23,14 +36,14 @@ export const stampNew = <Thing>(
 
 export const hashIdAndHash = (id: Id, hash: Hash) => getHash(id + ':' + hash);
 
-export const updateHashStamp = (
-  hashStamp: HashStamp<unknown>,
+export const updateStamp = (
+  stamp: Stamp<unknown, true>,
   hash: Hash,
   time: Time,
 ) => {
-  hashStamp[2] = hash >>> 0;
-  if (time > hashStamp[0]) {
-    hashStamp[0] = time;
+  stamp[2] = hash >>> 0;
+  if (time > stamp[0]) {
+    stamp[0] = time;
   }
 };
 
@@ -40,30 +53,30 @@ export const stampNewObj = <Thing>(
 
 export const hashStampNewMap = <Thing>(
   time = EMPTY_STRING,
-): HashStamp<IdMap<Thing>> => [time, mapNew<Id, Thing>(), 0];
+): StampMap<Thing> => [time, mapNew<Id, Thing>(), 0];
 
-export const hashStampNewThing = <Thing>(): HashStamp<Thing> => [
+export const hashStampNewThing = <Thing>(): Stamp<Thing, true> => [
   EMPTY_STRING,
   undefined as any,
   0,
 ];
 
 export const hashStampMap = <From, To = From>(
-  [time, value, hash]: HashStamp<From>,
+  [time, value, hash]: Stamp<From, true>,
   removeHash: 0 | 1,
   mapper: (value: From, time: Time) => To,
-): HashStamp<To> | Stamp<To> =>
+): Stamp<To, boolean> =>
   removeHash ? [time, mapper(value, time)] : [time, mapper(value, time), hash];
 
 export const hashStampMapToObj = <From, To = From>(
-  hashStamp: HashStamp<IdMap<From>>,
+  hashStamp: Stamp<IdMap<From>, true>,
   removeHash: 0 | 1 = 0,
   mapper: (
     mapValue: From,
     id: Id,
     removeHash: 0 | 1,
   ) => To = cloneHashStamp as any,
-): HashStamp<IdObj<To>> | Stamp<IdObj<To>> =>
+): Stamp<IdObj<To>, boolean> =>
   hashStampMap(hashStamp, removeHash, (map) =>
     mapToObj(map, (from: From, id: Id) => mapper(from, id, removeHash)),
   );
