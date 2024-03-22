75c03d12ad1f39575eb37786c0470f64eab28de2
James Pearce <github@tripleodeon.com>
2024-03-21 19:35:48 -0600

    [persister] More sync scaffolding

--- test/unit/persisters/mocks.ts
+++ test/unit/persisters/mocks.ts
@@ -2,6 +2,7 @@ import {
   Changes,
   Content,
   Id,
+  MergeableChanges,
   MergeableContent,
   Persister,
   Store,
@@ -108,10 +109,13 @@ const docObjMatch = (
 
 let customPersister: any;
 let customPersisterListener:
-  | ((getContent?: () => Content) => void)
-  | ((getContent?: () => Content, getChanges?: () => Changes) => void)
+  | ((getContent?: () => Content | MergeableContent) => void)
+  | ((
+      getContent?: () => Content | MergeableContent,
+      getChanges?: () => Changes | MergeableChanges,
+    ) => void)
   | undefined;
-let customPersisterChanges: Changes = [{}, {}];
+let customPersisterChanges: Changes | MergeableChanges = [{}, {}];
 
 const getMockedCustom = (
   write: (location: string, rawContent: any) => Promise<void>,
