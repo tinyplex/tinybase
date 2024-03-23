468569c27210dd0ef4fac59068f2860097aac36a
James Pearce <github@tripleodeon.com>
2024-03-23 10:31:17 -0600

    [mergeable] Simple SyncPersister implementation & tests

--- test/unit/persisters/mocks.ts
+++ test/unit/persisters/mocks.ts
@@ -4,11 +4,13 @@ import {
   Id,
   MergeableChanges,
   MergeableContent,
+  MergeableStore,
   Persister,
   Store,
   Tables,
   Values,
   createCustomPersister,
+  createMergeableStore,
 } from 'tinybase/debug';
 import {DbSchema, ElectricClient} from 'electric-sql/client/model';
 import {DocHandle, Repo} from '@automerge/automerge-repo';
@@ -27,6 +29,7 @@ import {createAutomergePersister} from 'tinybase/debug/persisters/persister-auto
 import {createFilePersister} from 'tinybase/debug/persisters/persister-file';
 import {createIndexedDbPersister} from 'tinybase/debug/persisters/persister-indexed-db';
 import {createRemotePersister} from 'tinybase/debug/persisters/persister-remote';
+import {createSyncPersister} from 'tinybase/debug/persisters/persister-sync';
 import {createYjsPersister} from 'tinybase/debug/persisters/persister-yjs';
 import crypto from 'crypto';
 import fetchMock from 'jest-fetch-mock';
@@ -251,6 +254,40 @@ export const mockFile: Persistable = {
   write: async (location: string, rawContent: any): Promise<void> =>
     fs.writeFileSync(location, rawContent, 'utf-8'),
   del: async (location: string): Promise<void> => fs.unlinkSync(location),
+  testMissing: true,
+};
+
+export const mockSync: Persistable<MergeableStore> = {
+  autoLoadPause: 100,
+  getLocation: async (): Promise<MergeableStore> => {
+    return createMergeableStore('otherStore');
+  },
+  getLocationMethod: ['getOtherStore', (location) => location],
+  getPersister: createSyncPersister as any,
+  get: async (
+    location: MergeableStore,
+  ): Promise<Content | MergeableContent | void> => {
+    try {
+      location.getMergeableContent();
+    } catch {}
+  },
+  set: async (
+    location: MergeableStore,
+    content: Content | MergeableContent,
+  ): Promise<void> => await mockSync.write(location, content),
+  write: async (location: MergeableStore, rawContent: any): Promise<void> => {
+    location.setMergeableContent(rawContent);
+  },
+  del: async (location: MergeableStore): Promise<void> => {
+    location.setMergeableContent([
+      '',
+      [
+        ['', {}, 0],
+        ['', {}, 0],
+      ],
+      0,
+    ]);
+  },
   testMissing: true,
 };
 
