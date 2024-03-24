14724d9751349ce1ebe53fdd40fb757ecc4bfbd3
James Pearce <github@tripleodeon.com>
2024-03-24 10:31:26 -0600

    [sync] Sync persister improvements

--- src/persisters/persister-sync.ts
+++ src/persisters/persister-sync.ts
@@ -1,4 +1,8 @@
-import {MergeableContent, MergeableStore} from '../types/mergeable-store';
+import {
+  MergeableChanges,
+  MergeableContent,
+  MergeableStore,
+} from '../types/mergeable-store';
 import {
   SyncPersister,
   createSyncPersister as createSyncPersisterDecl,
@@ -6,6 +10,7 @@ import {
 import {Content} from '../types/store';
 import {PersisterListener} from '../types/persisters';
 import {createCustomPersister} from '../persisters';
+import {objForEach} from '../common/obj';
 
 export const createSyncPersister = ((
   store: MergeableStore,
@@ -14,10 +19,10 @@ export const createSyncPersister = ((
 ): SyncPersister => {
   const getPersisted = async (): Promise<MergeableContent> => {
     const persisted = otherStore.getMergeableContent();
-    if (persisted[0] !== '') {
-      return persisted;
+    if (persisted[0] === '') {
+      return null as any;
     }
-    return null as any;
+    return persisted;
   };
 
   const setPersisted = async (
@@ -27,7 +32,53 @@ export const createSyncPersister = ((
   };
 
   const addPersisterListener = (listener: PersisterListener) =>
-    otherStore.addDidFinishTransactionListener(() => listener());
+    otherStore.addDidFinishTransactionListener(() => {
+      const contentHashes = store.getMergeableContentHashes();
+      const otherContentHashes = otherStore.getMergeableContentHashes();
+
+      if (contentHashes[0] === otherContentHashes[0]) {
+        return null as any;
+      }
+
+      const changes: MergeableChanges = [
+        '',
+        [
+          ['', {}],
+          ['', {}],
+        ],
+      ];
+
+      if (contentHashes[1][0] !== otherContentHashes[1][0]) {
+        const tablesDelta = otherStore.getMergeableTablesDelta(
+          store.getMergeableTablesHashes(),
+        );
+        changes[1][0][0] = '!';
+        objForEach(tablesDelta[1], (_, tableId) => {
+          const tableDelta = otherStore.getMergeableTableDelta(
+            tableId,
+            store.getMergeableTableHashes(tableId),
+          );
+          changes[1][0][1][tableId] = ['!', {}];
+          objForEach(tableDelta[1], (_, rowId) => {
+            const rowDelta = otherStore.getMergeableRowDelta(
+              tableId,
+              rowId,
+              store.getMergeableRowHashes(tableId, rowId),
+            );
+            changes[1][0][1][tableId][1][rowId] = rowDelta;
+          });
+        });
+      }
+
+      if (contentHashes[1][1] !== otherContentHashes[1][1]) {
+        const valuesDelta = otherStore.getMergeableValuesDelta(
+          store.getMergeableValuesHashes(),
+        );
+        changes[1][1] = valuesDelta;
+      }
+
+      listener(undefined, () => changes);
+    });
 
   const delPersisterListener = otherStore.delListener;
 
