d02aed4c04101319b321303771e286160f0ca227
James Pearce <github@tripleodeon.com>
2024-03-29 05:12:45 -0600

    [sync] More tests

--- test/unit/persisters/persister-sync.test.ts
+++ test/unit/persisters/persister-sync.test.ts
@@ -52,53 +52,170 @@ describe('Syncs', () => {
   test('Both match', async () => {
     store1.setContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
+    await pause(1, true);
     store2.setContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
     await expectBothToHaveContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
   });
 
   test('store1 empty', async () => {
     store2.setContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
     await expectBothToHaveContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
   });
 
   test('store2 empty', async () => {
     store1.setContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
     await expectBothToHaveContent([
       {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
-      {v1: 2, v2: 2},
+      {v1: 1, v2: 2},
     ]);
   });
 
   test('store1 missing tables', async () => {
-    store1.setValues({v1: 2, v2: 2});
+    store1.setValues({v1: 1, v2: 2});
+    await pause(1, true);
+    store2.setContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+  });
+
+  test('store2 missing tables', async () => {
+    store2.setValues({v1: 1, v2: 2});
+    await pause(1, true);
+    store1.setContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+  });
+
+  test('store1 missing table', async () => {
+    store1.setTable('t1', {r1: {c1: 1, c2: 2}, r2: {c2: 2}});
     await pause(1, true);
     store2.setTables({
       t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}},
       t2: {r2: {c2: 2}},
     });
     await expectBothToHaveContent([
-      {
-        t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}},
-        t2: {r2: {c2: 2}},
-      },
-      {v1: 2, v2: 2},
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {},
     ]);
+  });
+
+  test('store2 missing table', async () => {
+    store2.setTable('t1', {r1: {c1: 1, c2: 2}, r2: {c2: 2}});
+    await pause(1, true);
+    store1.setTables({
+      t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}},
+      t2: {r2: {c2: 2}},
+    });
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {},
+    ]);
+  });
+
+  test('store1 missing row', async () => {
+    store1.setRow('t1', 'r1', {c1: 1, c2: 2});
+    await pause(1, true);
+    store2.setTable('t1', {r1: {c1: 1, c2: 2}, r2: {c2: 2}});
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}},
+      {},
+    ]);
+  });
+
+  test('store2 missing row', async () => {
+    store2.setRow('t1', 'r1', {c1: 1, c2: 2});
+    await pause(1, true);
+    store1.setTable('t1', {r1: {c1: 1, c2: 2}, r2: {c2: 2}});
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}},
+      {},
+    ]);
+  });
+
+  test('store1 missing cell', async () => {
+    store1.setCell('t1', 'r1', 'c1', 1);
+    await pause(1, true);
+    store2.setRow('t1', 'r1', {c1: 1, c2: 2});
+    await expectBothToHaveContent([{t1: {r1: {c1: 1, c2: 2}}}, {}]);
+  });
+
+  test('store2 missing cell', async () => {
+    store2.setCell('t1', 'r1', 'c1', 1);
+    await pause(1, true);
+    store1.setRow('t1', 'r1', {c1: 1, c2: 2});
+    await expectBothToHaveContent([{t1: {r1: {c1: 1, c2: 2}}}, {}]);
+  });
+
+  test('store1 missing values', async () => {
+    store1.setTables({
+      t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}},
+      t2: {r2: {c2: 2}},
+    });
+    await pause(1, true);
+    store2.setContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+  });
+
+  test('store2 missing values', async () => {
+    store2.setTables({
+      t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}},
+      t2: {r2: {c2: 2}},
+    });
+    await pause(1, true);
+    store1.setContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+    await expectBothToHaveContent([
+      {t1: {r1: {c1: 1, c2: 2}, r2: {c2: 2}}, t2: {r2: {c2: 2}}},
+      {v1: 1, v2: 2},
+    ]);
+  });
+
+  test('store1 missing value', async () => {
+    store1.setValue('v2', 2);
+    await pause(1, true);
+    store2.setValues({v1: 1, v2: 2});
+    await expectBothToHaveContent([{}, {v1: 1, v2: 2}]);
+  });
+
+  test('store2 missing value', async () => {
+    store2.setValue('v2', 2);
+    await pause(1, true);
+    store1.setValues({v1: 1, v2: 2});
+    await expectBothToHaveContent([{}, {v1: 1, v2: 2}]);
   });
 });
