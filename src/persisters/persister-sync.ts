252fb16e3deceba7eec2c846d136b447b0df078f
James Pearce <github@tripleodeon.com>
2024-04-01 18:01:49 -0600

    [sync] Transaction Id idea

--- src/persisters/persister-sync.ts
+++ src/persisters/persister-sync.ts
@@ -1,10 +1,10 @@
-import {MergeableContent, MergeableStore} from '../types/mergeable-store';
+import {Id, Ids} from '../types/common';
 import {
   SyncPersister,
   createSyncPersister as createSyncPersisterDecl,
 } from '../types/persisters/persister-sync';
 import {mapNew, mapSet} from '../common/map';
-import {Ids} from '../types/common';
+import {MergeableStore} from '../types/mergeable-store';
 import {PersisterListener} from '../types/persisters';
 import {collForEach} from '../common/coll';
 import {createCustomPersister} from '../persisters';
@@ -12,6 +12,7 @@ import {isUndefined} from '../common/other';
 import {objForEach} from '../common/obj';
 
 type ReceiveMessage = (
+  transactionId: Id,
   store: MergeableStore,
   message: string,
   payload: any,
@@ -19,6 +20,7 @@ type ReceiveMessage = (
 ) => void;
 
 type SendMessage = (
+  transactionId: Id,
   fromStore: MergeableStore,
   message: string,
   payload: any,
@@ -37,13 +39,16 @@ const getBusFunctions = (): [
     mapSet(stores, store, receiveMessage);
   };
   const sendMessage = (
+    transactionId: Id,
     fromStore: MergeableStore,
     message: string,
     payload: any,
     args?: Ids,
   ): void =>
     collForEach(stores, (receiveMessage, store) =>
-      store != fromStore ? receiveMessage(store, message, payload, args) : 0,
+      store != fromStore
+        ? receiveMessage(transactionId, store, message, payload, args)
+        : 0,
     );
   return [addStore, sendMessage];
 };
@@ -55,41 +60,59 @@ export const createSyncPersister = ((
 ): SyncPersister => {
   const [addStore, sendMessage] = getBusFunctions();
 
-  const receiveMessage = (
+  const receiveMessage: ReceiveMessage = (
+    transactionId: Id,
     store: MergeableStore,
     message: string,
     payload: any,
     args: Ids = [],
   ) => {
-    console.log(store.getId(), 'received', message, args);
-    console.log(JSON.stringify(payload));
-    console.log();
+    // console.log(transactionId, store.getId(), 'received', message, args);
+    // console.log(JSON.stringify(payload));
+    // console.log();
 
     if (message == 'contentHashes') {
       const [_contentHash, [tablesHashes, valuesHashes]] = payload;
       const [_myContentHash, [myTablesHashes, myValuesHashes]] =
         store.getMergeableContentHashes();
       if (tablesHashes != myTablesHashes) {
-        sendMessage(store, 'getTablesDelta', store.getMergeableTablesHashes());
+        sendMessage(
+          transactionId,
+          store,
+          'getTablesDelta',
+          store.getMergeableTablesHashes(),
+        );
       }
       if (valuesHashes != myValuesHashes) {
-        sendMessage(store, 'getValuesDelta', store.getMergeableValuesHashes());
+        sendMessage(
+          transactionId,
+          store,
+          'getValuesDelta',
+          store.getMergeableValuesHashes(),
+        );
       }
     }
 
     if (message == 'getContentDelta') {
       sendMessage(
+        transactionId,
         store,
         'contentDelta',
         store.getMergeableContentDelta(payload),
       );
     }
     if (message == 'getTablesDelta') {
-      sendMessage(store, 'tablesDelta', store.getMergeableTablesDelta(payload));
+      sendMessage(
+        transactionId,
+        store,
+        'tablesDelta',
+        store.getMergeableTablesDelta(payload),
+      );
     }
     if (message == 'getTableDelta') {
       const [tableId] = args;
       sendMessage(
+        transactionId,
         store,
         'tableDelta',
         store.getMergeableTableDelta(tableId, payload),
@@ -99,6 +122,7 @@ export const createSyncPersister = ((
     if (message == 'getRowDelta') {
       const [tableId, rowId] = args;
       sendMessage(
+        transactionId,
         store,
         'rowDelta',
         store.getMergeableRowDelta(tableId, rowId, payload),
@@ -106,7 +130,12 @@ export const createSyncPersister = ((
       );
     }
     if (message == 'getValuesDelta') {
-      sendMessage(store, 'valuesDelta', store.getMergeableValuesDelta(payload));
+      sendMessage(
+        transactionId,
+        store,
+        'valuesDelta',
+        store.getMergeableValuesDelta(payload),
+      );
     }
 
     if (message == 'contentDelta') {
@@ -114,6 +143,7 @@ export const createSyncPersister = ((
         const [_time, [tablesHash, valuesHash]] = payload;
         if (!isUndefined(tablesHash)) {
           sendMessage(
+            transactionId,
             store,
             'getTablesDelta',
             store.getMergeableTablesHashes(),
@@ -121,6 +151,7 @@ export const createSyncPersister = ((
         }
         if (!isUndefined(valuesHash)) {
           sendMessage(
+            transactionId,
             store,
             'getValuesDelta',
             store.getMergeableValuesHashes(),
@@ -132,6 +163,7 @@ export const createSyncPersister = ((
       const [_time, tablesChanges] = payload;
       objForEach(tablesChanges, (_, tableId) => {
         sendMessage(
+          transactionId,
           store,
           'getTableDelta',
           store.getMergeableTableHashes(tableId),
@@ -144,6 +176,7 @@ export const createSyncPersister = ((
       const [_time, tableChanges] = payload;
       objForEach(tableChanges, (_, rowId) => {
         sendMessage(
+          transactionId,
           store,
           'getRowDelta',
           store.getMergeableRowHashes(tableId, rowId),
@@ -177,19 +210,28 @@ export const createSyncPersister = ((
   addStore(store, receiveMessage);
   addStore(otherStore, receiveMessage);
 
-  const getPersisted = async (): Promise<MergeableContent> => {
-    const persisted = otherStore.getMergeableContent();
-    if (persisted[0] === '') {
-      return null as any;
+  const getPersisted = async (): Promise<undefined | 1> => {
+    if (otherStore.getMergeableContent()[0] === '') {
+      return undefined;
     }
 
-    sendMessage(store, 'getContentDelta', store.getMergeableContentHashes());
+    sendMessage(
+      'GP',
+      store,
+      'getContentDelta',
+      store.getMergeableContentHashes(),
+    );
 
-    //    return persisted;
+    return 1;
   };
 
   const setPersisted = async (): Promise<void> => {
-    sendMessage(store, 'contentHashes', store.getMergeableContentHashes());
+    sendMessage(
+      'SP',
+      store,
+      'contentHashes',
+      store.getMergeableContentHashes(),
+    );
   };
 
   const addPersisterListener = (listener: PersisterListener) =>
