c6223c3b0e581e47e0bdc8a9bcec8f955fa1aef4
James Pearce <github@tripleodeon.com>
2024-04-03 12:37:39 -0600

    [sync] Move to bus architecture

--- src/types/docs/persisters/persister-sync.js
+++ src/types/docs/persisters/persister-sync.js
@@ -1,38 +1,54 @@
 /**
  * The persister-sync module of the TinyBase project lets you synchronize
- * MergeableStore data to and from another MergeableStore instance.
+ * MergeableStore data to and from other MergeableStore instances.
  * @see Persisting Data guide
  * @packageDocumentation
  * @module persister-sync
  */
 /// persister-sync
 /**
+ * The Receive type
+ *
+ */
+/// Receive
+/**
+ * The Send type
+ *
+ */
+/// Send
+/**
+ * The Bus type
+ *
+ */
+/// Bus
+/**
  * The SyncPersister interface is a minor extension to the Persister interface.
  *
- * It simply provides an extra getOtherStore method for accessing the reference
- * of the other MergeableStore that the MergeableStore is being persisted to.
+ * It simply provides an extra getBus method for accessing the reference the Bus
+ * that the MergeableStore is being persisted via.
  * @category Persister
  * @since v5.0.0
  */
 /// SyncPersister
 {
   /**
-   * The getOtherStore method returns the reference of the other MergeableStore
-   * that the MergeableStore is being persisted to.
+   * The getBus method returns the reference of the Bus that the MergeableStore
+   * is being persisted via.
    * @returns The reference of the other MergeableStore.
    * @example
    * This example creates a Persister object against a newly-created
-   * MergeableStore and then gets the other MergeableStore back out
+   * MergeableStore and then gets the reference to the Bus back out
    * again.
    *
    * ```js
+   * const bus = createLocalBus();
+   *
    * const store1 = createMergeableStore('store1').setTables({
    *   pets: {fido: {species: 'dog'}},
    * });
-   * const store2 = createMergeableStore('store2');
-   * const persister = createSyncPersister(store1, store2);
+   * const persister = createSyncPersister(store1, bus);
    *
-   * console.log(persister.getOtherStore() == store2);
+   * console.log(persister.getBus() == bus);
    * // -> true
    *
    * persister.destroy();
@@ -40,17 +56,17 @@
    * @category Getter
    * @since v5.0.0
    */
-  /// SyncPersister.getOtherStore
+  /// SyncPersister.getBus
 }
 /**
  * The createSyncPersister function creates a Persister object that can persist
  * one MergeableStore to another.
  *
  * As well as providing a reference to the MergeableStore to persist, you must
- * provide a `otherStore` parameter which identifies the other MergeableStore to
- * persist it to.
+ * provide a `bus` parameter which identifies the Bus that is used to transmit
+ * changes to and from this MergeableStore and its peers. persist it to.
  * @param store The MergeableStore to persist.
- * @param otherStore The reference of the other MergeableStore.
+ * @param bus The reference of the Bus.
  * @param onIgnoredError An optional handler for the errors that the Persister
  * would otherwise ignore when trying to save or load data. This is suitable for
  * debugging persistence issues in a development environment.
@@ -60,23 +76,32 @@
  * to another.
  *
  * ```js
+ * const bus = createLocalBus();
+ *
  * const store1 = createMergeableStore('store1').setTables({
  *   pets: {fido: {species: 'dog'}},
  * });
- * const store2 = createMergeableStore('store2');
- * const persister = createSyncPersister(store1, store2);
+ * const persister1 = createSyncPersister(store1, bus);
  *
- * await persister.save();
+ * const store2 = createMergeableStore('store2');
+ * const persister2 = createSyncPersister(store2, bus);
+ *
+ * await persister1.save();
  * // Store2 will be synced with Store1.
  *
  * console.log(store2.getTables());
  * // -> {pets: {fido: {species: 'dog'}}}
  *
- * await persister.load();
+ * await persister1.load();
  * // Store1 will be synced with Store2.
  *
- * persister.destroy();
+ * persister1.destroy();
+ * persister2.destroy();
  * ```
  * @category Creation
  */
 /// createSyncPersister
+/**
+ * The createLocalBus function
+ */
+/// createLocalBus
