288acb0813a07d20ef964dc0a2ea629e03865233
James Pearce <github@tripleodeon.com>
2024-04-08 17:02:43 -0600

    [syc] Complete transition to request pattern

--- src/persisters/persister-sync.ts
+++ src/persisters/persister-sync.ts
@@ -24,10 +24,11 @@ import {
   promiseAll,
   promiseNew,
 } from '../common/other';
-import {objForEach, objToArray} from '../common/obj';
 import {EMPTY_STRING} from '../common/strings';
+import {PersisterListener} from '../types/persisters';
 import {createCustomPersister} from '../persisters';
 import {getHlcFunctions} from '../mergeable-store/hlc';
+import {objToArray} from '../common/obj';
 
 export const createSyncPersister = ((
   store: MergeableStore,
@@ -35,6 +36,8 @@ export const createSyncPersister = ((
   requestTimeoutSeconds = 1,
   onIgnoredError?: (error: any) => void,
 ): SyncPersister => {
+  let persisterListener: PersisterListener | undefined;
+
   const [join] = bus;
   const [getHlc] = getHlcFunctions(store.getId());
   const pendingRequests: IdMap<
@@ -51,20 +54,6 @@ export const createSyncPersister = ((
     payload: any,
     args: Ids = [],
   ) => {
-    // console.log(
-    //   requestId,
-    //   fromStoreId,
-    //   '->',
-    //   store.getId(),
-    //   'received',
-    //   message,
-    //   args,
-    //   JSON.stringify(payload),
-    //   persister.isAutoLoading(),
-    //   persister.isAutoSaving(),
-    //   '\n',
-    // );
-
     ifNotUndefined(
       mapGet(pendingRequests, requestId),
       ([toStoreId, handlePayload]) =>
@@ -73,34 +62,17 @@ export const createSyncPersister = ((
           : 0,
       () => {
         if (message == 'contentHashes' && persister.isAutoLoading()) {
-          const [_contentHash, [tablesHashes, valuesHashes]] = payload;
-          const [_myContentHash, [myTablesHashes, myValuesHashes]] =
-            store.getMergeableContentHashes();
-          if (tablesHashes != myTablesHashes) {
-            send(
-              requestId,
-              fromStoreId,
-              'getTablesDelta',
-              store.getMergeableTablesHashes(),
-            );
-          }
-          if (valuesHashes != myValuesHashes) {
-            send(
-              requestId,
-              fromStoreId,
-              'getValuesDelta',
-              store.getMergeableValuesHashes(),
-            );
-          }
+          getChangesFromOtherStore(fromStoreId, true).then((changes: any) =>
+            persisterListener?.(undefined, () => changes),
+          );
         }
 
-        if (message == 'getContentDelta' && persister.isAutoSaving()) {
-          send(
-            requestId,
-            fromStoreId,
-            'contentDelta',
-            store.getMergeableContentDelta(payload),
-          );
+        if (
+          (message == 'getContentDelta' && persister.isAutoSaving()) ||
+          message == 'getContentDelta!'
+        ) {
+          const cd = store.getMergeableContentDelta(payload);
+          send(requestId, fromStoreId, 'contentDelta', cd);
         }
 
         if (message == 'getTablesDelta') {
@@ -139,72 +111,6 @@ export const createSyncPersister = ((
             store.getMergeableValuesDelta(payload),
           );
         }
-
-        if (message == 'contentDelta') {
-          if (!isUndefined(payload)) {
-            const [_time, [tablesHash, valuesHash]] = payload;
-            if (!isUndefined(tablesHash)) {
-              send(
-                requestId,
-                fromStoreId,
-                'getTablesDelta',
-                store.getMergeableTablesHashes(),
-              );
-            }
-            if (!isUndefined(valuesHash)) {
-              send(
-                requestId,
-                fromStoreId,
-                'getValuesDelta',
-                store.getMergeableValuesHashes(),
-              );
-            }
-          }
-        }
-        if (message == 'tablesDelta') {
-          const [_time, tablesChanges] = payload;
-          objForEach(tablesChanges, (_, tableId) => {
-            send(
-              requestId,
-              fromStoreId,
-              'getTableDelta',
-              store.getMergeableTableHashes(tableId),
-              [tableId],
-            );
-          });
-        }
-        if (message == 'tableDelta') {
-          const [tableId] = args;
-          const [_time, tableChanges] = payload;
-          objForEach(tableChanges, (_, rowId) => {
-            send(
-              requestId,
-              fromStoreId,
-              'getRowDelta',
-              store.getMergeableRowHashes(tableId, rowId),
-              [tableId, rowId],
-            );
-          });
-        }
-        if (message == 'rowDelta') {
-          const [tableId, rowId] = args;
-          const [time, rowChanges] = payload;
-          store.applyMergeableChanges([
-            time,
-            [
-              [time, {[tableId]: [time, {[rowId]: [time, rowChanges]}]}],
-              [EMPTY_STRING, {}],
-              1,
-            ],
-          ]);
-        }
-        if (message == 'valuesDelta') {
-          const [time, valuesChanges] = payload;
-          store.applyMergeableChanges([
-            time,
-            [[EMPTY_STRING, {}], [time, valuesChanges], 1],
-          ]);
-        }
       },
     );
   };
@@ -234,14 +140,17 @@ export const createSyncPersister = ((
       send(requestId, toStoreId, message, payload, args);
     });
 
-  const getPersisted = async (): Promise<any> => {
+  const getChangesFromOtherStore = async (
+    knownOtherStoreId: IdOrNull = null,
+    forceOtherToSave = false,
+  ): Promise<MergeableChanges> => {
     const changes: MergeableChanges = [
       EMPTY_STRING,
       [[EMPTY_STRING, {}], [EMPTY_STRING, {}], 1],
     ];
     const [contentDelta, otherStoreId] = await request<ContentDelta>(
-      null,
-      'getContentDelta',
+      knownOtherStoreId,
+      'getContentDelta' + (forceOtherToSave ? '!' : ''),
       store.getMergeableContentHashes(),
     );
     if (!isUndefined(contentDelta)) {
@@ -286,7 +195,11 @@ export const createSyncPersister = ((
         changes[1][1] = valuesStamp;
       }
     }
+    return changes;
+  };
 
+  const getPersisted = async (): Promise<any> => {
+    const changes = await getChangesFromOtherStore();
     return changes[0] != EMPTY_STRING ? changes : undefined;
   };
 
@@ -294,9 +207,10 @@ export const createSyncPersister = ((
     send(null, null, 'contentHashes', store.getMergeableContentHashes());
   };
 
-  const addPersisterListener = () => 0;
+  const addPersisterListener = (listener: PersisterListener) =>
+    (persisterListener = listener);
 
-  const delPersisterListener = () => 0;
+  const delPersisterListener = () => (persisterListener = undefined);
 
   const persister = createCustomPersister(
     store,
@@ -309,7 +223,7 @@ export const createSyncPersister = ((
     {
       getBus: () => bus,
       startSync: async () =>
-        await (await persister.startAutoSave()).startAutoLoad(),
+        await (await persister.startAutoLoad()).startAutoSave(),
       stopSync: () => persister.destroy,
     },
   ) as SyncPersister;
