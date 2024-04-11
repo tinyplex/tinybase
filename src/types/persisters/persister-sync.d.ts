4436ab06b0a71234a89f18baea65cf962c576313
James Pearce <github@tripleodeon.com>
2024-04-11 11:38:45 -0600

    [sync] Numeric message types

--- src/types/persisters/persister-sync.d.ts
+++ src/types/persisters/persister-sync.d.ts
@@ -4,11 +4,14 @@ import {Id, IdOrNull} from '../common';
 import {MergeableStore} from '../mergeable-store';
 import {Persister} from '../persisters';
 
+/// MessageType
+export type MessageType = number;
+
 /// Receive
 export type Receive = (
   requestId: IdOrNull,
   fromStoreId: Id,
-  message: string,
+  messageType: MessageType,
   ...parts: any[]
 ) => void;
 
@@ -16,7 +19,7 @@ export type Receive = (
 export type Send = (
   requestId: IdOrNull,
   toStoreId: IdOrNull,
-  message: string,
+  messageType: MessageType,
   ...parts: any[]
 ) => void;
 
