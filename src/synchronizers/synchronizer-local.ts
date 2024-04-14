0faacde4abdfe2426aeba381e97119aa906b20b7
James Pearce <github@tripleodeon.com>
2024-04-14 13:58:24 -0600

    [sync] Remove client type and consolidate stats

--- src/synchronizers/synchronizer-local.ts
+++ src/synchronizers/synchronizer-local.ts
@@ -1,22 +1,13 @@
-import {
-  Client,
-  MessageType,
-  Receive,
-  SynchronizerStats,
-} from '../types/synchronizers';
-import {DEBUG, isUndefined} from '../common/other';
 import {Id, IdOrNull} from '../types/common';
 import {IdMap, mapForEach, mapGet, mapNew, mapSet} from '../common/map';
-import {IdObj, objDel, objNew} from '../common/obj';
+import {MessageType, Receive} from '../types/synchronizers';
 import {MergeableStore} from '../types/mergeable-store';
 import {collDel} from '../common/coll';
 import {createCustomSynchronizer} from '../synchronizers';
 import {createLocalSynchronizer as createLocalSynchronizerDecl} from '../types/synchronizers/synchronizer-local';
+import {isUndefined} from '../common/other';
 
 const clients: IdMap<Receive> = mapNew();
-
-const sends: IdObj<number> = objNew();
-const receives: IdObj<number> = objNew();
 
 export const createLocalSynchronizer = ((
   store: MergeableStore,
@@ -25,16 +16,7 @@ export const createLocalSynchronizer = ((
   const clientId: Id = '' + Math.random();
 
   const onReceive = (receive: Receive): void => {
-    mapSet(
-      clients,
-      clientId,
-      DEBUG
-        ? (...args) => {
-            receives[clientId] = (receives[clientId] ?? 0) + 1;
-            receive(...args);
-          }
-        : receive,
-    );
+    mapSet(clients, clientId, receive);
   };
 
   const send = (
@@ -43,9 +25,6 @@ export const createLocalSynchronizer = ((
     messageType: MessageType,
     messageBody: any,
   ): void => {
-    if (DEBUG) {
-      sends[clientId] = (sends[clientId] ?? 0) + 1;
-    }
     isUndefined(toClientId)
       ? mapForEach(clients, (otherClientId, receive) =>
           otherClientId != clientId
@@ -62,18 +41,14 @@ export const createLocalSynchronizer = ((
 
   const destroy = (): void => {
     collDel(clients, clientId);
-    if (DEBUG) {
-      objDel(sends, clientId);
-      objDel(receives, clientId);
-    }
   };
 
-  const getStats = (): SynchronizerStats =>
-    DEBUG
-      ? {sends: sends[clientId] ?? 0, receives: receives[clientId] ?? 0}
-      : {};
-
-  const client = {send, onReceive, destroy, getStats} as Client;
-
-  return createCustomSynchronizer(store, client, 0.001, onIgnoredError);
+  return createCustomSynchronizer(
+    store,
+    send,
+    onReceive,
+    destroy,
+    0.001,
+    onIgnoredError,
+  );
 }) as typeof createLocalSynchronizerDecl;
