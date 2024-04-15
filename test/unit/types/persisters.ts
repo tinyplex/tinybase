9a529d74bc9a3d0bf3cfd7e0990c2d39d8443432
James Pearce <github@tripleodeon.com>
2024-04-15 15:32:36 -0600

    [persisters] Load and startAutoLoad take Content

--- test/unit/types/persisters.ts
+++ test/unit/types/persisters.ts
@@ -20,21 +20,21 @@ const persisterWithNoSchema = createFilePersister(createStore(), '');
 persisterWithNoSchema.getStore().getTables().t1;
 persisterWithNoSchema.getStore().getTables().t2;
 
-persisterWithSchema.load({t1: {r1: {c1: 1}}});
-persisterWithSchema.load({t1: {r1: {c1: 1}}}, {v1: 1});
-persisterWithSchema.load({t1: {r1: {c2: 1}}}); // !
-persisterWithSchema.load({t1: {r1: {c1: 'a'}}}); // !
-persisterWithSchema.load({t2: {r2: {c2: 1}}}); // !
-persisterWithSchema.load({t1: {r1: {c1: 1}}}, {v1: 'a'}); // !
-persisterWithSchema.load({t1: {r1: {c1: 1}}}, {v2: 1}); // !
+persisterWithSchema.load([{t1: {r1: {c1: 1}}}, {}]);
+persisterWithSchema.load([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+persisterWithSchema.load([{t1: {r1: {c2: 1}}}, {}]); // !
+persisterWithSchema.load([{t1: {r1: {c1: 'a'}}}, {}]); // !
+persisterWithSchema.load([{t2: {r2: {c2: 1}}}, {}]); // !
+persisterWithSchema.load([{t1: {r1: {c1: 1}}}, {v1: 'a'}]); // !
+persisterWithSchema.load([{t1: {r1: {c1: 1}}}, {v2: 1}]); // !
 
-persisterWithSchema.startAutoLoad({t1: {r1: {c1: 1}}});
-persisterWithSchema.startAutoLoad({t1: {r1: {c1: 1}}}, {v1: 1});
-persisterWithSchema.startAutoLoad({t1: {r1: {c2: 1}}}); // !
-persisterWithSchema.startAutoLoad({t1: {r1: {c1: 'a'}}}); // !
-persisterWithSchema.startAutoLoad({t2: {r2: {c2: 1}}}); // !
-persisterWithSchema.startAutoLoad({t1: {r1: {c1: 1}}}, {v1: 'a'}); // !
-persisterWithSchema.startAutoLoad({t1: {r1: {c1: 1}}}, {v2: 1}); // !
+persisterWithSchema.startAutoLoad([{t1: {r1: {c1: 1}}}, {}]);
+persisterWithSchema.startAutoLoad([{t1: {r1: {c1: 1}}}, {v1: 1}]);
+persisterWithSchema.startAutoLoad([{t1: {r1: {c2: 1}}}, {}]); // !
+persisterWithSchema.startAutoLoad([{t1: {r1: {c1: 'a'}}}, {}]); // !
+persisterWithSchema.startAutoLoad([{t2: {r2: {c2: 1}}}, {}]); // !
+persisterWithSchema.startAutoLoad([{t1: {r1: {c1: 1}}}, {v1: 'a'}]); // !
+persisterWithSchema.startAutoLoad([{t1: {r1: {c1: 1}}}, {v2: 1}]); // !
 
 persisterWithSchema.getStore().getTables().t1;
 persisterWithSchema.getStore().getTables().t2; // !
