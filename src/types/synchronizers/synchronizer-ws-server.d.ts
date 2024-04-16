b22937acb1908f26ebf37228bfcc601043471d03
James Pearce <github@tripleodeon.com>
2024-04-15 20:43:39 -0600

    [sync] Beef up WsServer

--- src/types/synchronizers/synchronizer-ws-server.d.ts
+++ src/types/synchronizers/synchronizer-ws-server.d.ts
@@ -1,10 +1,20 @@
 /// synchronizer-ws-server
 
+import {Ids} from '../common';
 import {WebSocketServer} from 'ws';
+
+/// WsServerStats
+export type WsServerStats = {
+  paths?: number;
+  clients?: number;
+};
 
 /// WsServer
 export interface WsServer {
   getWebSocketServer: () => WebSocketServer;
+  getPaths: () => string[];
+  getClientIds: (path: string) => Ids;
+  getStats: () => WsServerStats;
   destroy: () => void;
 }
 
