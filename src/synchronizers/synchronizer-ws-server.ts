bfa9a987fcad4634f2a8fd3aa668810a63896e14
James Pearce <github@tripleodeon.com>
2024-04-15 20:43:39 -0600

    [sync] Beef up WsServer

--- src/synchronizers/synchronizer-ws-server.ts
+++ src/synchronizers/synchronizer-ws-server.ts
@@ -1,51 +1,91 @@
 import {EMPTY_STRING, UTF8} from '../common/strings';
-import {IdMap, mapForEach, mapGet, mapNew, mapSet} from '../common/map';
+import {Id, Ids} from '../types/common';
+import {
+  IdMap2,
+  mapEnsure,
+  mapForEach,
+  mapGet,
+  mapKeys,
+  mapNew,
+  mapSet,
+} from '../common/map';
 import {WebSocket, WebSocketServer} from 'ws';
-import {collClear, collDel} from '../common/coll';
-import {Id} from '../types/common';
+import {
+  WsServerStats,
+  createWsServer as createWsServerDecl,
+} from '../types/synchronizers/synchronizer-ws-server';
+import {
+  collClear,
+  collDel,
+  collIsEmpty,
+  collSize,
+  collSize2,
+} from '../common/coll';
+import {ifNotUndefined, slice} from '../common/other';
 import {MESSAGE_SEPARATOR} from './common';
-import {createWsServer as createWsServerDecl} from '../types/synchronizers/synchronizer-ws-server';
-import {slice} from '../common/other';
+
+const PATH_REGEX = /\/([^?]*)/;
 
 export const createWsServer = ((webSocketServer: WebSocketServer) => {
-  const clients: IdMap<WebSocket> = mapNew();
-  webSocketServer.on('connection', (webSocket, request) => {
-    const clientId: Id = request.headers['sec-websocket-key']!;
-    mapSet(clients, clientId, webSocket);
+  const clientsByPath: IdMap2<WebSocket> = mapNew();
+  webSocketServer.on('connection', (webSocket, request) =>
+    ifNotUndefined(request.url?.match(PATH_REGEX), ([path]) =>
+      ifNotUndefined(request.headers['sec-websocket-key'], (clientId) => {
+        const clients = mapEnsure(clientsByPath, path, mapNew<Id, WebSocket>);
+        mapSet(clients, clientId, webSocket);
 
-    webSocket.on('message', (data) => {
-      const payload = data.toString(UTF8);
-      const splitAt = payload.indexOf(MESSAGE_SEPARATOR);
-      if (splitAt !== -1) {
-        const toClientId = slice(payload, 0, splitAt);
-        const message = slice(payload, splitAt + 1);
+        webSocket.on('message', (data) => {
+          const payload = data.toString(UTF8);
+          const splitAt = payload.indexOf(MESSAGE_SEPARATOR);
+          if (splitAt !== -1) {
+            const toClientId = slice(payload, 0, splitAt);
+            const message = slice(payload, splitAt + 1);
+            toClientId === EMPTY_STRING
+              ? mapForEach(clients, (otherClientId, otherWebSocket) =>
+                  otherClientId != clientId
+                    ? otherWebSocket.send(
+                        clientId + MESSAGE_SEPARATOR + message,
+                      )
+                    : 0,
+                )
+              : mapGet(clients, toClientId)?.send(
+                  clientId + MESSAGE_SEPARATOR + message,
+                );
+          }
+        });
 
-        toClientId === EMPTY_STRING
-          ? mapForEach(clients, (otherClientId, otherWebSocket) =>
-              otherClientId != clientId
-                ? otherWebSocket.send(clientId + MESSAGE_SEPARATOR + message)
-                : 0,
-            )
-          : mapGet(clients, toClientId)?.send(
-              clientId + MESSAGE_SEPARATOR + message,
-            );
-      }
-    });
-
-    webSocket.on('close', () => {
-      collDel(clients, clientId);
-    });
-  });
+        webSocket.on('close', () => {
+          collDel(clients, clientId);
+          if (collIsEmpty(clients)) {
+            collDel(clientsByPath, path);
+          }
+        });
+      }),
+    ),
+  );
 
   const getWebSocketServer = () => webSocketServer;
 
+  const getPaths = (): string[] => mapKeys(clientsByPath);
+
+  const getClientIds = (path: string): Ids =>
+    mapKeys(mapGet(clientsByPath, path));
+
+  const getStats = (): WsServerStats => ({
+    paths: collSize(clientsByPath),
+    clients: collSize2(clientsByPath),
+  });
+
   const destroy = () => {
     webSocketServer.close();
-    collClear(clients);
+    collClear(clientsByPath);
   };
 
   return {
     getWebSocketServer,
+    getPaths,
+    getClientIds,
+    getStats,
     destroy,
   };
 }) as typeof createWsServerDecl;
