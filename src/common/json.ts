ec5d9d6d701ea29c5e05acbaae7db6a7b20177c4
James Pearce <github@tripleodeon.com>
2024-04-17 10:00:48 -0600

    [sync] Synchronize deletions

--- src/common/json.ts
+++ src/common/json.ts
@@ -1,9 +1,21 @@
 import {isInstanceOf} from './other';
 import {object} from './obj';
 
+const UNDEFINED_MARKER = '\uFFFC';
+
 export const jsonString = (obj: unknown): string =>
   JSON.stringify(obj, (_key, value) =>
     isInstanceOf(value, Map) ? object.fromEntries([...value]) : value,
   );
 
 export const jsonParse = JSON.parse;
+
+export const jsonStringWithUndefined = (obj: unknown): string =>
+  JSON.stringify(obj, (_key, value) =>
+    value === undefined ? UNDEFINED_MARKER : value,
+  );
+
+export const jsonParseWithUndefined = (str: string): unknown =>
+  JSON.parse(str, (_key, value) =>
+    value === UNDEFINED_MARKER ? undefined : value,
+  );
