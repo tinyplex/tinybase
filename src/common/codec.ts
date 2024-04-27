ac43e381c5eeeb712070ea449a3508626b5500cf
James Pearce <github@tripleodeon.com>
2024-04-27 08:58:18 -0600

    [hlc] Encode from characters

---
+++ src/common/codec.ts
@@ -0,0 +1,15 @@
+import {mapGet, mapNew} from './map';
+import {EMPTY_STRING} from './strings';
+import {arrayMap} from './array';
+
+const MASK6 = 63;
+const ENCODE =
+  '0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmno'.split(
+    EMPTY_STRING,
+  );
+const DECODE = mapNew(arrayMap(ENCODE, (char, index) => [char, index])) as any;
+
+export const encode = (num: number): string => ENCODE[num & MASK6];
+
+export const decode = (str: string, pos: number): number =>
+  mapGet(DECODE, str[pos]) ?? 0;
