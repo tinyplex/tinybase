849386842c28822f823a025791cddc5809dda25f
James Pearce <github@tripleodeon.com>
2024-04-27 10:23:17 -0600

    [common] getUniqueId

--- src/common.ts
+++ src/common.ts
@@ -1,4 +1,13 @@
-import {SortKey} from './types/common.d';
+import {Id, SortKey} from './types/common.d';
+import {arrayReduce} from './common/array';
+import {encode} from './common/codec';
 
 export const defaultSorter = (sortKey1: SortKey, sortKey2: SortKey): number =>
   (sortKey1 ?? 0) < (sortKey2 ?? 0) ? -1 : 1;
+
+export const getUniqueId = (): Id =>
+  arrayReduce<number, Id>(
+    crypto.getRandomValues(new Uint8Array(16)),
+    (uniqueId, number) => uniqueId + encode(number),
+    '',
+  );
