a0805ad293767a2e5d180e3d4111ac908127a004
James Pearce <github@tripleodeon.com>
2024-06-09 18:04:38 -0500

    [package] Exports field

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,12 +1,15 @@
 /// index
 
-export type * from './checkpoints/index.d.ts';
-export type * from './common/index.d.ts';
-export type * from './indexes/index.d.ts';
-export type * from './metrics/index.d.ts';
-export type * from './persisters/index.d.ts';
-export type * from './synchronizers/index.d.ts';
-export type * from './queries/index.d.ts';
-export type * from './relationships/index.d.ts';
-export type * from './mergeable-store/index.d.ts';
-export type * from './store/index.d.ts';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
+export * from './checkpoints/index.d.ts';
+export * from './common/index.d.ts';
+export * from './indexes/index.d.ts';
+export * from './metrics/index.d.ts';
+export * from './persisters/index.d.ts';
+export * from './synchronizers/index.d.ts';
+export * from './queries/index.d.ts';
+export * from './relationships/index.d.ts';
+export * from './mergeable-store/index.d.ts';
+export * from './store/index.d.ts';
