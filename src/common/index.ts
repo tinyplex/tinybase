6989b9a80d07400dccbbbf423b3d036161b090d2
James Pearce <github@tripleodeon.com>
2024-06-16 17:21:53 -0500

    [expo] Fallback random values for React Native

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,13 +1,21 @@
 import type {Id, SortKey} from '../@types/common/index.d.ts';
-import {arrayReduce} from './array.ts';
+import {arrayMap, arrayReduce} from './array.ts';
+import {math, mathFloor} from './other.ts';
 import {encode} from './codec.ts';
+
+// Fallback is not cryptographically secure but tolerable for ReactNative UUIDs.
+const getRandomValues = crypto.getRandomValues
+  ? (array: Uint8Array): Uint8Array => crypto.getRandomValues(array)
+  : /*! istanbul ignore next */
+    (array: Uint8Array): Uint8Array =>
+      arrayMap(array as any, () => mathFloor(math.random() * 256)) as any;
 
 export const defaultSorter = (sortKey1: SortKey, sortKey2: SortKey): number =>
   (sortKey1 ?? 0) < (sortKey2 ?? 0) ? -1 : 1;
 
 export const getUniqueId = (length = 16): Id =>
   arrayReduce<number, Id>(
-    crypto.getRandomValues(new Uint8Array(length)),
+    getRandomValues(new Uint8Array(length)) as any,
     (uniqueId, number) => uniqueId + encode(number),
     '',
   );
