a5cdb1249990c5864370234c9143f8e6ea60cd7c
James Pearce <github@tripleodeon.com>
2024-07-13 11:21:24 -0500

    [sync] Broadcast onSend and onReceive

--- src/@types/synchronizers/synchronizer-broadcast-channel/docs.js
+++ src/@types/synchronizers/synchronizer-broadcast-channel/docs.js
@@ -57,8 +57,17 @@
  * As well as providing a reference to the MergeableStore to persist, you must
  * provide a channel name, used by all the browser tabs, workers, or contexts
  * that need to synchronize together.
+ *
+ * A final set of optional handlers can be provided to help debug sends,
+ * receives, and errors respectively.
  * @param store The MergeableStore to synchronize.
  * @param channelName The name of the channel to use.
+ * @param onSend An optional handler for the messages that this Synchronizer
+ * sends. This is suitable for debugging synchronization issues in a development
+ * environment, since v5.1.
+ * @param onReceive An optional handler for the messages that this Synchronizer
+ * receives. This is suitable for debugging synchronization issues in a
+ * development environment, since v5.1.
  * @param onIgnoredError An optional handler for the errors that the
  * Synchronizer would otherwise ignore when trying to synchronize data. This is
  * suitable for debugging synchronization issues in a development environment.
