86433af85cc13ff3c726131cbe32546667967eeb
James Pearce <github@tripleodeon.com>
2024-07-27 14:43:14 -0500

    [pg] PG common folder

---
+++ src/persisters/common/pg/create.ts
@@ -0,0 +1,52 @@
+import type {
+  DatabasePersisterConfig,
+  PersistedStore,
+  Persister,
+  Persists,
+} from '../../../@types/persisters/index.d.ts';
+import {Cmd} from './commands.ts';
+import {collValues} from '../../../common/coll.ts';
+import {createJsonPgPersister} from './json.ts';
+import {createTabularPgPersister} from './tabular.ts';
+import {getConfigStructures} from './config.ts';
+
+export const createPgPersister = <
+  Persist extends Persists = Persists.StoreOnly,
+>(
+  store: PersistedStore<Persist>,
+  configOrStoreTableName: DatabasePersisterConfig | string | undefined,
+  cmd: Cmd,
+  _addUpdateListener: () => 0,
+  _delUpdateListener: () => void,
+  onSqlCommand: ((sql: string, args?: any[]) => void) | undefined,
+  onIgnoredError: ((error: any) => void) | undefined,
+  persist: Persist,
+  thing: any,
+  getThing = 'getDb',
+): Persister<Persist> => {
+  const [isJson, , defaultedConfig, managedTableNamesSet] = getConfigStructures(
+    configOrStoreTableName,
+  );
+
+  const addPersisterListener = (): 0 => 0;
+
+  const delPersisterListener = (): void => {};
+
+  return (isJson ? createJsonPgPersister : createTabularPgPersister)(
+    store,
+    onSqlCommand
+      ? async (sql, args) => {
+          onSqlCommand(sql, args);
+          return await cmd(sql, args);
+        }
+      : cmd,
+    addPersisterListener,
+    delPersisterListener,
+    onIgnoredError,
+    persist,
+    defaultedConfig as any,
+    collValues(managedTableNamesSet),
+    thing,
+    getThing,
+  );
+};
---
+++ src/persisters/common/pg/json.ts
@@ -0,0 +1,78 @@
+import {Cmd, getCommandFunctions} from './commands.ts';
+import type {
+  PersistedContent,
+  PersistedStore,
+  Persister,
+  PersisterListener,
+  Persists,
+} from '../../../@types/persisters/index.d.ts';
+import {
+  jsonParseWithUndefined,
+  jsonStringWithUndefined,
+} from '../../../common/json.ts';
+import {DefaultedJsonConfig} from './config.ts';
+import {SINGLE_ROW_ID} from './common.ts';
+import {createCustomPersister} from '../../index.ts';
+
+export const createJsonPgPersister = <
+  ListeningHandle,
+  Persist extends Persists = Persists.StoreOnly,
+>(
+  store: PersistedStore<Persist>,
+  cmd: Cmd,
+  addPersisterListener: (
+    listener: PersisterListener<Persist>,
+  ) => ListeningHandle,
+  delPersisterListener: (listeningHandle: ListeningHandle) => void,
+  onIgnoredError: ((error: any) => void) | undefined,
+  persist: Persist,
+  [storeTableName, storeIdColumnName, storeColumnName]: DefaultedJsonConfig,
+  managedTableNames: string[],
+  thing: any,
+  getThing: string,
+): Persister<Persist> => {
+  const [refreshSchema, loadTable, saveTable, transaction] =
+    getCommandFunctions(cmd, managedTableNames, onIgnoredError);
+
+  const getPersisted = async (): Promise<PersistedContent<Persist>> =>
+    await transaction(async () => {
+      await refreshSchema();
+      return jsonParseWithUndefined(
+        ((await loadTable(storeTableName, storeIdColumnName))[SINGLE_ROW_ID]?.[
+          storeColumnName
+        ] as string) ?? 'null',
+      );
+    });
+
+  const setPersisted = async (
+    getContent: () => PersistedContent<Persist>,
+  ): Promise<void> =>
+    await transaction(async () => {
+      await refreshSchema();
+      await saveTable(
+        storeTableName,
+        storeIdColumnName,
+        {
+          [SINGLE_ROW_ID]: {
+            [storeColumnName]: jsonStringWithUndefined(getContent() ?? null),
+          },
+        },
+        true,
+        true,
+      );
+    });
+
+  const persister: any = createCustomPersister(
+    store,
+    getPersisted,
+    setPersisted,
+    addPersisterListener,
+    delPersisterListener,
+    onIgnoredError,
+    persist,
+    {[getThing]: () => thing},
+    thing,
+  );
+
+  return persister;
+};
