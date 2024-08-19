5b7bc6eefb44e16745ec5ab30b8a5b1e5886b1d9
James Pearce <github@tripleodeon.com>
2024-08-18 23:30:29 -0500

    [pg] Triggers tolerate table drop/create

--- src/persisters/persister-postgres/index.ts
+++ src/persisters/persister-postgres/index.ts
@@ -7,15 +7,16 @@ import {
   UpdateListener,
   createPostgresqlPersister,
 } from '../common/database/postgresql.ts';
+import {arrayForEach, arrayMap} from '../../common/array.ts';
+import {collHas, collValues} from '../../common/coll.ts';
 import type {DatabasePersisterConfig} from '../../@types/persisters/index.d.ts';
 import type {MergeableStore} from '../../@types/mergeable-store/index.d.ts';
 import type {Store} from '../../@types/store/index.d.ts';
 import {TINYBASE} from '../../common/strings.ts';
-import {arrayMap} from '../../common/array.ts';
-import {collValues} from '../../common/coll.ts';
 import {promiseAll} from '../../common/other.ts';
 
-const TRIGGER = TINYBASE;
+const DATA_TRIGGER = TINYBASE + '_data';
+const TABLE_TRIGGER = TINYBASE + '_table';
 
 export const createPostgresPersister = (async (
   store: Store | MergeableStore,
@@ -25,6 +26,12 @@ export const createPostgresPersister = (async (
   onIgnoredError?: (error: any) => void,
 ): Promise<PostgresPersister> => {
   const cmdSql = await sql.reserve?.();
+
+  const addDataTrigger = async (tableName: string) => {
+    // eslint-disable-next-line max-len
+    await sql`CREATE OR REPLACE TRIGGER ${sql(DATA_TRIGGER + '_' + tableName)} AFTER INSERT OR UPDATE OR DELETE ON ${sql(tableName)} EXECUTE FUNCTION ${sql(DATA_TRIGGER)}()`;
+  };
+
   return createPostgresqlPersister(
     store,
     configOrStoreTableName,
@@ -32,28 +39,42 @@ export const createPostgresPersister = (async (
     async (
       listener: UpdateListener,
       managedTableNamesSet: Set<string>,
-    ): Promise<ListenMeta> => {
-      const listen = await sql.listen(TRIGGER, (value: string) =>
-        listener?.(value),
-      );
+    ): Promise<[ListenMeta, ListenMeta]> => {
       // eslint-disable-next-line max-len
-      await sql`CREATE OR REPLACE FUNCTION ${sql(TRIGGER)}()RETURNS trigger AS $t$ BEGIN PERFORM pg_notify('${sql.unsafe(TRIGGER)}',TG_TABLE_NAME);RETURN NULL;END;$t$ LANGUAGE plpgsql;`;
+      await sql`CREATE OR REPLACE FUNCTION ${sql(TABLE_TRIGGER)}()RETURNS event_trigger AS $t2$ DECLARE row record; BEGIN FOR row IN SELECT object_identity FROM pg_event_trigger_ddl_commands()WHERE command_tag='CREATE TABLE' LOOP PERFORM pg_notify('${sql.unsafe(TABLE_TRIGGER)}',SPLIT_PART(row.object_identity,'.',2));END LOOP;END;$t2$ LANGUAGE plpgsql;`;
+      try {
+        // eslint-disable-next-line max-len
+        await sql`CREATE EVENT TRIGGER ${sql(TABLE_TRIGGER)} ON ddl_command_end WHEN TAG IN('CREATE TABLE')EXECUTE FUNCTION ${sql(TABLE_TRIGGER)}();`;
+      } catch {}
+
+      // eslint-disable-next-line max-len
+      await sql`CREATE OR REPLACE FUNCTION ${sql(DATA_TRIGGER)}()RETURNS trigger AS $t1$ BEGIN PERFORM pg_notify('${sql.unsafe(DATA_TRIGGER)}',TG_TABLE_NAME);RETURN NULL;END;$t1$ LANGUAGE plpgsql;`;
       await promiseAll(
         arrayMap(collValues(managedTableNamesSet), async (tableName) => {
           // eslint-disable-next-line max-len
           await sql`CREATE TABLE IF NOT EXISTS ${sql(tableName)}("_id"text PRIMARY KEY)`;
-          // eslint-disable-next-line max-len
-          await sql`CREATE OR REPLACE TRIGGER ${sql(TRIGGER + tableName)} AFTER INSERT OR UPDATE OR DELETE ON ${sql(tableName)} EXECUTE FUNCTION ${sql(TRIGGER)}()`;
+          await addDataTrigger(tableName);
         }),
       );
-      return listen;
+
+      return [
+        await sql.listen(TABLE_TRIGGER, async (tableName) => {
+          if (collHas(managedTableNamesSet, tableName)) {
+            await addDataTrigger(tableName);
+            listener?.(tableName);
+          }
+        }),
+        await sql.listen(DATA_TRIGGER, listener),
+      ];
     },
-    (listen: ListenMeta) => listen.unlisten().catch(() => 0),
+    (listeners: [ListenMeta, ListenMeta]) =>
+      arrayForEach(listeners, (listener) => listener.unlisten().catch(() => 0)),
     onSqlCommand,
     onIgnoredError,
     () => cmdSql?.release?.(),
     3, // StoreOrMergeableStore,
     sql,
+
     'getSql',
   ) as PostgresPersister;
 }) as typeof createPostgresPersisterDecl;
