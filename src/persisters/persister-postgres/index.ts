52975680490eb94e0529591256e8fd206813c462
James Pearce <github@tripleodeon.com>
2024-08-19 09:36:43 -0500

    [pg] Consolidate channel listener

--- src/persisters/persister-postgres/index.ts
+++ src/persisters/persister-postgres/index.ts
@@ -7,16 +7,19 @@ import {
   UpdateListener,
   createPostgresqlPersister,
 } from '../common/database/postgresql.ts';
-import {arrayForEach, arrayMap} from '../../common/array.ts';
 import {collHas, collValues} from '../../common/coll.ts';
+import {ifNotUndefined, promiseAll} from '../../common/other.ts';
 import type {DatabasePersisterConfig} from '../../@types/persisters/index.d.ts';
 import type {MergeableStore} from '../../@types/mergeable-store/index.d.ts';
 import type {Store} from '../../@types/store/index.d.ts';
 import {TINYBASE} from '../../common/strings.ts';
-import {promiseAll} from '../../common/other.ts';
+import {arrayMap} from '../../common/array.ts';
 
-const DATA_TRIGGER = TINYBASE + '_data';
-const TABLE_TRIGGER = TINYBASE + '_table';
+const EVENT_CHANNEL = TINYBASE;
+const EVENT_REGEX = /^([cd]:)(.+)/;
+
+const CHANGE_DATA_TRIGGER = TINYBASE + '_data';
+const CREATE_TABLE_TRIGGER = TINYBASE + '_table';
 
 export const createPostgresPersister = (async (
   store: Store | MergeableStore,
@@ -29,7 +32,7 @@ export const createPostgresPersister = (async (
 
   const addDataTrigger = async (tableName: string) => {
     // eslint-disable-next-line max-len
-    await sql`CREATE OR REPLACE TRIGGER ${sql(DATA_TRIGGER + '_' + tableName)} AFTER INSERT OR UPDATE OR DELETE ON ${sql(tableName)} EXECUTE FUNCTION ${sql(DATA_TRIGGER)}()`;
+    await sql`CREATE OR REPLACE TRIGGER ${sql(CHANGE_DATA_TRIGGER + '_' + tableName)} AFTER INSERT OR UPDATE OR DELETE ON ${sql(tableName)} EXECUTE FUNCTION ${sql(CHANGE_DATA_TRIGGER)}()`;
   };
 
   return createPostgresqlPersister(
@@ -39,16 +42,17 @@ export const createPostgresPersister = (async (
     async (
       listener: UpdateListener,
       managedTableNamesSet: Set<string>,
-    ): Promise<[ListenMeta, ListenMeta]> => {
+    ): Promise<ListenMeta> => {
       // eslint-disable-next-line max-len
-      await sql`CREATE OR REPLACE FUNCTION ${sql(TABLE_TRIGGER)}()RETURNS event_trigger AS $t2$ DECLARE row record; BEGIN FOR row IN SELECT object_identity FROM pg_event_trigger_ddl_commands()WHERE command_tag='CREATE TABLE' LOOP PERFORM pg_notify('${sql.unsafe(TABLE_TRIGGER)}',SPLIT_PART(row.object_identity,'.',2));END LOOP;END;$t2$ LANGUAGE plpgsql;`;
-      try {
-        // eslint-disable-next-line max-len
-        await sql`CREATE EVENT TRIGGER ${sql(TABLE_TRIGGER)} ON ddl_command_end WHEN TAG IN('CREATE TABLE')EXECUTE FUNCTION ${sql(TABLE_TRIGGER)}();`;
-      } catch {}
+      await sql`CREATE OR REPLACE FUNCTION ${sql(CREATE_TABLE_TRIGGER)}()RETURNS event_trigger AS $t2$ DECLARE row record; BEGIN FOR row IN SELECT object_identity FROM pg_event_trigger_ddl_commands()WHERE command_tag='CREATE TABLE' LOOP PERFORM pg_notify('${sql.unsafe(EVENT_CHANNEL)}','c:'+SPLIT_PART(row.object_identity,'.',2));END LOOP;END;$t2$ LANGUAGE plpgsql;`;
 
       // eslint-disable-next-line max-len
-      await sql`CREATE OR REPLACE FUNCTION ${sql(DATA_TRIGGER)}()RETURNS trigger AS $t1$ BEGIN PERFORM pg_notify('${sql.unsafe(DATA_TRIGGER)}',TG_TABLE_NAME);RETURN NULL;END;$t1$ LANGUAGE plpgsql;`;
+      await sql`CREATE EVENT TRIGGER ${sql(CREATE_TABLE_TRIGGER)} ON ddl_command_end WHEN TAG IN('CREATE TABLE')EXECUTE FUNCTION ${sql(CREATE_TABLE_TRIGGER)}();`.catch(
+        onIgnoredError,
+      );
+
+      // eslint-disable-next-line max-len
+      await sql`CREATE OR REPLACE FUNCTION ${sql(CHANGE_DATA_TRIGGER)}()RETURNS trigger AS $t1$ BEGIN PERFORM pg_notify('${sql.unsafe(EVENT_CHANNEL)}','d:'+TG_TABLE_NAME);RETURN NULL;END;$t1$ LANGUAGE plpgsql;`;
       await promiseAll(
         arrayMap(collValues(managedTableNamesSet), async (tableName) => {
           // eslint-disable-next-line max-len
@@ -57,18 +61,25 @@ export const createPostgresPersister = (async (
         }),
       );
 
-      return [
-        await sql.listen(TABLE_TRIGGER, async (tableName) => {
-          if (collHas(managedTableNamesSet, tableName)) {
-            await addDataTrigger(tableName);
-            listener?.(tableName);
-          }
-        }),
-        await sql.listen(DATA_TRIGGER, listener),
-      ];
+      return await sql.listen(
+        EVENT_CHANNEL,
+        async (prefixAndTableName) =>
+          await ifNotUndefined(
+            prefixAndTableName.match(EVENT_REGEX),
+            async ([, eventType, tableName]) => {
+              if (
+                eventType == 'c:' &&
+                collHas(managedTableNamesSet, tableName)
+              ) {
+                await addDataTrigger(tableName);
+              }
+              listener?.(tableName);
+            },
+          ),
+      );
     },
-    (listeners: [ListenMeta, ListenMeta]) =>
-      arrayForEach(listeners, (listener) => listener.unlisten().catch(() => 0)),
+    (eventListener: ListenMeta) =>
+      eventListener.unlisten().catch(onIgnoredError),
     onSqlCommand,
     onIgnoredError,
     () => cmdSql?.release?.(),
