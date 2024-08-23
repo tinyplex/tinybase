d4d770553b7db652c8ff849f49fa1340338cd3c1
James Pearce <github@tripleodeon.com>
2024-08-23 09:00:14 -0500

    [pg] PGLite basics

---
+++ src/@types/persisters/persister-pglite/with-schemas/index.d.ts
@@ -0,0 +1,29 @@
+/// persister-pglite
+
+import type {
+  DatabasePersisterConfig,
+  Persister,
+  Persists,
+} from '../../with-schemas/index.d.ts';
+import type {
+  OptionalSchemas,
+  Store,
+} from '../../../store/with-schemas/index.d.ts';
+import type {MergeableStore} from '../../../mergeable-store/with-schemas/index.d.ts';
+import type {PGlite} from '@electric-sql/pglite';
+
+/// PglitePersister
+export interface PglitePersister<Schemas extends OptionalSchemas>
+  extends Persister<Schemas, Persists.StoreOrMergeableStore> {
+  /// PglitePersister.getPglite
+  getPglite(): PGlite;
+}
+
+/// createPglitePersister
+export function createPglitePersister<Schemas extends OptionalSchemas>(
+  store: Store<Schemas> | MergeableStore<Schemas>,
+  pglite: PGlite,
+  configOrStoreTableName?: DatabasePersisterConfig<Schemas> | string,
+  onSqlCommand?: (sql: string, args?: any[]) => void,
+  onIgnoredError?: (error: any) => void,
+): Promise<PglitePersister<Schemas>>;
