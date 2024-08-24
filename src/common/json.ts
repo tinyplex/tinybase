a130b1feabe4af391b6c06ff9b76a04f2f035013
James Pearce <github@tripleodeon.com>
2024-08-24 10:20:01 -0500

    [common] More json

--- src/common/json.ts
+++ src/common/json.ts
@@ -2,17 +2,16 @@ import {UNDEFINED} from './strings.ts';
 import {isInstanceOf} from './other.ts';
 import {object} from './obj.ts';
 
+export const jsonString = JSON.stringify;
+export const jsonParse = JSON.parse;
+
 export const jsonStringWithMap = (obj: unknown): string =>
-  JSON.stringify(obj, (_key, value) =>
+  jsonString(obj, (_key, value) =>
     isInstanceOf(value, Map) ? object.fromEntries([...value]) : value,
   );
 
-export const jsonParse = JSON.parse;
-
 export const jsonStringWithUndefined = (obj: unknown): string =>
-  JSON.stringify(obj, (_key, value) =>
-    value === undefined ? UNDEFINED : value,
-  );
+  jsonString(obj, (_key, value) => (value === undefined ? UNDEFINED : value));
 
 export const jsonParseWithUndefined = (str: string): any =>
-  JSON.parse(str, (_key, value) => (value === UNDEFINED ? undefined : value));
+  jsonParse(str, (_key, value) => (value === UNDEFINED ? undefined : value));
