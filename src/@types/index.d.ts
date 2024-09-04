bdf35a7d1c9ce0e2503aa4d0badd5fa0c980d73d
James Pearce <github@tripleodeon.com>
2024-09-03 23:09:45 -0500

    [types] Remove missing modules

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -7,8 +7,6 @@ export * from './checkpoints/index.d.ts';
 export * from './common/index.d.ts';
 export * from './indexes/index.d.ts';
 export * from './metrics/index.d.ts';
-export * from './persisters/index.d.ts';
-export * from './synchronizers/index.d.ts';
 export * from './queries/index.d.ts';
 export * from './relationships/index.d.ts';
 export * from './mergeable-store/index.d.ts';
--- src/@types/with-schemas/index.d.ts
+++ src/@types/with-schemas/index.d.ts
@@ -7,8 +7,6 @@ export * from '../checkpoints/with-schemas/index.d.ts';
 export * from '../common/with-schemas/index.d.ts';
 export * from '../indexes/with-schemas/index.d.ts';
 export * from '../metrics/with-schemas/index.d.ts';
-export * from '../persisters/with-schemas/index.d.ts';
-export * from '../synchronizers/with-schemas/index.d.ts';
 export * from '../queries/with-schemas/index.d.ts';
 export * from '../relationships/with-schemas/index.d.ts';
 export * from '../mergeable-store/with-schemas/index.d.ts';
