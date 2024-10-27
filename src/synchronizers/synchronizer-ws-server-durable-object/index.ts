a0c60369c662fa5dcf26256e178db63df1e7a900
James Pearce <github@tripleodeon.com>
2024-10-27 13:43:45 -0500

    [do] Initial implementation

--- src/synchronizers/synchronizer-ws-server-durable-object/index.ts
+++ src/synchronizers/synchronizer-ws-server-durable-object/index.ts
@@ -1,3 +1,18 @@
+import type {Connection, WSMessage} from 'partyserver';
+import {createRawPayload, ifPayloadValid} from '../common.ts';
+import {EMPTY_STRING} from '../../common/strings.ts';
 import {Server} from 'partyserver';
 
-export class WsServerDurableObject extends Server {}
+export class WsServerDurableObject extends Server {
+  onMessage(connection: Connection, message: WSMessage) {
+    const payload = message.toString();
+    ifPayloadValid(payload, (toClientId, remainder) => {
+      const forwardedPayload = createRawPayload(connection.id, remainder);
+      if (toClientId === EMPTY_STRING) {
+        this.broadcast(forwardedPayload, [connection.id]);
+      } else {
+        this.getConnection(toClientId)?.send(forwardedPayload);
+      }
+    });
+  }
+}
