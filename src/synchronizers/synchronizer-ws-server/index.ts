839513863c5a7b029b859041fcb4af2122188427
James Pearce <github@tripleodeon.com>
2024-10-29 22:16:15 -0500

    [hygiene] Renames

--- src/synchronizers/synchronizer-ws-server/index.ts
+++ src/synchronizers/synchronizer-ws-server/index.ts
@@ -160,10 +160,8 @@ export const createWsServer = (<
           if (clientId !== SERVER_CLIENT_ID) {
             serverClient[Sc.Send]?.(forwardedPayload);
           }
-          mapForEach(clients, (otherClientId, otherWebSocket) =>
-            otherClientId !== clientId
-              ? otherWebSocket.send(forwardedPayload)
-              : 0,
+          mapForEach(clients, (otherClientId, otherClient) =>
+            otherClientId !== clientId ? otherClient.send(forwardedPayload) : 0,
           );
         } else if (toClientId === SERVER_CLIENT_ID) {
           serverClient[Sc.Send]?.(forwardedPayload);
@@ -172,7 +170,7 @@ export const createWsServer = (<
         }
       });
 
-  webSocketServer.on('connection', (webSocket, request) =>
+  webSocketServer.on('connection', (client, request) =>
     ifNotUndefined(strMatch(request.url, PATH_REGEX), ([, pathId]) =>
       ifNotUndefined(request.headers['sec-websocket-key'], async (clientId) => {
         const clients = mapEnsure(clientsByPath, pathId, mapNew<Id, WebSocket>);
@@ -191,10 +189,10 @@ export const createWsServer = (<
           callListeners(pathIdListeners, undefined, pathId, 1);
           await configureServerClient(serverClient, pathId, clients);
         }
-        mapSet(clients, clientId, webSocket);
+        mapSet(clients, clientId, client);
         callListeners(clientIdListeners, [pathId], clientId, 1);
 
-        webSocket.on('message', (data) => {
+        client.on('message', (data) => {
           const payload = data.toString(UTF8);
           if (serverClient[Sc.State] == ScState.Ready) {
             messageHandler(payload);
@@ -209,7 +207,7 @@ export const createWsServer = (<
           serverClient[Sc.Buffer] = [];
         }
 
-        webSocket.on('close', () => {
+        client.on('close', () => {
           collDel(clients, clientId);
           callListeners(clientIdListeners, [pathId], clientId, -1);
           if (collIsEmpty(clients)) {
