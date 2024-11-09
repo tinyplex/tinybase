988f96b2175aafadfc37806a17ba01c39fc57140
James Pearce <github@tripleodeon.com>
2024-11-09 16:18:03 -0500

    [do] New types and docs

--- src/@types/synchronizers/synchronizer-ws-server-durable-object/index.d.ts
+++ src/@types/synchronizers/synchronizer-ws-server-durable-object/index.d.ts
@@ -1,10 +1,20 @@
 /// synchronizer-ws-server-durable-object
 
 import type {Id, IdAddedOrRemoved, Ids} from '../../index.d.ts';
+import type {Persister, Persists} from '../../persisters/index.d.ts';
 import {DurableObject} from 'cloudflare:workers';
 
 /// WsServerDurableObject
 export class WsServerDurableObject<Env = unknown> extends DurableObject<Env> {
+  /// WsServerDurableObject.createPersister
+  createPersister():
+    | Persister<Persists.MergeableStoreOnly>
+    | Promise<Persister<Persists.MergeableStoreOnly>>
+    | undefined;
+
+  /// WsServerDurableObject.getPathId
+  getPathId(): Id;
+
   /// WsServerDurableObject.getClientIds
   getClientIds(pathId: Id): Ids;
 
@@ -13,6 +23,9 @@ export class WsServerDurableObject<Env = unknown> extends DurableObject<Env> {
 
   /// WsServerDurableObject.onClientId
   onClientId(pathId: Id, clientId: Id, addedOrRemoved: IdAddedOrRemoved): void;
+
+  /// WsServerDurableObject.onMessage
+  onMessage(_fromClientId: Id, _toClientId: Id, _remainder: string): void;
 }
 
 /// getWsServerDurableObjectFetch
--- src/@types/synchronizers/synchronizer-ws-server-durable-object/with-schemas/index.d.ts
+++ src/@types/synchronizers/synchronizer-ws-server-durable-object/with-schemas/index.d.ts
@@ -1,10 +1,31 @@
 /// synchronizer-ws-server-durable-object
 
-import type {Id, IdAddedOrRemoved, Ids} from '../../../with-schemas/index.d.ts';
+import type {
+  Id,
+  IdAddedOrRemoved,
+  Ids,
+  OptionalSchemas,
+} from '../../../with-schemas/index.d.ts';
+import type {
+  Persister,
+  Persists,
+} from '../../../persisters/with-schemas/index.d.ts';
 import {DurableObject} from 'cloudflare:workers';
 
 /// WsServerDurableObject
-export class WsServerDurableObject<Env = unknown> extends DurableObject<Env> {
+export class WsServerDurableObject<
+  Schemas extends OptionalSchemas,
+  Env = unknown,
+> extends DurableObject<Env> {
+  /// WsServerDurableObject.createPersister
+  createPersister():
+    | Persister<Schemas, Persists.MergeableStoreOnly>
+    | Promise<Persister<Schemas, Persists.MergeableStoreOnly>>
+    | undefined;
+
+  /// WsServerDurableObject.getPathId
+  getPathId(): Id;
+
   /// WsServerDurableObject.getClientIds
   getClientIds(pathId: Id): Ids;
 
@@ -13,14 +34,22 @@ export class WsServerDurableObject<Env = unknown> extends DurableObject<Env> {
 
   /// WsServerDurableObject.onClientId
   onClientId(pathId: Id, clientId: Id, addedOrRemoved: IdAddedOrRemoved): void;
+
+  /// WsServerDurableObject.onMessage
+  onMessage(_fromClientId: Id, _toClientId: Id, _remainder: string): void;
 }
 
 /// getWsServerDurableObjectFetch
-export function getWsServerDurableObjectFetch<Namespace extends string>(
+export function getWsServerDurableObjectFetch<
+  Schemas extends OptionalSchemas,
+  Namespace extends string,
+>(
   namespace: Namespace,
 ): (
   request: Request,
   env: {
-    [namespace in Namespace]: DurableObjectNamespace<WsServerDurableObject>;
+    [namespace in Namespace]: DurableObjectNamespace<
+      WsServerDurableObject<Schemas>
+    >;
   },
 ) => Response;
