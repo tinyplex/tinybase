5db102ca7f39673a6a25c333df940c6493c09070
James Pearce <github@tripleodeon.com>
2024-11-20 15:47:15 -0500

    [docs] Social links

--- site/ui/Footer.tsx
+++ site/ui/Footer.tsx
@@ -5,6 +5,16 @@ import React from 'react';
 export const Footer: NoPropComponent = () => (
   <footer>
     <nav>
+      <a id="gh" href="https://github.com/tinyplex/tinybase" target="_blank">
+        GitHub
+      </a>
+      <a
+        id="bs"
+        href="https://bsky.app/profile/tinybase.bsky.social"
+        target="_blank"
+      >
+        Bluesky
+      </a>
       <a id="tw" href="https://x.com/tinybasejs" target="_blank">
         X / Twitter
       </a>
@@ -16,7 +26,7 @@ export const Footer: NoPropComponent = () => (
       <a href="/">
         TinyBase <span id="version" />
       </a>{' '}
-      © 2021- All Rights Reserved
+      © 2022- All Rights Reserved
     </nav>
   </footer>
 );
