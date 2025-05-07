e995fc526221084c24cd5e4839879706d6600daf
James Pearce <github@tripleodeon.com>
2025-05-07 09:55:06 -1000

    [types] Bun types

--- eslint.config.js
+++ eslint.config.js
@@ -78,6 +78,7 @@ export default tsLint.config(
             '^cloudflare:workers$',
             'eslint/config',
             'typescript-eslint',
+            'bun:sqlite',
           ],
         },
       ],
