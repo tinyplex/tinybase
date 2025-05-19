5ac1f9942e275153df36eb8469ef735ffc0920b4
James Pearce <github@tripleodeon.com>
2025-05-19 13:01:30 -1000

    [docs] Generate page index

--- site/build.ts
+++ site/build.ts
@@ -185,6 +185,26 @@ export const build = async (
       });
     }),
   );
+
+  const pagesTable: {[url: string]: {n: string; s: string}} = {};
+  docs.forEachNode((node) => {
+    const summary =
+      node.summary
+        ?.replaceAll(/<[^>]*>/g, '')
+        .replaceAll(/\s+/g, ' ')
+        .trim() ?? '';
+    if (node?.url != '/' && summary) {
+      pagesTable[node.url] = {
+        n: node.name,
+        s: summary,
+      };
+    }
+  });
+  writeFileSync(
+    `${outDir}/pages.json`,
+    JSON.stringify([{p: pagesTable}, {}]),
+    'utf-8',
+  );
 };
 
 const addApi = (docs: Docs): Docs =>
