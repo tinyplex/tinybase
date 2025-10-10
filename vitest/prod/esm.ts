404ae357affaa65ac7302ad6ec9640af24e670fc
James Pearce <github@tripleodeon.com>
2025-10-11 07:36:49 +1300

    [vitest] Prod

---
+++ vitest/prod/esm.ts
@@ -0,0 +1,16 @@
+import type {Store, Store as StoreDebug} from 'tinybase';
+import {createStore, createStore as createStoreDebug} from 'tinybase';
+import type {
+  Store as StoreDebugWithSchemas,
+  Store as StoreWithSchemas,
+} from 'tinybase/with-schemas';
+import {
+  createStore as createStoreDebugWithSchemas,
+  createStore as createStoreWithSchemas,
+} from 'tinybase/with-schemas';
+
+const _store: Store = createStore();
+const _storeDebug: StoreDebug = createStoreDebug();
+const _storeWithSchemas: StoreWithSchemas<any> = createStoreWithSchemas();
+const _storeDebugWithSchemas: StoreDebugWithSchemas<any> =
+  createStoreDebugWithSchemas();
