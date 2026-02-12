cd04c0a4baf65aa37fbc0106b7e5f3e5f5291150
James Pearce <github@tripleodeon.com>
2026-02-12 15:38:34 +1300

    [tests] Parallelize PW

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   testDir: './test/e2e',
   outputDir: 'tmp/test-results',
   fullyParallel: false,
-  workers: 1,
+  workers: 4,
   reporter: 'list',
   use: {trace: 'on-first-retry'},
   projects: [{name: 'chromium', use: {...devices['Desktop Chrome']}}],
