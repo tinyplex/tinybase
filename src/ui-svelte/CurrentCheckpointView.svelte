399936cc8b9033ac9c70bf4aa70a6ad3f34b9465
James Pearce <github@tripleodeon.com>
2026-03-10 22:41:44 +1300

    [svelte] isUndefined

--- src/ui-svelte/CurrentCheckpointView.svelte
+++ src/ui-svelte/CurrentCheckpointView.svelte
@@ -2,6 +2,7 @@
   import type {CurrentCheckpointViewProps} from '../@types/ui-svelte/index.d.ts';
   import {useCheckpointIds} from './hooks.svelte.ts';
   import CheckpointView from './CheckpointView.svelte';
+  import {isUndefined} from '../common/other.ts';
 
   let {checkpoints, debugIds, checkpoint}: CurrentCheckpointViewProps =
     $props();
@@ -9,7 +10,7 @@
   const currentId = $derived(checkpointIds.current[1]);
 </script>
 
-{#if currentId !== undefined}{#if checkpoint}{@render checkpoint(
+{#if !isUndefined(currentId)}{#if checkpoint}{@render checkpoint(
       currentId,
     )}{:else}<CheckpointView
       checkpointId={currentId}
