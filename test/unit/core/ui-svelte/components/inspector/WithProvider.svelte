aa4bc14929e7373bbc23881ed6bddf29a04ee7b8
James Pearce <github@tripleodeon.com>
2026-04-07 12:16:05 +1200

    [sv-inspector] More tests

---
+++ test/unit/core/ui-svelte/components/inspector/WithProvider.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type {Store} from 'tinybase';
+  import {Provider} from 'tinybase/ui-svelte';
+  import {Inspector} from 'tinybase/ui-svelte-inspector';
+
+  let {store}: {readonly store: Store} = $props();
+</script>
+
+<Provider {store}>
+  {#snippet children()}
+    <Inspector open={true} />
+  {/snippet}
+</Provider>
