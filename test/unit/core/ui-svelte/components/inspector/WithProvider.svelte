01ffb68d78c8119b1ad3ee48d85313ea8dd8f2ac
James Pearce <github@tripleodeon.com>
2026-04-07 13:22:59 +1200

    [sv-inspector] More implementation tests

--- test/unit/core/ui-svelte/components/inspector/WithProvider.svelte
+++ test/unit/core/ui-svelte/components/inspector/WithProvider.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
-  import type {Store} from 'tinybase';
+  import type {Indexes, Metrics, Queries, Relationships, Store} from 'tinybase';
   import {Provider} from 'tinybase/ui-svelte';
   import {Inspector} from 'tinybase/ui-svelte-inspector';
 
-  let {store}: {readonly store: Store} = $props();
+  let {
+    store,
+    metrics,
+    indexes,
+    relationships,
+    queries,
+  }: {
+    readonly store: Store;
+    readonly metrics?: Metrics;
+    readonly indexes?: Indexes;
+    readonly relationships?: Relationships;
+    readonly queries?: Queries;
+  } = $props();
 </script>
 
-<Provider {store}>
+<Provider {store} {metrics} {indexes} {relationships} {queries}>
   {#snippet children()}
     <Inspector open={true} />
   {/snippet}
