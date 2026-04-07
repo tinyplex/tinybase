b01f4ab664737f530923b5ef65229b92564f7ed6
James Pearce <github@tripleodeon.com>
2026-04-07 16:10:47 +1200

    [sv-inspector] E2E

--- test/e2e/demos/ui-components-svelte.test.ts
+++ test/e2e/demos/ui-components-svelte.test.ts
@@ -771,4 +771,45 @@ describe('ui-components-svelte', () => {
     await input2.fill('Animation!?');
     await expect(input1).toHaveValue('Animation!?');
   });
+
+  test('Inspector', async ({page}) => {
+    await expectPage(page, `/demos/ui-components-svelte/inspector-svelte`);
+    await expectedElement(page, 'h1', '<Inspector /> (Svelte)');
+    await expectedFramedElement(page, '#info', 'Loaded tables: movies, genres, people, cast');
+    await expectedFramedElement(page, 'aside#tinybaseInspector', undefined, {
+      state: 'attached',
+    });
+    await expectedFramedElement(page, 'aside#tinybaseInspector main[data-position="3"]');
+    await expectedFramedElement(page, 'main', 'Store: default');
+    await expectedFramedElement(page, 'main', 'Tables');
+    await expectedFramedElement(page, 'main', 'Table: genres');
+
+    await (await expectedFramedElement(page, 'img[title="Dock to left"]')).click();
+    await expectedFramedElement(page, 'aside#tinybaseInspector main[data-position="0"]');
+
+    const frame = page.frameLocator('iframe').first();
+    await frame
+      .locator('summary span')
+      .filter({hasText: 'Store: default'})
+      .first()
+      .click();
+    await frame.locator('summary span').filter({hasText: 'Tables'}).first().click();
+    await frame
+      .locator('summary span')
+      .filter({hasText: 'Table: genres'})
+      .first()
+      .click();
+
+    const genresDetails = frame.locator(
+      'details:has(> summary span:has-text("Table: genres"))',
+    );
+    await genresDetails.locator('summary img[title="Edit"]').click();
+
+    const genreInput = genresDetails.locator('tbody tr:nth-of-type(1) td input').first();
+    await expect(genreInput).toHaveValue('Drama');
+    await genreInput.fill('Drama!');
+
+    await genresDetails.locator('summary img[title="Done editing"]').click();
+    await expectedFramedElement(page, 'details td', 'Drama!');
+  });
 });
