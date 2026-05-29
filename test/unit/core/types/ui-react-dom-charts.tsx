9a343eec03bfd92cc24edeedc76bdc1512914ed3
James Pearce <github@tripleodeon.com>
2026-05-30 10:23:15 +1200

    [tests] Snapshots

--- test/unit/core/types/ui-react-dom-charts.tsx
+++ test/unit/core/types/ui-react-dom-charts.tsx
@@ -18,39 +18,61 @@ const UiReactDomChartsWithSchemas =
   UiReactDomCharts as UiReactDomCharts.WithSchemas<
     [typeof _tablesSchema, typeof _valuesSchema]
   >;
-const {
-  BarChart: BarChartWithSchemas,
-  LineChart: LineChartWithSchemas,
-} = UiReactDomChartsWithSchemas;
+const {BarChart: BarChartWithSchemas, LineChart: LineChartWithSchemas} =
+  UiReactDomChartsWithSchemas;
 
-const _App = () => (
-  <>
-    <LineChart tableId="t1" xCellId="c1" yCellId="c1d" />
-    <BarChart queryId="q1" xCellId="c1" yCellId="c1d" />
-    <LineChart tableId="t1" queryId="q1" xCellId="c1" yCellId="c1d" /> {/* ! */}
-    <BarChart queryId="q1" tableId="t1" xCellId="c1" yCellId="c1d" /> {/* ! */}
-    {/*
+const _App = () => {
+  // prettier-ignore
+  LineChartWithSchemas({ // !
+    tableId: 't1',
+    xCellId: 'c1',
+    yCellId: 'c1d',
+    sortCellId: 'c0',
+  });
+  // prettier-ignore
+  BarChartWithSchemas({ // !
+    tableId: 't1',
+    queryId: 'q1',
+    xCellId: 'c1',
+    yCellId: 'c1d',
+  });
+  BarChartWithSchemas({
+    queryId: 'q1',
+    tableId: 't1', // !
+    xCellId: 'c1',
+    yCellId: 'c1d',
+  });
+
+  return (
+    <>
+      <LineChart tableId="t1" xCellId="c1" yCellId="c1d" />
+      <BarChart queryId="q1" xCellId="c1" yCellId="c1d" />
+      {/* eslint-disable max-len */}
+      {/* prettier-ignore */}
+      <LineChart className="// !" tableId="t1" queryId="q1" xCellId="c1" yCellId="c1d" />
+      {/* prettier-ignore */}
+      <BarChart className="// !" queryId="q1" tableId="t1" xCellId="c1" yCellId="c1d" />
+      {/* eslint-enable max-len */}
+      {/*
     
     */}
-    <LineChartWithSchemas tableId="t1" xCellId="c1" yCellId="c1d" />
-    <LineChartWithSchemas
-      tableId="t1"
-      xCellId="c1"
-      yCellId="c1d"
-      sortCellId="c1"
-      descending={true}
-      offset={1}
-      limit={2}
-      className="sales"
-    />
-    <BarChartWithSchemas tableId="t0" xCellId="c0" yCellId="c0" />
-    <BarChartWithSchemas queryId="q1" xCellId="c2" yCellId="c3" />
-    <LineChartWithSchemas tableId="t2" xCellId="c1" yCellId="c1d" /> {/* ! */}
-    <LineChartWithSchemas tableId="t1" xCellId="c2" yCellId="c1" /> {/* ! */}
-    <LineChartWithSchemas tableId="t1" xCellId="c0" yCellId="c1" /> {/* ! */}
-    <LineChartWithSchemas tableId="t1" xCellId="c1" yCellId="c0" /> {/* ! */}
-    <LineChartWithSchemas tableId="t1" xCellId="c1" yCellId="c1d" sortCellId="c0" /> {/* ! */}
-    <BarChartWithSchemas tableId="t1" queryId="q1" xCellId="c1" yCellId="c1d" /> {/* ! */}
-    <BarChartWithSchemas queryId="q1" tableId="t1" xCellId="c1" yCellId="c1d" /> {/* ! */}
-  </>
-);
+      <LineChartWithSchemas tableId="t1" xCellId="c1" yCellId="c1d" />
+      <LineChartWithSchemas
+        tableId="t1"
+        xCellId="c1"
+        yCellId="c1d"
+        sortCellId="c1"
+        descending={true}
+        offset={1}
+        limit={2}
+        className="sales"
+      />
+      <BarChartWithSchemas tableId="t0" xCellId="c0" yCellId="c0" />
+      <BarChartWithSchemas queryId="q1" xCellId="c2" yCellId="c3" />
+      <LineChartWithSchemas tableId="t2" xCellId="c1" yCellId="c1d" /> {/* ! */}
+      <LineChartWithSchemas tableId="t1" xCellId="c2" yCellId="c1" /> {/* ! */}
+      <LineChartWithSchemas tableId="t1" xCellId="c0" yCellId="c1" /> {/* ! */}
+      <LineChartWithSchemas tableId="t1" xCellId="c1" yCellId="c0" /> {/* ! */}
+    </>
+  );
+};
