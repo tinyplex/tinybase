08667ab3f6698895f33311f5f2b8834ab5c50011
James Pearce <github@tripleodeon.com>
2026-06-21 16:02:30 +1200

    [svelte] Test runes

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,11 @@
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
 
+const isNodeModule = (filename) =>
+  filename.includes('/node_modules/') || filename.includes('\\node_modules\\');
+
 export default {
   preprocess: vitePreprocess(),
-  compilerOptions: {runes: true},
+  compilerOptions: {
+    runes: ({filename}) => (isNodeModule(filename) ? undefined : true),
+  },
 };
