a75b8a7340833b26ce11b9564ae72a46209ea6b8
James Pearce <github@tripleodeon.com>
2026-07-14 21:32:19 +1200

    [multistore] Support multiplexing in simple WebSocket server

--- test/unit/synchronizers/synchronizer-ws-server-simple.test.ts
+++ test/unit/synchronizers/synchronizer-ws-server-simple.test.ts
@@ -52,3 +52,40 @@ test('Accessors', async () => {
   expect(wsServerSimple.getWebSocketServer()).toEqual(wssServer);
   await wsServerSimple.destroy();
 });
+
+test('Multiple stores', async () => {
+  const wsServerSimple = createWsServerSimple(
+    new WebSocketServer({port: 8054}),
+  );
+  const webSocket1 = new WebSocket('ws://localhost:8054', 'tinybase');
+  const webSocket2 = new WebSocket('ws://localhost:8054', 'tinybase');
+  const filesStore1 = createMergeableStore('files1', getNow);
+  const filesStore2 = createMergeableStore('files2', getNow);
+  const editorStore1 = createMergeableStore('editor1', getNow);
+  const editorStore2 = createMergeableStore('editor2', getNow);
+  const synchronizers = await Promise.all([
+    createWsSynchronizer(filesStore1, webSocket1, 'files'),
+    createWsSynchronizer(editorStore1, webSocket1, 'editor'),
+    createWsSynchronizer(filesStore2, webSocket2, 'files'),
+    createWsSynchronizer(editorStore2, webSocket2, 'editor'),
+  ]);
+
+  await Promise.all(
+    synchronizers.map((synchronizer) => synchronizer.startSync()),
+  );
+  filesStore1.setCell('files', 'f1', 'name', 'pets.json');
+  editorStore2.setValue('selection', 'fido');
+  await pause();
+
+  expect(filesStore2.getTables()).toEqual({
+    files: {f1: {name: 'pets.json'}},
+  });
+  expect(editorStore1.getValues()).toEqual({selection: 'fido'});
+  expect(filesStore1.getValues()).toEqual({});
+  expect(editorStore2.getTables()).toEqual({});
+
+  await Promise.all(
+    synchronizers.map((synchronizer) => synchronizer.destroy()),
+  );
+  await wsServerSimple.destroy();
+});
