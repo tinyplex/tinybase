9da492f62460a1fdd3f32fb848e6cb457083f1ba
James Pearce <github@tripleodeon.com>
2026-07-18 01:19:06 +1200

    [tests] Cover Svelte provider cleanup fallback

---
+++ test/unit/core/ui-svelte/components/ContextUnknownProviderCleanup.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import {getContext} from 'svelte';
+
+  const delThing = getContext<unknown[]>('tinybase_uisc')[17] as
+    ((offset: number, id: string, owner: object) => void) | undefined;
+
+  delThing?.(0, 'missing', {});
+</script>
